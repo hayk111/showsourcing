6f4b3c56cbcdc9a1ea46cc67f2abc25fc395056f
cedric <cedric@showsourcing.com>
2017-11-19 21:07:16 +0100

    product and task model

---
+++ src/app/modules/store/model/product.model.ts
@@ -0,0 +1,48 @@
+import { Counters } from "./counters.interface";
+import { Category } from "./category.model";
+
+export enum ProductStatus {
+	IDEA = 'Idea',
+	UNDER_ASSESSMENT = 'UnderAssessment',
+	NEED_REVIEW = 'NeedReview',
+	COMPLETE = 'Complete',
+	REFUSED = 'Refused'
+}
+
+export interface Product {
+	id: string;
+	name: string;
+	supplierId: string;
+	categoryId: string;
+	teamId: string;
+	creationDate: number;
+	lastModifiedDate: number;
+	createdByUserId: string;
+	lastUpdatedByUserId: string;
+	counters: Counters;
+	additionalInfo: {customFields: any};
+	additionalInfoCounters: any;
+	status: ProductStatus;
+	flags: { archived: boolean, feedbackRequested: boolean, locked: boolean };
+	computedValues: { taskCount: number },
+	description: string;
+	eventId: string;
+	rating: number;
+	category: Category;
+	mainImage: { 
+		id: string, 
+		filename: string, 
+		imageType: string, 
+		creationDate: number, 
+		createdByUserId: string,
+		orientation: number,
+		urls: {
+			url_60x45: string,
+			url_120x90: string,
+			url_220x165: string,
+			url_400x300: string,
+			url_600x450: string,
+			url_1000x1000: string,
+		};
+	}
+}
---
+++ src/app/modules/store/model/task.model.ts
@@ -0,0 +1,30 @@
+
+export enum TaskStatus {
+	TODO = 'Todo',
+	IN_PROGRESS = 'InProgress',
+	DONE = 'Done'
+}
+
+export enum TaskType {
+	CUSTOM = 'Custom',
+	QUOTATION = 'Quotation',
+	SAMPLE = 'Sample',
+	CATAMPGIE = 'Catalogue'
+}
+
+export interface Task {
+	id: string;
+	entityCounter: number;
+	teamId: string;
+	taskType: TaskType;
+	description: string;
+	status: TaskStatus;
+	code: string;
+	modificationCounterStatus: number;
+	creationDate: number;
+	lastModifiedDate: number;
+	createdByUserId: string;
+	lastUpdatedByUserId: string;
+	archived: boolean;
+	modificationCounterArchived: number;
+}
