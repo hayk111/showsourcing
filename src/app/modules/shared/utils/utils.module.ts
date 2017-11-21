b915e1bd76a2da5794451314504e59e8d83836c1
cedric <cedric@showsourcing.com>
2017-11-21 11:50:24 +0100

    add clickOutside directive

---
+++ src/app/modules/shared/utils/utils.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ClickOutsideDirective } from './directives/click-outside.directive';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [ClickOutsideDirective]
+})
+export class UtilsModule { }
