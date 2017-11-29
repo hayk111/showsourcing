6a8c3bd1328c204209f18b4093d9171cec7d1d4e
cedric <cedric@showsourcing.com>
2017-11-29 10:19:04 +0100

    SHOW26-2062 -upload picture

--- src/app/modules/features/products-page/components/product-dialog/product-dialog.component.ts
+++ src/app/modules/features/products-page/components/product-dialog/product-dialog.component.ts
@@ -1,27 +1,93 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { DialogNames } from '../../../../shared/dialog/dialogs.enum';
+import { Component, OnInit, Input, EventEmitter } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { dotSelector } from '../../../../store/selectors/dot-selector';
 import { FormBuilderService } from '../../../../shared/form-builder/services/form-builder.service';
 import { FormGroupDescriptor } from '../../../../shared/form-builder/interfaces/form-group-descriptor.interface';
 import { FormDescriptor } from '../../../../shared/form-builder/interfaces/form-descriptor.interface';
 import { Observable } from 'rxjs/Observable';
+import { FileUploader, FileItem } from 'ng2-file-upload';
+import { FormGroup } from '@angular/forms/src/model';
+import { AutoUnsub } from '../../../../../utils/auto-unsub.component';
+import { EntityState } from '../../../../store/utils/entities.utils';
+import { Supplier } from '../../../../store/model/supplier.model';
+import { DialogName } from '../../../../store/model/dialog.model';
+import { selectDialog } from '../../../../store/selectors/dialog.selector';
+import { selectProductById } from '../../../../store/selectors/products.selector';
+import { map, switchMap, merge } from 'rxjs/operators';
+import { Product } from '../../../../store/model/product.model';
+import { combineLatest } from 'rxjs/operators/combineLatest';
+import { Subject } from 'rxjs/Subject';
+import { zip } from 'rxjs/observable/zip';
+import { filter } from 'rxjs/operators/filter';
+import { takeUntil } from 'rxjs/operator/takeUntil';
+import { HttpClient } from '@angular/common/http';
+
 
 @Component({
 	selector: 'product-dialog-app',
 	templateUrl: './product-dialog.component.html',
 	styleUrls: ['./product-dialog.component.scss']
 })
-export class ProductDialogComponent implements OnInit {
-	dlgName = DialogNames.PRODUCT;
+export class ProductDialogComponent extends AutoUnsub implements OnInit {
+	dlgName = DialogName.PRODUCT;
 	@Input() product;
-	formDescriptor$: Observable<FormDescriptor>;
+	formDescriptor$;
+	uploader: FileUploader = new FileUploader({autoUpload: false});
+	isOver = false;
+	product$: Observable<EntityState<Product>>;
+	private form$ = new Subject<FormGroup>();
 
-	constructor(private store: Store<any>, private formBuilderSrv: FormBuilderService) {}
+	constructor(private store: Store<any>, private formBuilderSrv: FormBuilderService, private http: HttpClient) {
+		super();
+
+	}
 
 	ngOnInit() {
 		this.formDescriptor$ = this.store.select(dotSelector('customFields.productsCFDef'))
 		.filter( r => r);
+	}
+
+	onDlgRegistered() {
+		// when we receive dlg metadata, we get the correct product
+		this.product$ = this.store.select(selectDialog(DialogName.PRODUCT))
+			.pipe(
+				filter((dlgInfo: any) =>  dlgInfo.metadata),
+				map((dlgInfo: any) => dlgInfo.metadata.id),
+				switchMap(id => this.store.select<any>(selectProductById(id)))
+			);
+		// when form and product are both received we path the value of the form with the product
+		this.form$.pipe(combineLatest(this.product$))
+		.takeUntil(this._destroy$)
+		.subscribe(([form, product]) => {
+			form.patchValue({'Basic info': product});
+			// trigger change detection
+			setTimeout(() => {}, 0);
+		});
+	}
+
+	fileOverBase(e: any): void {
+		this.isOver = e;
+	}
+
+	onFileDrop(fileArr) {
+		const file = fileArr[0];
+		this.http.post('api/image', {imageType: 'Photo'}).subscribe((r: any) => {
+			const xhr: XMLHttpRequest = new XMLHttpRequest();
+			const formData = new FormData();
+			const url = r.url;
+			r.formData.forEach(ent => {
+				Object.entries(ent).forEach(([k, v]) => {
+					formData.append(k, v);
+				});
+			});
+			formData.append('file', file);
+			xhr.open('POST', url, true);
+			xhr.send(formData);
+		});
+	}
+
+	onFormCreated(form: FormGroup) {
+		this.form$.next(form);
 	}
 
 }
