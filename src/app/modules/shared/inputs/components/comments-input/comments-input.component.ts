71e300f69a61934962a654999a9c58359ad4234f
cedric <cedric@showsourcing.com>
2017-12-05 18:34:12 +0100

    comments

---
+++ src/app/modules/shared/inputs/components/comments-input/comments-input.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { AppComment } from '../../../../store/model/comment.model';
+import { Validators, FormControl } from '@angular/forms';
+
+@Component({
+	selector: 'comments-input-app',
+	templateUrl: './comments-input.component.html',
+	styleUrls: ['./comments-input.component.scss']
+})
+export class CommentsInputComponent implements OnInit {
+	@Input() comments: Array<AppComment>;
+	@Output() newComment = new EventEmitter<string>();
+	ctrl = new FormControl('', Validators.required);
+
+	constructor() { }
+
+	ngOnInit() {
+	}
+
+	onEnter() {
+		if (this.ctrl.valid)
+			this.newComment.emit(this.ctrl.value);
+	}
+
+}
