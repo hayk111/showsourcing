569287575ba8e1d5f67ef0cc2196785dbf5d7cd3
cedric <cedric@showsourcing.com>
2017-12-22 02:48:36 +0100

    stretching searchable panel

--- src/app/modules/shared/inputs/components/custom/select/input-select-entity/input-select-entity.component.ts
+++ src/app/modules/shared/inputs/components/custom/select/input-select-entity/input-select-entity.component.ts
@@ -1,15 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Injector, Output, EventEmitter } from '@angular/core';
+import { EntityRepresentation, Entity } from '../../../../../../store/utils/entities.utils';
+import { ChangeDetectorRef } from '@angular/core';
+import { AbstractInput } from '../../../../abstract-input.class';
+import { Store } from '@ngrx/store';
+import { selectEntity, selectEntityArray } from '../../../../../../store/selectors/utils.selector';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
-  selector: 'app-input-select-entity',
-  templateUrl: './input-select-entity.component.html',
-  styleUrls: ['./input-select-entity.component.scss']
+	selector: 'app-input-select-entity',
+	templateUrl: './input-select-entity.component.html',
+	styleUrls: ['./input-select-entity.component.scss']
 })
-export class InputSelectEntityComponent implements OnInit {
+export class InputSelectEntityComponent extends AbstractInput implements OnInit {
+	@Input() entityRep: EntityRepresentation;
+	@Input() multi = false;
+	@Output() itemAdded = new EventEmitter<any>();
+	@Output() itemRemoved = new EventEmitter<any>();
+	entities$: Observable<Array<Entity>>;
 
-  constructor() { }
+	constructor(protected inj: Injector, protected cd: ChangeDetectorRef, private store: Store<any>) {
+		super(inj, cd);
+	}
 
-  ngOnInit() {
-  }
+	ngOnInit() {
+		this.entities$ = this.store.select(selectEntityArray(this.entityRep));
+	}
+
+	onUpdate(v: any) {
+		super.onChange(v);
+		this.update.emit(v);
+	}
 
 }
