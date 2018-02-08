a81f12f17940fdc0c00c2a88b167c04cefedeb1e
Ced c <cedvandenbosch@gmail.com>
2018-02-08 09:40:27 +0100

    ic

--- src/app/modules/shared/icons/components/icon/icon.component.ts
+++ src/app/modules/shared/icons/components/icon/icon.component.ts
@@ -7,8 +7,7 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class IconComponent implements OnInit {
 	@Input() name: string;
-	@Input() width;
-	@Input() height;
+	@Input() size: 'xs' | 's' | 'default' | 'l' | 'xl' | 'xxl';
 	constructor() { }
 
 	ngOnInit() {
