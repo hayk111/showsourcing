48c58bfebcc3bf4f296203e35555c0fd0bd26704
Ced c <cedvandenbosch@gmail.com>
2018-02-09 11:09:23 +0100

    misc before creating entity exist pipe

--- src/app/modules/shared/kanban/kanban.module.ts
+++ src/app/modules/shared/kanban/kanban.module.ts
@@ -4,11 +4,13 @@ import { KanbanColComponent } from './components/kanban-col/kanban-col.component
 import { KanbanItemComponent } from './components/kanban-item/kanban-item.component';
 import { KanbanService } from './services/kanban.service';
 import { CardModule } from '../card/card.module';
+import { IconsModule } from '../icons/icons.module';
 
 @NgModule({
 	imports: [
 		CommonModule,
-		CardModule
+		CardModule,
+		IconsModule
 	],
 	declarations: [ KanbanColComponent, KanbanItemComponent],
 	exports: [ KanbanColComponent, KanbanItemComponent ],
