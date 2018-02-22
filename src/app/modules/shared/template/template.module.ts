3b21b0edd86f585bf35ea733eb7498b114e14c8c
Ced c <cedvandenbosch@gmail.com>
2018-02-22 15:29:52 +0100

    removed unused module

--- src/app/modules/shared/template/template.module.ts
+++ src/app/modules/shared/template/template.module.ts
@@ -3,14 +3,12 @@ import { CommonModule } from '@angular/common';
 import { TemplateComponent } from './components/template/template.component';
 import { AppRoutingModule } from '../app-routing/app-routing.module';
 import { RouterModule } from '@angular/router';
-import { SidenavModule } from '../sidenav/sidenav.module';
 import { HeaderModule } from '../header/header.module';
 
 @NgModule({
 	imports: [
 		CommonModule,
 		RouterModule.forChild([]),
-		SidenavModule,
 		HeaderModule,
 	],
 	providers: [  ],
