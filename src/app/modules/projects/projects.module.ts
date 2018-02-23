2154426c30b9fca4f1270bae5c3f75d1472a7368
Ced c <cedvandenbosch@gmail.com>
2018-02-23 14:08:05 +0100

    project

--- src/app/modules/projects/projects.module.ts
+++ src/app/modules/projects/projects.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ProjectsPageComponent } from './components/projects-page/projects-page.component';
+import { ProjectsPageComponent } from './containers/projects-page/projects-page.component';
 import { ProjectsListViewComponent } from './components/projects-list-view/projects-list-view.component';
-import { EntityPageModule } from '../../shared/entity-page/entity-page.module';
+import { EntityPageModule } from '../shared/entity-page/entity-page.module';
 import { NgxDatatableModule } from '@swimlane/ngx-datatable';
-import { UtilsModule } from '../../shared/utils/utils.module';
-import { AppStoreModule } from '../../store/store.module';
-import { UserModule } from '../../user/user.module';
-import { SelectionBarModule } from '../../shared/selection-bar/selection-bar.module';
+import { UtilsModule } from '../shared/utils/utils.module';
+import { AppStoreModule } from '../store/store.module';
+import { UserModule } from '../user/user.module';
+import { SelectionBarModule } from '../shared/selection-bar/selection-bar.module';
 
 @NgModule({
   imports: [
