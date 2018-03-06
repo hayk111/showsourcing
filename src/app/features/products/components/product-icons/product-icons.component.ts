a54e19532d80e622ebd134cff4f1b90af8221691
Hassan Hammoud <hghammoud@gmail.com>
2018-03-06 23:27:13 +0100

    - Improvements to product-icons-app components (no more ngIf) - Hooked the projects numbers to product-selectable-card

--- src/app/features/products/components/product-icons/product-icons.component.ts
+++ src/app/features/products/components/product-icons/product-icons.component.ts
@@ -3,18 +3,17 @@ import { Component, OnInit, Input } from '@angular/core';
 @Component({
 	selector: 'product-icons-app',
 	templateUrl: './product-icons.component.html',
-	styleUrls: ['./product-icons.component.scss']
+	styleUrls: ['./product-icons.component.scss'],
 })
 export class ProductIconsComponent implements OnInit {
 	@Input() big = false;
 	@Input() score;
 	@Input() numComments: number;
+	@Input() numProjects: number;
 	@Input() numTasks: number;
 	@Input() sample: boolean;
 
-	constructor() { }
+	constructor() {}
 
-	ngOnInit() {
-	}
-
+	ngOnInit() {}
 }
