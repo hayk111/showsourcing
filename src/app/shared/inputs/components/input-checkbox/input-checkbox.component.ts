3796b3bb1f1eef03d2b82873d30cca35619a9ed7
Ced c <cedvandenbosch@gmail.com>
2018-03-08 10:44:16 +0100

    externalize checkbox

--- src/app/shared/inputs/components/input-checkbox/input-checkbox.component.ts
+++ src/app/shared/inputs/components/input-checkbox/input-checkbox.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
-  selector: 'app-input-checkbox',
-  templateUrl: './input-checkbox.component.html',
-  styleUrls: ['./input-checkbox.component.scss']
+	selector: 'input-checkbox-app',
+	templateUrl: './input-checkbox.component.html',
+	styleUrls: ['./input-checkbox.component.scss'],
 })
 export class InputCheckboxComponent implements OnInit {
+	@Input() checked = false;
+	// when clicked returns the opposite of current.
+	// so if it's checked then we click it, it will return false
+	@Output() update = new EventEmitter<boolean>();
+	constructor() {}
 
-  constructor() { }
+	ngOnInit() {}
 
-  ngOnInit() {
-  }
-
+	onClick() {
+		this.update.emit(!this.checked);
+	}
 }
