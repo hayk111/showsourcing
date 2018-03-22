2c48b114f4860a3cc3ecb54aad4c75b9082cd832
Ced c <cedvandenbosch@gmail.com>
2018-03-22 15:15:22 +0100

    resolve inco terms and harbour not being stored in store properyl

--- src/app/app-root/store/effects/entities/inco-terms.effects.ts
+++ src/app/app-root/store/effects/entities/inco-terms.effects.ts
@@ -7,12 +7,12 @@ import { EntityService, ERM } from '~app/shared/entity';
 @Injectable()
 export class IncoTermsEffects {
 	@Effect()
-	load$ = this.action$
-		.ofType<any>(ActionType.LOAD)
-		.pipe(
-			switchMap(_ => this.srv.load({ target: ERM.incoTerms })),
-			map((result: any) => IncoTermsActions.add(result))
-		);
+	load$ = this.action$.ofType<any>(ActionType.LOAD).pipe(
+		switchMap(_ => this.srv.load({ target: ERM.incoTerms })),
+		// we receive an array of string, instead we need entities so we can transform those string into entities
+		map((result: Array<string>) => result.map(str => ({ id: str, name: str }))),
+		map((result: any) => IncoTermsActions.add(result))
+	);
 
 	constructor(private action$: Actions, private srv: EntityService) {}
 }
