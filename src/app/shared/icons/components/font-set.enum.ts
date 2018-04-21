4e5d66515c08e04edc28e8889809c01cd313379b
Ced c <cedvandenbosch@gmail.com>
2018-04-21 18:52:45 +0200

    first try not compiling yet

---
+++ src/app/shared/icons/components/font-set.enum.ts
@@ -0,0 +1,5 @@
+export enum FontSet {
+	FA = 'fa',
+	SVG = 'svg',
+	ICOMOON = 'icomoon',
+}
--- src/app/shared/icons/components/icon/icon.component.ts
+++ src/app/shared/icons/components/icon/icon.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Input, ViewChild, TemplateRef, Attribute } from '@an
 import { ChangeDetectionStrategy } from '@angular/core';
 import * as fontawesome from '@fortawesome/fontawesome';
 import { ElementRef } from '@angular/core';
+import { FontSet } from '../font-set.enum';
 
 @Component({
 	selector: 'icon-app',
@@ -9,24 +10,13 @@ import { ElementRef } from '@angular/core';
 	styleUrls: ['./icon.component.scss'],
 	changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class IconComponent implements OnInit {
+export class IconComponent {
 	@Input() name: string;
-	@Input() sizePx: number;
-	@Input() size: 'xs' | 's' | 'default' | 'l' | 'xl' | 'xxl' = 'default';
-	@Input() color = 'inherit';
-	// some icons are displayed with a circular background
-	@Input() circleSize: number;
-	// color is done via css4 var
-	@Input() circleColor: string;
-	@Input() circleBorderSize: number;
-	@Input() circleBorderColor: string;
 	// type solid by default https://fontawesome.com/icons/heart?style=regular
-	private _type = 's';
+	@Input() type: 's' | 'r' | 'l' = 's';
 	// symbols give perf gains but are less configurable
-	@Input() useSymbol = true;
-	// whether font-awesome is used or not
-	@Input() fontSet: 'fa' | 'icomoon' | 'svg' = 'icomoon';
-	@ViewChild('icon') icon: TemplateRef<any>;
+	// the fontset used, could be font awesome, svg or anything else added
+	@Input() fontSet: FontSet = FontSet.FA;
 
 	constructor(elementRef: ElementRef, @Attribute('aria-hidden') ariaHidden: string) {
 		// If the user has not explicitly set aria-hidden, mark the icon as hidden, as this is
@@ -36,33 +26,6 @@ export class IconComponent implements OnInit {
 		}
 	}
 
-	ngOnInit() {
-		if (!this.useSymbol) fontawesome.dom.i2svg(this.icon.elementRef.nativeElement);
-	}
 
-	@Input()
-	set type(type: string) {
-		// input is either solid regular or light
-		// we want the accronym s | r | l
-		this._type = type.charAt(0);
-	}
-
-	get type() {
-		return this._type;
-	}
-
-	get style() {
-		return {
-			color: this.color === 'inherit' ? this.color : 'var(--color-' + this.color + ')',
-			// background is used for circle
-			background: this.circleColor ? 'var(--color-' + this.circleColor + ')' : 'transparent',
-			'font-size': this.sizePx ? this.sizePx + 'px' : 'var(--font-size-' + this.size + ')',
-			height: this.circleSize + 'px',
-			width: this.circleSize + 'px',
-			border: this.circleBorderSize
-				? this.circleBorderSize + 'px solid var(--color-' + this.circleBorderColor + ')'
-				: 'none',
-		};
-	}
 
 }
