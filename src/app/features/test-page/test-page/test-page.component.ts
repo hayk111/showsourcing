45dd833c0bbc89087e052c32fdb852a105afd03a
Ced c <cedvandenbosch@gmail.com>
2018-05-22 17:52:24 +0200

    custom fields

--- src/app/features/test-page/test-page/test-page.component.ts
+++ src/app/features/test-page/test-page/test-page.component.ts
@@ -1,38 +1,70 @@
 import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
 import { CustomField } from '~shared/dynamic-forms';
 import { FormGroup } from '@angular/forms';
+import { Apollo } from 'apollo-angular';
+import gql from 'graphql-tag';
+import { Supplier } from '~models';
+import { SupplierQueries } from '~features/supplier/services/supplier.queries';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+const supplierMutation = gql`
+mutation supplier($input: SupplierInput) {
+	updateSupplier(input: $input) {
+		id
+	}
+}
+`;
+
+const querySupplier = gql`
+	subscription supplier($query: String!) {
+		suppliers(query: $query) {
+			id, name
+		}
+	}
+`;
 
 @Component({
 	selector: 'app-test-page',
 	templateUrl: './test-page.component.html',
 	styleUrls: ['./test-page.component.scss'],
-	changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class TestPageComponent implements OnInit {
-	currency = 'PKR';
-	country = ['DZ'];
-	harbour = 'Bangkok';
-	incoTerm = 'FOB';
-	supplier = ["b8b9726f-c14a-4921-ab06-b0803e243557"];
-	category = "609cf9eb-d63f-448b-9f41-a3c5d678a5a6";
-	event;
-	tags = [];
-
-	text = 'lol';
 
 	customFields: CustomField[] = [
-		{ name: 'name', type: 'text', label: 'some label' }
-	]
+		{ name: 'name', type: 'text', label: 'Name' }
+	];
+	supplier$: Observable<Supplier>;
+	supplier: Supplier;
+	form: FormGroup;
 
-	constructor() { }
+	constructor(private apollo: Apollo) { }
 
 	ngOnInit() {
+		this.supplier$ = this.apollo.subscribe({
+			query: querySupplier,
+			variables: { query: 'id = "3243ed5b-4e7b-4646-a858-5e0c41427ccf"' }
+		}).pipe(map((r: any) => r.data.suppliers[0]));
+		this.supplier$.subscribe(s => {
+			this.supplier = s;
+		});
 	}
 
 
 	onFormCreated(form: FormGroup) {
-		form.valueChanges.subscribe(d => {
-			debugger;
-		})
+		this.form = form;
+		form.valueChanges.subscribe(supplier => this.updateSupplier(supplier));
+	}
+
+	updateSupplier(supplier) {
+		const supplierUpdate = {
+			id: this.supplier.id,
+			...supplier
+		}
+		this.apollo.mutate({
+			mutation: supplierMutation, variables: {
+				input: supplierUpdate
+			}
+		}).subscribe();
 	}
 }
