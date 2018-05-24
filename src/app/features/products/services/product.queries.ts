ae17e31c7991c629fbf4ad0836daf47d2bfe4a53
Thierry Templier <templth@gmail.com>
2018-05-24 11:12:25 +0200

    feat(product): migrated product list to graphql

--- src/app/features/products/services/product.queries.ts
+++ src/app/features/products/services/product.queries.ts
@@ -1,6 +1,20 @@
 import gql from 'graphql-tag';
 
 export class ProductQueries {
+    static list = gql`
+		query products {
+			products(take: 30) {
+				id,
+				name,
+                description,
+				creationDate,
+				createdBy {
+					lastName,
+					firstName
+				}
+			}
+        }`;
+
 	static oneProduct = gql`
 		subscription products($query: String!) {
 			products(query: $query) {
