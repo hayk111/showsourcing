42c7d9dcad2a5bac7db804ce7b7a8d59bceec7f2
Ced c <cedvandenbosch@gmail.com>
2018-05-28 13:45:50 +0200

    removed hmr reliques

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,6 +3,5 @@ import { LogLevels } from '~utils/logger/log-levels';
 export const environment = {
 	production: true,
 	apiUrl: 'https://stoemelings.showsourcing.com',
-	LOG_LEVEL: LogLevels.ERROR,
 	hmr: false,
 };
