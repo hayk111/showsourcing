fabfca133b6fea66928d7457ec880dfdcfe0edb3
Ced c <cedvandenbosch@gmail.com>
2018-05-29 15:42:09 +0200

    fixed environment variable

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,5 +3,5 @@ import { LogLevels } from '~utils/logger/log-levels';
 export const environment = {
 	production: true,
 	apiUrl: 'https://stoemelings.showsourcing.com',
-	hmr: false,
+	LOG_LEVEL: LogLevels.ERROR,
 };
