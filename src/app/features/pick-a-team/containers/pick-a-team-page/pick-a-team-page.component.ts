349fb0ecc1469938fd9a3b3a2e927489c2b3465a
Ced c <cedvandenbosch@gmail.com>
2018-06-04 17:12:52 +0200

    some style changes

--- src/app/features/pick-a-team/containers/pick-a-team-page/pick-a-team-page.component.ts
+++ src/app/features/pick-a-team/containers/pick-a-team-page/pick-a-team-page.component.ts
@@ -7,7 +7,7 @@ import { Router } from '@angular/router';
 @Component({
 	selector: 'pick-a-team-page-app',
 	templateUrl: './pick-a-team-page.component.html',
-	styleUrls: ['./pick-a-team-page.component.scss'],
+	styleUrls: ['./pick-a-team-page.component.scss', '../../../auth/components/form-style.scss'],
 	changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class PickATeamPageComponent {
