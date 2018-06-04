40ac1f1190a1f0290f4a5f04cc5f4e3e173d0b9a
Ced c <cedvandenbosch@gmail.com>
2018-06-04 15:04:30 +0200

    pick a team  styling

--- src/app/features/pick-a-team/containers/pick-a-team-page/pick-a-team-page.component.ts
+++ src/app/features/pick-a-team/containers/pick-a-team-page/pick-a-team-page.component.ts
@@ -12,6 +12,9 @@ import { Router } from '@angular/router';
 })
 export class PickATeamPageComponent {
 	form: FormGroup;
+	pending = false;
+	error: string;
+
 	constructor(private fb: FormBuilder, private srv: PickATeamService, private router: Router) {
 		this.form = this.fb.group({
 			name: ['', Validators.required]
@@ -19,9 +22,16 @@ export class PickATeamPageComponent {
 	}
 
 	onSubmit() {
-		this.srv.createTeam(new Team(this.form.value)).subscribe(_ => {
-			this.router.navigate(['']);
-		});
+		this.pending = true;
+		this.srv.createTeam(new Team(this.form.value)).subscribe(
+			_ => {
+				this.pending = false;
+				this.router.navigate(['']);
+			},
+			e => {
+				this.error = 'We had an error creating your team. Please try again.';
+			}
+		);
 	}
 
 }
