3acfd1d414f7e43e9a4226150184b46985a5d855
Ced c <cedvandenbosch@gmail.com>
2018-06-05 14:13:32 +0200

    resolving conflicts

--- src/app/features/pick-a-team/services/has-team-guard.service.ts
+++ src/app/features/pick-a-team/services/has-team-guard.service.ts
@@ -1,39 +1,30 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, CanActivateChild, RouterStateSnapshot, ActivatedRouteSnapshot, Router } from '@angular/router';
-import { Observable, empty } from 'rxjs';
-import { TokenService } from '~features/auth/services';
-import { ApolloClient, ApolloService } from '~shared/apollo/services';
-import { filter, switchMap, map, tap, catchError } from 'rxjs/operators';
-import { PickATeamService } from '~features/pick-a-team/services/pick-a-team.service';
-import { Log } from '~utils';
+import { CanActivate, CanActivateChild, RouterStateSnapshot, ActivatedRouteSnapshot } from '@angular/router';
+import { Observable } from 'rxjs';
+import { TokenService } from '~features/auth';
+import { ApolloClient } from '~shared/apollo';
+import { filter, switchMap, map } from 'rxjs/operators';
+import { HasTeamQueries } from '~features/pick-a-team/services/has-team.queries';
 
 @Injectable({
 	providedIn: 'root'
 })
-export class HasTeamGuard implements CanActivate, CanActivateChild {
+export class HasTeamGuardService implements CanActivate, CanActivateChild {
 
 
-	constructor(private srv: PickATeamService, private router: Router, private apolloSrv: ApolloService) { }
+	constructor(private apollo: ApolloClient) { }
 
 	canActivateChild(childRoute: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean | Observable<boolean> | Promise<boolean> {
 		return this.canActivate(childRoute, state);
 	}
 
 	canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean | Observable<boolean> | Promise<boolean> {
-		return this.apolloSrv.userClientReady$.pipe(
+		return this.apollo.clientReady$.pipe(
 			filter(ready => ready !== null),
-			switchMap(_ => this.srv.getTeams()),
-			map(teams => teams.length),
-			map(length => length > 0),
-			tap(v => this.redirect(v)),
-			tap(v => Log.debug('has team guard: has team ?', v))
+			switchMap(_ => this.apollo.query({ query: HasTeamQueries.getTeams }).valueChanges),
+			map((r: any) => r.data.teams.length),
+			map(length => length > 0)
 		);
-	}
-
-	private redirect(hasTeam: boolean) {
-		if (!hasTeam) {
-			this.router.navigate(['user', 'pick-a-team']);
-		}
 	}
 
 }
