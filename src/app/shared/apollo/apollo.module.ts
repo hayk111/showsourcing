f6a563d9bc572bcaa9555c614b89729f6c2627c9
Ced c <cedvandenbosch@gmail.com>
2018-06-05 14:21:02 +0200

    resolving conflicts

--- src/app/shared/apollo/apollo.module.ts
+++ src/app/shared/apollo/apollo.module.ts
@@ -1,26 +1,13 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
-<<<<<<< HEAD
 import { ApolloModule } from 'apollo-angular';
 import { HttpLinkModule } from 'apollo-angular-link-http';
-
-import { ApolloIssuePageComponent } from './components/apollo-issue-page/apollo-issue-page.component';
 import { ApolloClient } from '~shared/apollo/services/apollo-client.service';
 import { ApolloService } from '~shared/apollo/services/apollo.service';
-=======
-import { Apollo, ApolloModule } from 'apollo-angular';
-import { HttpLink, HttpLinkModule } from 'apollo-angular-link-http';
-import { split, from } from 'apollo-link';
-import { WebSocketLink } from 'apollo-link-ws';
-import { getMainDefinition } from 'apollo-utilities';
-import { InMemoryCache } from 'apollo-cache-inmemory';
-import { cleanTypenameLink } from './clean.typename.link';
-import { AuthenticationService } from '~features/auth/services/authentication.service';
-import { BehaviorSubject, Observable } from 'rxjs';
-import { ClientQueries } from '~shared/apollo/apollo-client-queries';
-import { map } from 'rxjs/operators';
-import { TokenService } from '~features/auth/services/token.service';
->>>>>>> implementing the schema
+
+import {
+	ApolloIssuePageComponent,
+} from './components/apollo-issue-page/apollo-issue-page.component';
 
 
 
