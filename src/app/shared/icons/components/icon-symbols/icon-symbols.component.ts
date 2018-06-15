b7cf95e8191f50aa802041771195b91f1669bb92
Ced c <cedvandenbosch@gmail.com>
2018-06-15 19:00:27 +0200

    icons

--- src/app/shared/icons/components/icon-symbols/icon-symbols.component.ts
+++ src/app/shared/icons/components/icon-symbols/icon-symbols.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { faIcons } from '../../font-awesome.config';
+import { faIconsRegular, faIconsSolid } from '~shared/icons/font-awesome.config';
 
 // This component should be put once in the app so it can create symbols for enhanced performances
 // Perf was an issue in big lists https://fontawesome.com/how-to-use/performance-and-security
@@ -9,8 +9,10 @@ import { faIcons } from '../../font-awesome.config';
 	styleUrls: ['./icon-symbols.component.scss'],
 })
 export class IconSymbolsComponent implements OnInit {
-	icons = faIcons;
-	constructor() {}
+	iconsSolid = faIconsSolid;
+	iconsRegular = faIconsRegular;
 
-	ngOnInit() {}
+	constructor() { }
+
+	ngOnInit() { }
 }
