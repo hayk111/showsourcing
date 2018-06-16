49bfc10b257d23ded607ae729ccd81b678dad078
Ced c <cedvandenbosch@gmail.com>
2018-06-16 21:13:27 +0200

    some style fixes

--- src/app/shared/header/components/header/header.component.ts
+++ src/app/shared/header/components/header/header.component.ts
@@ -16,7 +16,6 @@ export class HeaderComponent extends AutoUnsub implements OnInit {
 
 	constructor(private authSrv: AuthenticationService) {
 		super();
-		console.log('header');
 	}
 
 	ngOnInit() {
