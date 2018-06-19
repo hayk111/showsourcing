538bef5863ec293db7f647017b380104099077f4
Ced c <cedvandenbosch@gmail.com>
2018-06-19 11:04:43 +0200

    price close event

--- src/app/shared/dynamic-forms/components/dynamic-editable-text/inputs/editable-price/editable-price.component.ts
+++ src/app/shared/dynamic-forms/components/dynamic-editable-text/inputs/editable-price/editable-price.component.ts
@@ -3,6 +3,7 @@ import { makeAccessorProvider, AbstractInput, InputDirective } from '~shared/inp
 import { CustomField } from '~shared/dynamic-forms/models';
 import { Price } from '~models';
 import { SelectorConstComponent } from '~shared/selectors/components/selector-const/selector-const.component';
+import { EditableTextComponent } from '~shared/editable-field';
 
 @Component({
 	selector: 'editable-price-app',
@@ -16,6 +17,7 @@ export class EditablePriceComponent extends AbstractInput {
 	@Input() customField: CustomField;
 	@Output() change = new EventEmitter();
 	@Output() blur = new EventEmitter();
+	@ViewChild('editable2') currencyEditable: EditableTextComponent;
 	isOpen: boolean;
 	accumulator;
 
@@ -32,6 +34,7 @@ export class EditablePriceComponent extends AbstractInput {
 
 	onSave() {
 		this.value = this.accumulator;
+		this.isOpen = false;
 		this.onChange();
 	}
 
@@ -46,6 +49,13 @@ export class EditablePriceComponent extends AbstractInput {
 	onChange() {
 		this.onChangeFn(this.value);
 		this.change.emit(this.value);
+		// change only happens when the field closes
+	}
+
+	onCurrencyChange() {
+		this.isOpen = false;
+		this.onChange();
+		this.currencyEditable.close();
 	}
 
 	onBlur() {
