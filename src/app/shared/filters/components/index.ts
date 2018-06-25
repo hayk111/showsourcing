08bb8561021d3de4f7732107b1d1e75e06ae7706
Ced c <cedvandenbosch@gmail.com>
2018-06-25 05:38:07 +0200

    build fixes

--- src/app/shared/filters/components/index.ts
+++ src/app/shared/filters/components/index.ts
@@ -4,4 +4,3 @@ export * from './filter-price-panel/filter-price-panel.component';
 export * from './filter-rating-panel/filter-rating-panel.component';
 export * from './filter-tag/filter-tag.component';
 export * from './filter-smart-panel/filter-smart-panel.component';
-export * from './filter-panel/filter-panel.component';
