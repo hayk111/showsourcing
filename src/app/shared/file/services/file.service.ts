adb8e7586f9d57fd22e8af5e6a70782a790aaa1e
Ced c <cedvandenbosch@gmail.com>
2018-06-26 16:03:49 +0200

    adding support for files

--- src/app/shared/file/services/file.service.ts
+++ src/app/shared/file/services/file.service.ts
@@ -5,6 +5,7 @@ import { mergeMap, tap, filter, first, switchMap, map } from 'rxjs/operators';
 import { ImageUploadService } from '~global-services';
 import { AppFile, AppImage, ImageUploadRequest } from '~models';
 import { log, LogColor } from '~utils';
+import { GlobalService } from '~global-services/_global/global.service';
 
 
 @Injectable({ providedIn: 'root' })
@@ -15,61 +16,38 @@ export class FileService {
 	) { }
 
 	uploadImages(imgs: File[]): Observable<any> {
-		return forkJoin(imgs.map(img => this.uploadImage(img))).pipe(first());
+		return forkJoin(imgs.map(img => this.uploadFile(img, 'image'))).pipe(first());
 	}
 
-	uploadImage(img: File): Observable<AppImage> {
-		const request = new ImageUploadRequest();
-		return this.imageUploadSrv.create(request).pipe(
+	uploadFiles(files: File[]): Observable<any> {
+		return forkJoin(files.map(file => this.uploadFile(file, 'file'))).pipe(first());
+	}
+
+	uploadFile(file: File, type: 'file' | 'image' = 'file'): Observable<AppImage> {
+		const isImage = type === 'image';
+		const request = isImage ? new ImageUploadRequest() : new FileUploadRequest();
+		const service: GlobalService<any> = isImage ? this.imageUploadSrv : this.fileUploadSrv;
+		const returned = isImage ? request.image : request.file;
+
+		return this.service.create(request).pipe(
 			// subscribing to that upload request so we can wait till it's ready
-			mergeMap(_ => this.imageUploadSrv.selectOne(request.id)),
+			mergeMap(_ => service.selectOne(request.id)),
 			filter(imgRequest => imgRequest.status === 'upload-ready'),
 			// when ready we make the upload
-			mergeMap(info => this.uploadImageToAws(info, img)),
+			mergeMap(info => this.uploadFileToAws(info, file)),
 			// when the upload is done we tell realm that it's the case
-			switchMap(_ => this.imageUploadSrv.update({ id: request.id, status: 'uploaded' })),
+			switchMap(_ => service.update({ id: request.id, status: 'uploaded' })),
 			first(),
 			// sending the image back
-			map(_ => request.image)
+			map(_ => returned)
 		);
 	}
-
-	// uploadFiles(files: Array<AppFile>, type: 'image' | 'attachment' = 'attachment') {
-	// 	return combineLatest(
-	// 		files.map((file: AppFile) =>
-	// 			// resolving to a swap so we can replace easily
-	// 			this.uploadFile(file)
-	// 		)
-	// 	);
-	// }
-
-	// uploads a file and returns an observable of the response which is the saved file
-	// uploadFile(
-	// 	file: AppFile,
-	// 	type: 'image' | 'attachment' = 'attachment'
-	// ): Observable<any> {
-	// 	let data;
-	// 	const fileName = file.fileName;
-
-	// 	if (type === 'attachment')
-	// 		data = { fileName, id: file.id };
-	// 	else
-	// 		data = { imageType: 'Photo', id: file.id };
-
-	// 	return this.upload(data, type, file).pipe(
-	// 		tap(_ => this.notificationSrv.add({
-	// 			type: NotificationType.SUCCESS,
-	// 			title: 'File Uploaded',
-	// 			message: 'Your file was uploaded with success',
-	// 		}))
-	// 	);
-	// }
 
 	download(url: string) {
 		window.open(url);
 	}
 
-	private uploadImageToAws(awsInfo, file: any): Observable<AppImage> {
+	private uploadFileToAws(awsInfo, file: any): Observable<AppImage> {
 		log.group('%c uploading to aws', LogColor.SERVICES);
 		log.debug(`%c upload url ${awsInfo.uploadUrl}`, LogColor.SERVICES);
 		log.group('%c form data', LogColor.SERVICES);
@@ -87,7 +65,9 @@ export class FileService {
 	// we receive an array but formData wants an object
 	private converFormData(file: any, formDataObj: any) {
 		const formData = new FormData();
-		formData.append('Content-Type', 'image/jpg');
+		const contentType;
+		debugger;
+		// formData.append('Content-Type', );
 		delete formDataObj.__typename;
 		Object.entries(formDataObj).forEach(([k, v]: any) => formData.append(k, v));
 		formData.append('file', file);
