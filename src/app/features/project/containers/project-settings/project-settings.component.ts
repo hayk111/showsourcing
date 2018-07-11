7c0e23f80831066de13fc3995dc55c93413355eb
Ced c <cedvandenbosch@gmail.com>
2018-07-11 15:10:43 +0200

    rebase

--- src/app/features/project/containers/project-settings/project-settings.component.ts
+++ src/app/features/project/containers/project-settings/project-settings.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ProjectService } from '~global-services';
+import { switchMap, map, tap } from 'rxjs/operators';
+import { Observable } from 'rxjs';
+import { Project } from '~models/project.model';
 
 @Component({
 	selector: 'project-settings-app',
@@ -7,10 +12,15 @@ import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
 	changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ProjectSettingsComponent implements OnInit {
+	project$: Observable<Project>;
 
-	constructor() { }
+	constructor(private route: ActivatedRoute, private projectSrv: ProjectService) { }
 
 	ngOnInit() {
+		this.project$ = this.route.parent.params.pipe(
+			map(params => params.id),
+			switchMap(id => this.projectSrv.selectOne(id))
+		);
 	}
 
 }
