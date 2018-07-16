5a24f0eee2915a309befa97bc85b4560e4ad1f5d
Ced c <cedvandenbosch@gmail.com>
2018-07-16 12:37:42 +0200

    added pagination with button to product list

--- src/app/shared/table/components/custom-table/custom-table.component.ts
+++ src/app/shared/table/components/custom-table/custom-table.component.ts
@@ -15,12 +15,15 @@ export class CustomTableComponent {
 	@Input() contextualMenu: TemplateRef<any>;
 	@Input() rows: Array<any> = [];
 	@Input() pending: boolean;
+	@Input() currentPage;
 	// maps of the <id, true> so we can access the items that are selected
 	@Input() selected: Map<string, boolean> = new Map();
 	@Output() selectOne = new EventEmitter<string>();
 	@Output() unselectOne = new EventEmitter<string>();
 	@Output() selectAll = new EventEmitter<string[]>();
 	@Output() unselectAll = new EventEmitter<null>();
+	@Output() previous = new EventEmitter<null>();
+	@Output() next = new EventEmitter<null>();
 
 	constructor() { }
 
