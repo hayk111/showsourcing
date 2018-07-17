63ace7c9d7bf918b644480e57067cf657de70681
Ced c <cedvandenbosch@gmail.com>
2018-07-17 17:50:47 +0200

    scroll auto

--- src/app/shared/inputs/components-directives/textarea-auto-grows.directive.ts
+++ src/app/shared/inputs/components-directives/textarea-auto-grows.directive.ts
@@ -1,16 +1,22 @@
-import { Directive, HostBinding, ElementRef } from '@angular/core';
+import { Directive, HostBinding, ElementRef, Renderer2, HostListener, OnInit } from '@angular/core';
 
 
 @Directive({
 	selector: '[autoGrows]',
 })
-export class TextareaAutoGrowsDirective {
+export class TextareaAutoGrowsDirective implements OnInit {
 
-	constructor(private el: ElementRef) { }
+	constructor(private el: ElementRef, private renderer: Renderer2) { }
 
-	@HostBinding('keyup')
-	onKeyUp() {
-		// this
+	ngOnInit() {
+		this.renderer.setStyle(this.el.nativeElement, 'resize', 'none');
+		this.renderer.setStyle(this.el.nativeElement, 'overflow', 'hidden');
+		this.onInput();
+	}
+
+	@HostListener('input')
+	onInput() {
+		this.renderer.setStyle(this.el.nativeElement, 'height', this.el.nativeElement.scrollHeight + 'px');
 	}
 
 }
