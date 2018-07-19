2915e50dd70a2e261a826a4d88f4cd76fb8b4f2a
Ced c <cedvandenbosch@gmail.com>
2018-07-19 15:14:45 +0200

    resolving

--- src/app/features/project/containers/project-products/project-products.component.ts
+++ src/app/features/project/containers/project-products/project-products.component.ts
@@ -1,17 +1,14 @@
-import {
-	ChangeDetectionStrategy, Component, OnInit, ViewChild, Output,
-	EventEmitter, TemplateRef, Renderer2
-} from '@angular/core';
-import { Router } from '@angular/router';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { map, takeUntil } from 'rxjs/operators';
 import { NewProductDialogComponent } from '~features/products/components/new-product-dialog/new-product-dialog.component';
 import { ProductService } from '~global-services';
-import { Product, ERM } from '~models';
+import { ERM, Product } from '~models';
 import { DialogService } from '~shared/dialog';
-import { FilterService, SearchService } from '~shared/filters';
+import { FilterService, SearchService, FilterType } from '~shared/filters';
 import { ListPageComponent } from '~shared/list-page/list-page.component';
 import { SelectionService } from '~shared/list-page/selection.service';
 import { StoreKey } from '~utils';
-import { TableDescriptor, ColumnDescriptor } from '~shared/table';
 
 @Component({
 	selector: 'project-products-app',
@@ -23,18 +20,27 @@ import { TableDescriptor, ColumnDescriptor } from '~shared/table';
 		{ provide: 'storeKey', useValue: StoreKey.FILTER_PROJECT_PRODUCTS }
 	]
 })
-export class ProjectProductsComponent extends ListPageComponent<Product, ProductService> {
+export class ProjectProductsComponent extends ListPageComponent<Product, ProductService> implements OnInit {
+
 	constructor(
 		protected router: Router,
 		protected srv: ProductService,
 		protected selectionSrv: SelectionService,
 		protected filterSrv: FilterService,
 		protected searchSrv: SearchService,
-		protected dlgSrv: DialogService) {
+		protected dlgSrv: DialogService,
+		protected route: ActivatedRoute) {
 		super(router, srv, selectionSrv, filterSrv, searchSrv, dlgSrv, ERM.PRODUCT, NewProductDialogComponent);
 	}
 
+	ngOnInit() {
+		this.route.params.pipe(
+			map(params => params.id),
+			takeUntil(this._destroy$)
+		).subscribe(id => {
+			this.filterSrv.addFilter({ type: FilterType.PROJECT, value: id });
+		});
+		super.ngOnInit();
+	}
 
-	// TODO we need to change the relation from project.products to product.projects
-	// since we are filtering the name
 }
--- src/app/features/project/services/project-feature.service.ts
+++ src/app/features/project/services/project-feature.service.ts
@@ -1,16 +1,18 @@
-import { ProjectService } from '~global-services';
 import { Injectable } from '@angular/core';
-import { ApolloWrapper } from '~shared/apollo/services/apollo-wrapper.service';
-import { Observable } from 'subscriptions-transport-ws';
+import { ProjectService, ProductService } from '~global-services';
+import { ApolloWrapper } from '~shared/apollo';
+import { Observable } from 'rxjs';
 import { SelectParams } from '~global-services/_global/select-params';
 import { of } from 'rxjs';
-import { switchMap } from 'rxjs/operators';
 
 @Injectable({
 	providedIn: 'root'
 })
 export class ProjectFeatureService extends ProjectService {
-	constructor(protected wrapper: ApolloWrapper) {
+	constructor(
+		protected wrapper: ApolloWrapper,
+		protected productSrv: ProductService
+	) {
 		super(wrapper);
 	}
 
