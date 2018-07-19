384cdef341936ec99b1f8edd228538f741d34ab5
Ced c <cedvandenbosch@gmail.com>
2018-07-19 15:22:13 +0200

    resolving

--- src/app/features/products/containers/product-details/product-details.component.ts
+++ src/app/features/products/containers/product-details/product-details.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, NgModuleRef } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Observable } from 'rxjs';
 import { map, switchMap, takeUntil, tap } from 'rxjs/operators';
@@ -25,7 +25,8 @@ export class ProductDetailsComponent extends AutoUnsub implements OnInit {
 	constructor(
 		private route: ActivatedRoute,
 		private featureSrv: ProductFeatureService,
-		private dlgSrv: DialogService) {
+		private dlgSrv: DialogService,
+		private moduleRef: NgModuleRef<any>) {
 		super();
 	}
 
@@ -46,11 +47,13 @@ export class ProductDetailsComponent extends AutoUnsub implements OnInit {
 
 	/** opens the dialog to add multiple project to product.projects */
 	openAddProjectDlg() {
-		this.dlgSrv.open(ProductAddToProjectDlgComponent, { selectedProducts: [this.product] });
+		this.dlgSrv.openFromModule(ProductAddToProjectDlgComponent, this.moduleRef, { selectedProducts: [this.product] });
 	}
 
 	/** remove project from product.projects */
 	removeProject(project: Project) {
+		// mapping project to their respective id, to not inadvertently change other props, then removing
+		// the project we need to from the array
 		const projects = this.product.projects.map(p => ({ id: p.id })).filter(p => p.id !== project.id);
 		this.featureSrv.update({ id: this.product.id, projects }).subscribe();
 	}
--- src/app/features/products/containers/products-page/products-page.component.ts
+++ src/app/features/products/containers/products-page/products-page.component.ts
@@ -53,25 +53,28 @@ export class ProductsPageComponent extends ListPageComponent<Product, ProductFea
 
 	/** Opens a dialog that lets the user add different products to different projects (many to many) */
 	openAddToProjectDialog(product: Product) {
-		this.dlgSrv.open(ProductAddToProjectDlgComponent, {
-			selectedProducts: product ? [product] : Array.from(this.selectionSrv.selection.values())
+		this.dlgSrv.openFromModule(ProductAddToProjectDlgComponent, this.moduleRef, {
+			selectedProducts: product ? [product] : this.getSelectedProducts()
 		});
 	}
 
 
 	/** Opens a dialog that lets the user export a product either in PDF or EXCEL format */
 	openExportDialog(product: Product) {
-		this.dlgSrv.open(ProductExportDlgComponent, {
-			selectedProducts: product ? [product] : Array.from(this.selectionSrv.selection.values())
+		this.dlgSrv.openFromModule(ProductExportDlgComponent, this.moduleRef, {
+			selectedProducts: product ? [product] : this.getSelectedProducts()
 		});
 	}
 
 	/** Opens a dialog that lets the user request members of his team for feedback regarding the products he selectioned */
 	openRequestFeedbackDialog(product: Product) {
-		this.dlgSrv.open(ProductRequestTeamFeedbackDlgComponent, {
-			selectedProducts: product ? [product] : Array.from(this.selectionSrv.selection.values())
+		this.dlgSrv.openFromModule(ProductRequestTeamFeedbackDlgComponent, this.moduleRef, {
+			selectedProducts: product ? [product] : this.getSelectedProducts()
 		});
 	}
 
+	getSelectedProducts() {
+		return Array.from(this.selectionSrv.selection.values());
+	}
 
 }
