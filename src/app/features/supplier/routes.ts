78dbeba21dfaf8be51c2f8c105d1834a1b369e67
Ced c <cedvandenbosch@gmail.com>
2018-07-23 14:22:11 +0200

    fix faulty rebase ?!!!? #!$

--- src/app/features/supplier/routes.ts
+++ src/app/features/supplier/routes.ts
@@ -1,5 +1,8 @@
 import { Route } from '@angular/router';
 import { SupplierDetailsComponent, SuppliersPageComponent } from '~features/supplier/containers';
+import { SupplierActivityComponent } from '~features/supplier/containers/supplier-activity/supplier-activity.component';
+import { SupplierPublicProfileComponent } from '~features/supplier/containers/supplier-public-profile/supplier-public-profile.component';
+import { SupplierProductsComponent } from '~features/supplier/containers/supplier-products/supplier-products.component';
 
 export const routes: Array<Route> = [
 	{ path: '', redirectTo: 'all', pathMatch: 'full' },
@@ -7,5 +10,11 @@ export const routes: Array<Route> = [
 	{
 		path: 'details/:id',
 		component: SupplierDetailsComponent,
-	},
+		children: [
+			{ path: 'activity', component: SupplierActivityComponent },
+			{ path: 'public-profile', component: SupplierPublicProfileComponent },
+			{ path: 'products', component: SupplierProductsComponent },
+			{ path: '', redirectTo: 'activity', pathMatch: 'full' }
+		]
+	}
 ];
