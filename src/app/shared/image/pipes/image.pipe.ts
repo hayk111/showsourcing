e9358fa594b51c6aafe58e19cdfc4fd9ad62642c
Ced c <cedvandenbosch@gmail.com>
2018-07-23 10:24:54 +0200

    added defaults

--- src/app/shared/image/pipes/image.pipe.ts
+++ src/app/shared/image/pipes/image.pipe.ts
@@ -29,7 +29,7 @@ export class ImagePipe implements PipeTransform {
 	 * @param value : Entity object (like supplier), array or string
 	 * @param args : 's' | 'm' | 'l' | 'xl' size of the image
 	 */
-	transform(value: any | string, size: ('s' | 'm' | 'l' | 'xl'), type: string): string {
+	transform(value: any | string, size: ('s' | 'm' | 'l' | 'xl') = 's', type: string = 'image'): string {
 		try {
 			// no value
 			if (!value)
