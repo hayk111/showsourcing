2adb852989c72eff2a9c61c7c85ae2acdc2157bd
Michael Vanegas <michael@showsourcing.com>
2018-07-25 09:36:28 +0200

    fix

--- src/app/shared/stage-indicator/stage-indicator.component.ts
+++ src/app/shared/stage-indicator/stage-indicator.component.ts
@@ -1,19 +1,19 @@
 import { Component, OnInit, ChangeDetectionStrategy, Input } from '@angular/core';
 
 @Component({
-		selector: 'stage-indicator-app',
-		templateUrl: './stage-indicator.component.html',
-		styleUrls: ['./stage-indicator.component.scss'],
-		changeDetection: ChangeDetectionStrategy.OnPush
+	selector: 'stage-indicator-app',
+	templateUrl: './stage-indicator.component.html',
+	styleUrls: ['./stage-indicator.component.scss'],
+	changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class StageIndicatorComponent implements OnInit {
 
-		@Input() titles: Array<string>;
-		@Input() index: number;
+	@Input() titles: Array<string>;
+	@Input() index: number;
 
-		constructor() { }
+	constructor() { }
 
-		ngOnInit() {
-		}
+	ngOnInit() {
+	}
 
 }
