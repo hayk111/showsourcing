264c5761eddea219b9312126b221a05c97a1b702
Ced c <cedvandenbosch@gmail.com>
2018-07-26 11:32:29 +0200

    rebasing

---
+++ src/app/shared/activity/components/multiple-products-activity-card/multiple-products-activity-card.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit, ChangeDetectionStrategy, Input, Output, EventEmitter } from '@angular/core';
+import { Product } from '~models';
+
+@Component({
+	selector: 'multiple-products-activity-card-app',
+	templateUrl: './multiple-products-activity-card.component.html',
+	styleUrls: ['./multiple-products-activity-card.component.scss'],
+	changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class MultipleProductsActivityCardComponent implements OnInit {
+	@Input() products: Product[];
+	@Output() viewProducts = new EventEmitter<Product[]>();
+	constructor() { }
+
+	ngOnInit() {
+	}
+
+}
