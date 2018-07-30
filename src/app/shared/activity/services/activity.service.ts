78087ae15bf1e216bab461b73639240492dfb2bc
Michael Vanegas <michael@showsourcing.com>
2018-07-30 18:22:55 +0200

    changes styling after rebase

--- src/app/shared/activity/services/activity.service.ts
+++ src/app/shared/activity/services/activity.service.ts
@@ -7,104 +7,104 @@ import { ProductService } from '~global-services';
 
 
 export interface GetStreamResponse {
-  next: string;
-  results: GetStreamResult[];
+	next: string;
+	results: GetStreamResult[];
 }
 
 export interface GetStreamResult {
-  activities: GetStreamActivity[];
-  activity_count: number;
-  actor_count: number;
-  created_at: Date;
-  group: string;
-  id: string;
-  updated_at: Date;
-  verb: string;
-  // observable version of objects we need (added by us)
-  obs: Observable<any>;
+	activities: GetStreamActivity[];
+	activity_count: number;
+	actor_count: number;
+	created_at: Date;
+	group: string;
+	id: string;
+	updated_at: Date;
+	verb: string;
+	// observable version of objects we need (added by us)
+	obs: Observable<any>;
 }
 
 export interface GetStreamActivity {
-  actor: string;
-  foreign_id: string;
-  id: string;
-  object: string;
-  origin: string;
-  target: string;
-  time: Date;
-  verb: string;
+	actor: string;
+	foreign_id: string;
+	id: string;
+	object: string;
+	origin: string;
+	target: string;
+	time: Date;
+	verb: string;
 }
 
 
 
 @Injectable({
-  providedIn: 'root'
+	providedIn: 'root'
 })
 export class ActivityService {
 
-  constructor(
-    private http: HttpClient,
-    private productSrv: ProductService
-  ) { }
+	constructor(
+		private http: HttpClient,
+		private productSrv: ProductService
+	) { }
 
 
-  init() {
+	init() {
 
-  }
+	}
 
-  /**
+	/**
    * Gets the dashboard activity
    */
-  getDashboardActivity(page$: Observable<number>) {
-    const teamId = '2a0ac87c-e1a8-4912-9c0d-2748a4aa9e46';
-    const client = stream.connect('mvufdhfnfz83', null, '39385');
-    // gets feed token
-    return this.http.get<GetStreamResponse>(`https://murmuring-sierra-85015.herokuapp.com/${teamId}`).pipe(
-      // once we have the token we can get a feed
-      switchMap(({ token }: any) => this.getActivityStream(page$, client, teamId, token)),
-      tap((r: any) => this.addData(r.results)),
-      map(r => r.results),
-      scan((pre, curr) => ([...pre, ...curr]), [])
-    );
-  }
+	getDashboardActivity(page$: Observable<number>) {
+		const teamId = '2a0ac87c-e1a8-4912-9c0d-2748a4aa9e46';
+		const client = stream.connect('mvufdhfnfz83', null, '39385');
+		// gets feed token
+		return this.http.get<GetStreamResponse>(`https://murmuring-sierra-85015.herokuapp.com/${teamId}`).pipe(
+			// once we have the token we can get a feed
+			switchMap(({ token }: any) => this.getActivityStream(page$, client, teamId, token)),
+			tap((r: any) => this.addData(r.results)),
+			map(r => r.results),
+			scan((pre, curr) => ([...pre, ...curr]), [])
+		);
+	}
 
-  private getActivityStream(page$, client, teamId, token) {
-    return page$.pipe(
-      switchMap((page: number) => {
-        const teamStream = client.feed('team', teamId, token);
-        // TODO : we use offset but it isn't recommended, we should use id_lt
-        return teamStream.get({ limit: 15, offset: page * 15 });
-      })
-    );
-  }
+	private getActivityStream(page$, client, teamId, token) {
+		return page$.pipe(
+			switchMap((page: number) => {
+				const teamStream = client.feed('team', teamId, token);
+				// TODO : we use offset but it isn't recommended, we should use id_lt
+				return teamStream.get({ limit: 15, offset: page * 15 });
+			})
+		);
+	}
 
-  loadMore(feed: GetStreamResponse) {
-    return this.http.get('https://api.stream-io-api.com' + feed.next).pipe(
-      tap((r: any) => this.addData(r.results))
-    );
-  }
+	loadMore(feed: GetStreamResponse) {
+		return this.http.get('https://api.stream-io-api.com' + feed.next).pipe(
+			tap((r: any) => this.addData(r.results))
+		);
+	}
 
-  /**
+	/**
    *
    * Selects item in db and add those to feed
    */
-  private addData(results: GetStreamResult[]) {
-    results.forEach(res => {
-      res.obs = forkJoin(res.activities.map(act => this.addDataToActivity(act)));
-    });
-  }
+	private addData(results: GetStreamResult[]) {
+		results.forEach(res => {
+			res.obs = forkJoin(res.activities.map(act => this.addDataToActivity(act)));
+		});
+	}
 
-  private addDataToActivity(activity: GetStreamActivity) {
-    switch (activity.verb) {
-      case 'update_product':
-      case 'create_product':
-        return this.productSrv.selectOne(activity.object).pipe(first());
-    }
-  }
+	private addDataToActivity(activity: GetStreamActivity) {
+		switch (activity.verb) {
+			case 'update_product':
+			case 'create_product':
+				return this.productSrv.selectOne(activity.object).pipe(first());
+		}
+	}
 
-  private getLastUuid(feeds: GetStreamResult[]) {
-    const lastIndex = feeds.length - 1;
-    const lastActivities = feeds[lastIndex].activities;
-    return lastActivities[lastActivities.length - 1].id;
-  }
+	private getLastUuid(feeds: GetStreamResult[]) {
+		const lastIndex = feeds.length - 1;
+		const lastActivities = feeds[lastIndex].activities;
+		return lastActivities[lastActivities.length - 1].id;
+	}
 }
