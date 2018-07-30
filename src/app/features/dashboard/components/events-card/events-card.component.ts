3869615d6a3857bc6927489de6431b894a3e9ba8
Ced c <cedvandenbosch@gmail.com>
2018-07-30 16:50:17 +0200

    re-added pagination

--- src/app/features/dashboard/components/events-card/events-card.component.ts
+++ src/app/features/dashboard/components/events-card/events-card.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
 })
 export class EventsCardComponent implements OnInit {
   events = [
-    { startAt: new Date(), endAt: new Date((new Date).getDate() + 1), name: 'Super event', address: 'Guangzhou', country: 'ch' },
+    { startAt: new Date(), endAt: new Date((new Date).getDate() + 1), name: 'Super event', address: 'Guangzhou', country: 'CH' },
     { startAt: new Date(), endAt: new Date((new Date).getDate() + 1), name: 'Better event', address: 'Guangzhou', country: 'ch' }
   ]
   constructor() { }
