3e0bff7d777f1dafd92617913777d57e2c6313a9
Ced c <cedvandenbosch@gmail.com>
2018-08-08 23:01:43 +0200

    changing urls in environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,7 +3,7 @@ import { LogLevel } from '~utils/logger/log-level';
 export const environment = {
 	production: true,
 	graphqlUrl: 'ws://ros-dev3.showsourcing.com:9443/graphql',
-	apiUrl: 'https://ros-dev3.showsourcing.com',
+	apiUrl: 'https://ros-dev3.showsourcing.com:9443',
 	LOG_LEVEL: LogLevel.ERROR,
 };
 
