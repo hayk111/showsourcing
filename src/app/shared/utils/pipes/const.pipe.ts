129ee61177f767fad2d74400fbf9f4f59d44a823
Thierry Templier <templth@gmail.com>
2018-08-16 17:08:58 +0200

    feat(i18n): extended cosnt pipe

--- src/app/shared/utils/pipes/const.pipe.ts
+++ src/app/shared/utils/pipes/const.pipe.ts
@@ -1,5 +1,5 @@
-import { Pipe, PipeTransform } from '@angular/core';
-import { currencyMap, countryMap, harbourMap, incoTermsMap } from '~utils/constants';
+import { LOCALE_ID, Inject, Pipe, PipeTransform } from '@angular/core';
+import { currencyMap, countryMap, harbourMap, incoTermsMap, statusMap } from '~utils/constants';
 
 /**
  * transform a const to it's given name or if any additional param is given the property named with that param
@@ -12,6 +12,16 @@ import { currencyMap, countryMap, harbourMap, incoTermsMap } from '~utils/consta
 	name: 'const'
 })
 export class ConstPipe implements PipeTransform {
+	localeCountry: string;
+
+	constructor(@Inject(LOCALE_ID) private locale: string) {
+		if (locale) {
+			const localeTokens = locale.split('-');
+			if (localeTokens && localeTokens.length > 0) {
+				this.localeCountry = localeTokens[0];
+			}
+		}
+	}
 
 	transform(value: any, ...args: string[]): any {
 		let constMap;
@@ -34,6 +44,10 @@ export class ConstPipe implements PipeTransform {
 			case 'incoTerms':
 				constMap = incoTermsMap;
 				break;
+			case 'status':
+			case 'statuses':
+				constMap = statusMap;
+				break;
 			default: throw Error(`The target ${args[0]} for the const pipe is not a valid const`);
 		}
 		const constObject = constMap[value];
@@ -44,7 +58,35 @@ export class ConstPipe implements PipeTransform {
 		if (!propertyName)
 			return constMap[value].name;
 
-		return constMap[value][propertyName];
+		// Try to translate the value
+		return this.getI18nValue(constMap[value], propertyName, this.localeCountry) || value;
+	}
+
+	/**
+	 * Leverage the const structure to translate the value and display the value if no translation.
+	 *
+	 * The structure of the const structure is the following:
+	 *
+	 * export const statusMap = {
+	 *   '_GetQuotation': {
+	 *     locales: {
+	 *	     en: {
+	 *	       name: 'Get Quotation'
+	 *   },
+	 *   (...)
+	 * };
+	 *
+	 */
+	getI18nValue(obj: any, propertyName: string, localeCountry: string) {
+		const localeValues = obj.locales;
+		if (localeValues) {
+			if (localeCountry && localeValues[localeCountry]) {
+				return localeValues[localeCountry][propertyName];
+			} else if (localeValues.en) {
+				return localeValues.en[propertyName];
+			}
+		}
+		return ''	;
 	}
 
 }
---
+++ src/app/utils/constants/statuses.map.ts
@@ -0,0 +1,30 @@
+export const statusMap = {
+	'_GetQuotation': {
+		locales: {
+			en: {
+				name: 'Get Quotation'
+			}
+		}
+	},
+	'_Refused': {
+		locales: {
+			en: {
+				name: 'Refused'
+			}
+		}
+	},
+	'_TeamReview': {
+		locales: {
+			en: {
+				name: 'Team Review'
+			}
+		}
+	},
+	'_Validated': {
+		locales: {
+			en: {
+				name: 'Validated'
+			}
+		}
+	}
+};
