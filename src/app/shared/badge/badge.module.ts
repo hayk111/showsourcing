c29f3ff916497e0e02fa8f78d08c58194283e07e
Michael Vanegas <michael@showsourcing.com>
2018-08-20 10:20:05 +0200

    pipe translation

--- src/app/shared/badge/badge.module.ts
+++ src/app/shared/badge/badge.module.ts
@@ -3,9 +3,10 @@ import { NgModule } from '@angular/core';
 import { BadgeComponent } from '~shared/badge/components/badge/badge.component';
 import { IconsModule } from '~shared/icons';
 import { StatusBadgeComponent } from '~shared/badge/components/status-badge/status-badge.component';
+import { UtilsModule } from '~shared/utils';
 
 @NgModule({
-	imports: [CommonModule, IconsModule],
+	imports: [CommonModule, IconsModule, UtilsModule],
 	declarations: [BadgeComponent, StatusBadgeComponent],
 	exports: [BadgeComponent, StatusBadgeComponent],
 })
