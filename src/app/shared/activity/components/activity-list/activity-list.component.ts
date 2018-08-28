1a490fea345d0df4855feec1075e68cf33ca618b
Michael Vanegas <michael@showsourcing.com>
2018-08-28 13:47:07 +0200

    first changes one image product

--- src/app/shared/activity/components/activity-list/activity-list.component.ts
+++ src/app/shared/activity/components/activity-list/activity-list.component.ts
@@ -9,52 +9,52 @@ import { takeUntil } from 'rxjs/operators';
 import { AutoUnsub } from '~utils';
 
 @Component({
-  selector: 'activity-list-app',
-  templateUrl: './activity-list.component.html',
-  styleUrls: ['./activity-list.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+	selector: 'activity-list-app',
+	templateUrl: './activity-list.component.html',
+	styleUrls: ['./activity-list.component.scss'],
+	changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ActivityListComponent extends AutoUnsub implements OnInit {
-  @Input() feedName: string[];
-  feeds$: Observable<GetStreamResult[]>;
-  private page$ = new BehaviorSubject(0);
-  private page: number;
+	@Input() feedName: string[];
+	feeds$: Observable<GetStreamResult[]>;
+	private page$ = new BehaviorSubject(0);
+	private page: number;
 
-  constructor(
-    private productSrv: ProductService,
-    private activitySrv: ActivityService,
-    private router: Router,
-    private templateSrv: TemplateService
-  ) {
-    super();
-  }
+	constructor(
+		private productSrv: ProductService,
+		private activitySrv: ActivityService,
+		private router: Router,
+		private templateSrv: TemplateService
+	) {
+		super();
+	}
 
-  ngOnInit() {
-    // getting the feed
-    this.feeds$ = this.activitySrv.getFeed({
-      page$: this.page$,
-      feedName: this.feedName
-    });
+	ngOnInit() {
+		// getting the feed
+		this.feeds$ = this.activitySrv.getFeed({
+			page$: this.page$,
+			feedName: this.feedName
+		});
 
-    // when we reach the bottom of the ** PAGE ** then we load more.
-    this.templateSrv.bottomReached$.subscribe(_ => this.loadMore());
+		// when we reach the bottom of the ** PAGE ** then we load more.
+		this.templateSrv.bottomReached$.subscribe(_ => this.loadMore());
 
-    this.page$.pipe(
-      takeUntil(this._destroy$)
-    ).subscribe(page => this.page = page)
-  }
+		this.page$.pipe(
+			takeUntil(this._destroy$)
+		).subscribe(page => this.page = page);
+	}
 
-  loadMore() {
-    this.page$.next(++this.page);
-  }
+	loadMore() {
+		this.page$.next(++this.page);
+	}
 
-  updateProduct(product: Product) {
-    this.productSrv.update(product).subscribe();
-  }
+	updateProduct(product: Product) {
+		this.productSrv.update(product).subscribe();
+	}
 
-  goToProduct(id: string) {
-    this.router.navigate(['product', 'details', id]);
-  }
+	goToProduct(id: string) {
+		this.router.navigate(['product', 'details', id]);
+	}
 
 
 }
