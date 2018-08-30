abef065a8b6fcb07072b382b6f941d0137fbf7a6
Michael Vanegas <michael@showsourcing.com>
2018-08-30 16:38:41 +0200

    build fix

--- src/app/shared/comment/comment.module.ts
+++ src/app/shared/comment/comment.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CommentComponent, CommentListComponent } from '~shared/comment';
+import { UserPictureModule } from '~shared/user-picture';
 
 @NgModule({
 	imports: [
-		CommonModule
+		CommonModule,
+		UserPictureModule
 	],
 	declarations: [CommentComponent, CommentListComponent],
 	exports: [CommentComponent, CommentListComponent]
