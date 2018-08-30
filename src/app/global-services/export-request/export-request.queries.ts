27af36fd62ea02b1e59cdb03606f97ef070948ef
Thierry Templier <templth@gmail.com>
2018-08-30 19:09:16 +0200

    feat(export): connected pdf export

--- src/app/global-services/export-request/export-request.queries.ts
+++ src/app/global-services/export-request/export-request.queries.ts
@@ -3,4 +3,21 @@ import { GlobalQueries } from '~global-services/_global/global-queries.class';
 
 export abstract class ExportRequestQueries extends GlobalQueries {
 
+	static readonly one = `
+      format,
+      type,
+      query,
+      status,
+      documentUrl,
+      errors
+  `;
+
+	static readonly many = `
+      format,
+      type,
+      query,
+      status,
+      documentUrl,
+      errors
+  `;
 }
