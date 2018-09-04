56d59350b5f40c41671d560ab00cfbef631ef649
Thierry Templier <templth@gmail.com>
2018-09-04 08:28:49 +0200

    feat(workspace): added products kanban

--- src/app/features/workspace/containers/my-products-page/my-products-page.component.ts
+++ src/app/features/workspace/containers/my-products-page/my-products-page.component.ts
@@ -1,10 +1,150 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component, NgModuleRef, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Observable, Subject } from 'rxjs';
+import { map, switchMap, tap, first, takeUntil } from 'rxjs/operators';
+import { WorkspaceWorkflowFeatureService } from '~features/workspace/services/workspace-workflow-feature.service';
+import { ProductService, ProjectService } from '~global-services';
+import { ERM, Product, Project, ProductStatus } from '~models';
+import {
+	ProductAddToProjectDlgComponent,
+	ProductExportDlgComponent,
+	ProductRequestTeamFeedbackDlgComponent,
+} from '~shared/custom-dialog';
+import { DialogService } from '~shared/dialog';
+import { ConfirmDialogComponent } from '~shared/dialog/containers/confirm-dialog/confirm-dialog.component';
+import { SelectionService } from '~shared/list-page/selection.service';
+import { NotificationService, NotificationType } from '~shared/notifications';
+import { AutoUnsub } from '~utils/auto-unsub.component';
+
 
 @Component({
 	selector: 'workspace-my-products-page-app',
 	templateUrl: './my-products-page.component.html',
 	styleUrls: ['./my-products-page.component.scss']
 })
-export class MyProductsPageComponent {
+export class MyProductsPageComponent extends AutoUnsub implements OnInit {
+
+	project$: Observable<Project>;
+	statuses$ = new Subject<ProductStatus[]>();
+	id: string;
+	project: Project;
+	/** keeps tracks of the current selection */
+	selected$: Observable<Map<string, boolean>>;
+
+	constructor(
+		private route: ActivatedRoute,
+		private projectSrv: ProjectService,
+		private productSrv: ProductService,
+		private workflowService: WorkspaceWorkflowFeatureService,
+		private selectionSrv: SelectionService,
+		private cdr: ChangeDetectorRef,
+		protected dlgSrv: DialogService,
+		protected moduleRef: NgModuleRef<any>,
+		protected featureSrv: WorkspaceWorkflowFeatureService,
+		private notifSrv: NotificationService
+	) {
+		super();
+	}
+
+	ngOnInit() {
+		/* this.project$ = this.route.parent.params.pipe(
+			map(params => params.id),
+			tap(id => this.id = id),
+			switchMap(id => this.projectSrv.selectOne(id)),
+			tap(project => this.project = project)
+		);
+
+		this.project$.pipe(
+			takeUntil(this._destroy$),
+			switchMap(project => this.workflowService.getStatuses(project))
+		).subscribe(statuses => {
+			this.statuses$.next(statuses);
+		}); */
+
+		this.workflowService.getStatuses().pipe(
+			takeUntil(this._destroy$)
+		).subscribe(statuses => {
+			this.statuses$.next(statuses);
+		});
+
+		this.selected$ = this.selectionSrv.selection$;
+	}
+
+	onUpdateProductStatus({ target, droppedElement }) {
+		this.workflowService.updateProductStatus(droppedElement, target)
+			.subscribe(() => {
+				this.cdr.detectChanges();
+			});
+	}
+
+	/** Selects a an entity */
+	onItemSelected(entity: any) {
+		this.selectionSrv.selectOne(entity);
+	}
+
+	/** Unselects a entity */
+	onItemUnselected(entity: any) {
+		this.selectionSrv.unselectOne(entity);
+	}
+
+	/** Selects an entity */
+	onAllItemsSelected(entity: any) {
+		this.selectionSrv.selectAll(entity);
+	}
+
+	/** Unselects a entity */
+	onAllItemsUnselected(entity: any) {
+		this.selectionSrv.unselectOne(entity);
+	}
+
+	/**
+	 * Selection bar actions
+	 *
+	 * Each of the actions to open dialog below will open a dialog that is itself a container.
+	 */
+
+	/** Opens a dialog that lets the user add different products to different projects (many to many) */
+	openAddToProjectDialog(product: Product) {
+		this.dlgSrv.openFromModule(ProductAddToProjectDlgComponent, this.moduleRef, {
+			selectedProducts: product ? [product] : this.getSelectedProducts()
+		});
+	}
+
+
+	/** Opens a dialog that lets the user export a product either in PDF or EXCEL format */
+	openExportDialog(product: Product) {
+		this.dlgSrv.openFromModule(ProductExportDlgComponent, this.moduleRef, {
+			selectedProducts: product ? [product] : this.getSelectedProducts()
+		});
+	}
+
+	/** Opens a dialog that lets the user request members of his team for feedback regarding the products he selectioned */
+	openRequestFeedbackDialog(product: Product) {
+		this.dlgSrv.openFromModule(ProductRequestTeamFeedbackDlgComponent, this.moduleRef, {
+			selectedProducts: product ? [product] : this.getSelectedProducts()
+		});
+	}
+
+	/** Will show a confirm dialog to delete items selected */
+	deleteSelected() {
+		const items = Array.from(this.selectionSrv.selection.keys());
+		// callback for confirm dialog
+		const callback = () => {
+			this.productSrv.deleteMany(items).subscribe(() => {
+				this.resetSelection();
+			});
+		};
+		const text = `Delete ${items.length} ${items.length > 1 ? ERM.ITEM.plural : ERM.ITEM.singular} ?`;
+		this.dlgSrv.open(ConfirmDialogComponent, { text, callback });
+	}
+
+	/** Unselect all entity */
+	resetSelection() {
+		this.selectionSrv.unselectAll();
+	}
+
+	getSelectedProducts() {
+		return Array.from(this.selectionSrv.selection.values());
+	}
 
 }
---
+++ src/app/features/workspace/services/workspace-workflow-feature.service.ts
@@ -0,0 +1,95 @@
+import { Injectable } from '@angular/core';
+import { ProductService, ProductStatusTypeService, UserService } from '~global-services';
+import { ProductStatusService } from '~global-services/product-status/product-status.service';
+import { ProductQueries } from '~global-services/product/product.queries';
+import { Observable } from 'rxjs';
+import { SelectParams } from '~global-services/_global/select-params';
+import { of, forkJoin } from 'rxjs';
+import { map, switchMap, tap } from 'rxjs/operators';
+import { Project, Product, ProductStatus, ProductStatusType } from '~models';
+import { Apollo } from 'apollo-angular';
+import { ListQuery } from '~global-services/_global/list-query.interface';
+
+
+@Injectable({
+	providedIn: 'root'
+})
+export class WorkspaceWorkflowFeatureService extends ProductService {
+	productsResult: ListQuery<Product>;
+
+	constructor(
+		protected apollo: Apollo,
+		protected productSrv: ProductService,
+		protected productStatusSrv: ProductStatusService,
+		protected productStatusTypeService: ProductStatusTypeService,
+		protected userSrv: UserService
+	) {
+		super(apollo, userSrv);
+	}
+
+	/**
+	 * Returns the product statuses including the associated products.
+	 *
+	 * The status with the category refused is removed since we don't need
+	 * to display it.
+	 * */
+	getStatuses(refresh = false) {
+		if (refresh && this.productsResult) {
+			this.productsResult.refetch({
+				query: `status.id != null`,
+				sortBy: 'lastUpdatedDate'
+			});
+		}
+
+		if (!this.productsResult) {
+			this.productsResult = this.productSrv.getListQuery({ query: `status.id != null`, sortBy: 'lastUpdatedDate' });
+		}
+		return this.productsResult.items$.pipe(
+			switchMap(products => {
+				return this.productStatusTypeService.queryAll().pipe(
+					// Remove the status with category refused
+					map(statuses => statuses.filter(status => (status.category !== 'refused' && status.category !== 'inspiration'))),
+					map(statuses => ({ statuses, products }))
+				);
+			}),
+			// Add products to the status
+			map(({ products, statuses }) => statuses.map(status => ({
+				...status,
+				products: this.getProductsWithStatus(status, products)
+			}))),
+			// Sort statuses per step
+			map(statuses => statuses.sort((s1, s2) => (s1.step - s2.step)))
+		);
+	}
+
+	/**
+	 * Filter the products list to get only products that have a current status
+	 * and correspond to the provided status.
+	 */
+	getProductsWithStatus(status: ProductStatus, products: Product[]) {
+		return products.filter(product => {
+			const productCurrentStatus = product.status ? product.status.status : null;
+			return (productCurrentStatus && productCurrentStatus.id === status.id);
+		});
+	}
+
+	/**
+	 * Update the status of the product with the specified one.
+	 */
+	updateProductStatus(product: Product, statusType: ProductStatusType) {
+		// we check if the product has a status
+		if (!product.status) {
+			const tempStatus = new ProductStatus({ status: { id: statusType.id } }) as any;
+			return this.update({ id: product.id, status: tempStatus }, [ProductQueries.status]);
+		} else {
+			// we dont update if we click the same status as the current one of the product
+			const productStatusType = product.status.status;
+			if (statusType.id !== productStatusType.id) {
+				const tempStatus = new ProductStatus({ status: { id: statusType.id } }) as any;
+				return this.update({ id: product.id, status: tempStatus }, [ProductQueries.status]);
+			}
+		}
+		return of();
+	}
+
+}
