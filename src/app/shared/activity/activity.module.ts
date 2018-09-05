4410d43cdb91d06712707b1c553afcf77f39c25b
Michael Vanegas <michael@showsourcing.com>
2018-09-05 16:43:07 +0200

    folder relocation, new server addr

--- src/app/shared/activity/activity.module.ts
+++ src/app/shared/activity/activity.module.ts
@@ -1,20 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { OneProductActivityCardComponent } from '~shared/activity/components/one-product-activity-card/one-product-activity-card.component';
+import {
+	OneProductActivityCardComponent
+} from '~shared/activity/components/grouped-feed-list/one-product-activity-card/one-product-activity-card.component';
 import {
 	MultipleProductsActivityCardComponent
-} from '~shared/activity/components/multiple-products-activity-card/multiple-products-activity-card.component';
+} from '~shared/activity/components/grouped-feed-list/multiple-products-activity-card/multiple-products-activity-card.component';
 import { SharedModule } from '~shared/shared.module';
 import { RatingModule } from '~shared/rating';
 import { ActivityUserComponent } from './components/activity-user/activity-user.component';
 import { ActionBarModule } from '~shared/action-bar/action-bar.module';
 import { LoadersModule } from '~shared/loaders';
 import { StatusModule } from '~shared/status/status.module';
-import { OneProductCarouselComponent } from './components/one-product-activity-card/one-product-carousel/one-product-carousel.component';
+import {
+	OneProductCarouselComponent
+} from './components/grouped-feed-list/one-product-activity-card/one-product-carousel/one-product-carousel.component';
 import { CarouselModule } from '~shared/carousel';
 import {
 	MultipleProductCarouselComponent
-} from './components/multiple-products-activity-card/multiple-product-carousel/multiple-product-carousel.component';
+} from './components/grouped-feed-list/multiple-products-activity-card/multiple-product-carousel/multiple-product-carousel.component';
 import { ProductCommonModule } from '~shared/product/product.module';
 import { CommentModule } from '~shared/comment';
 import { GroupedFeedListComponent } from '~shared/activity/components/grouped-feed-list/grouped-feed-list.component';
