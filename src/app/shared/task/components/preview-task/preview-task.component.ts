4b5826284f8b82743f15aabf98ed5adc7bae1e31
TRINH Van Huy <kingofspeed6@gmail.com>
2018-09-10 23:47:10 +0200

    :art: css for task preview panel

--- src/app/shared/task/components/preview-task/preview-task.component.ts
+++ src/app/shared/task/components/preview-task/preview-task.component.ts
@@ -5,10 +5,10 @@ import { FormDescriptor, CustomField } from '~shared/dynamic-forms';
 import { FormGroup } from '@angular/forms';
 import { AutoUnsub } from '~utils';
 import { takeUntil, distinctUntilChanged, map, tap, first } from 'rxjs/operators';
-import { ProductFeatureService } from '~features/products/services';
+import { TaskService } from '~global-services';
 import { DialogService } from '~shared/dialog';
 import { RfqDialogComponent } from '~features/products/components/rfq-dialog/rfq-dialog.component';
-import { Task } from '~models';
+import { Task, Comment } from '~models';
 import { NgModuleRef } from '@angular/core';
 import { Router } from '@angular/router';
 import { ProductAddToProjectDlgComponent } from '~shared/custom-dialog';
@@ -20,52 +20,13 @@ import { ProductAddToProjectDlgComponent } from '~shared/custom-dialog';
 	changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class PreviewTaskComponent extends AutoUnsub implements OnInit {
-	/** This is the product passed as input, but it's not yet fully loaded */
 	@Input() task: Task;
-	@Output() close = new EventEmitter<any>();
-	descriptor$ = new ReplaySubject<FormDescriptor>(1);
-	descriptor2$ = new ReplaySubject<FormDescriptor>(1);
-	/** this is the fully loaded product */
-	product$: Observable<Product>;
-	/** Contacts given a supplier of the product */
-	contacts: Array<Contact>;
-	prodERM = ERM.PRODUCT;
-
-	// those are the custom fields for the first form section
-	// ultimately "sections" should be added to the form descriptor
-	// so we only have one array of custom fields
-	customFields: CustomField[] = [
-		{ name: 'supplier', type: 'selector', metadata: { target: 'supplier', type: 'entity', labelName: 'name', canCreate: true } },
-		{ name: 'category', type: 'selector', metadata: { target: 'category', type: 'entity', labelName: 'name', canCreate: true } },
-		{ name: 'name', type: 'text', required: true, label: 'name' },
-		{ name: 'price', type: 'price' },
-		{ name: 'createdBy', type: 'selector', metadata: { target: 'user', type: 'entity', labelName: 'name' } },
-		{
-			name: 'createdBy', label: 'Assignee', type: 'selector', metadata:
-				{ target: 'user', type: 'entity', labelName: 'name' }
-		},
-		{ name: 'minimumOrderQuantity', type: 'number', label: 'MOQ' },
-		{ name: 'moqDescription', type: 'text', label: 'MOQ description' },
-		{
-			name: 'event', label: 'Found at', type: 'selector',
-			metadata: { target: 'event', type: 'entity', labelName: 'name', canCreate: true }
-		},
-		{ name: 'tags', type: 'selector', metadata: { target: 'tag', type: 'entity', labelName: 'name', canCreate: true }, multiple: true },
-		{ name: 'description', type: 'textarea', label: 'description' },
-	];
-
-	// those are the custom field for the second form section
-	customFields2: CustomField[] = [
-		{ name: 'innerCarton', type: 'packaging', label: 'inner carton' },
-		{ name: 'masterCarton', type: 'packaging', label: 'master carton' },
-		// { name: 'samplePrice', type: 'number', label: 'Sample Price' },
-		{ name: 'priceMatrix', type: 'priceMatrix', label: 'price matrix' },
-		{ name: 'sample', type: 'yesNo' },
-		{ name: 'samplePrice', type: 'number', label: 'Sample Price' },
-	];
+  @Output() close = new EventEmitter<any>();
+  
+  comment$: Observable<Comment>;
 
 	constructor(
-		private featureSrv: ProductFeatureService,
+		private featureSrv: TaskService,
 		private dlgSrv: DialogService,
 		private module: NgModuleRef<any>,
 		private router: Router) {
@@ -73,19 +34,9 @@ export class PreviewTaskComponent extends AutoUnsub implements OnInit {
 	}
 
 	ngOnInit() {
-		// creating the form descriptor
-		// this.product$ = this.featureSrv.selectOne(this.product.id);
-		// this.product$.pipe(
-		// 	takeUntil(this._destroy$),
-		// 	map(product => new FormDescriptor(this.customFields, product)),
-		// ).subscribe(this.descriptor$);
-		// this.product$.pipe(
-		// 	takeUntil(this._destroy$),
-		// 	map(product => new FormDescriptor(this.customFields2, product))
-		// ).subscribe(this.descriptor2$);
-		// this.featureSrv.getContacts(this.product.supplier.id).pipe(
-		// 	takeUntil(this._destroy$)
-		// ).subscribe(supp => this.contacts = supp.contacts);
+    
+    console.log(this.task);
+    
 	}
 
 	/** when we receive back the form from the dynamic form component we subscribe to changes to it and
