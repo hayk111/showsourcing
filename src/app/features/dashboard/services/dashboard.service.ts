07eb0177a81435cf4ca562d04ae3bc6f1f5e6e95
Michael Vanegas <michael@showsourcing.com>
2018-09-19 10:45:47 +0200

    fixes on count

--- src/app/features/dashboard/services/dashboard.service.ts
+++ src/app/features/dashboard/services/dashboard.service.ts
@@ -1,9 +1,10 @@
 import { Injectable } from '@angular/core';
-import { ProductService, SupplierService, TaskService } from '~global-services';
+import { ProductService, SupplierService, TaskService, UserService } from '~global-services';
 import { Observable, forkJoin } from 'rxjs';
 import { map, first } from 'rxjs/operators';
 import { Task } from '~models';
 import { realmDateFormat } from '~utils/realm-date-format.util';
+import { ID } from '~utils/id.utils';
 
 export interface DashboardCounters {
 	productsNeedReview: number;
@@ -18,11 +19,17 @@ export interface DashboardCounters {
 })
 export class DashboardService {
 
+	userId: ID;
+	/** magic number for 2 weeks in miliseconds */
+	twoWeeks = 12096e5;
+	weeksAgo = realmDateFormat(new Date(+new Date - this.twoWeeks));
+
 	constructor(
+		private userSrv: UserService,
 		private productSrv: ProductService,
 		private supplierSrv: SupplierService,
 		private taskSrv: TaskService
-	) { }
+	) { this.userId = this.userSrv.userSync.id; }
 
 	getCounters(): Observable<DashboardCounters> {
 		return forkJoin([
@@ -44,29 +51,43 @@ export class DashboardService {
 
 	getCountProductsNeedReview(): Observable<number> {
 		// products that have no status are the products that need review..
-		return this.productSrv.queryCount('status == NULL && archived == false && deleted == false').pipe(first());
+		return this.productSrv.queryCount('status.id == null && status.status.id == null && archived == false && deleted == false').pipe(first());
 	}
 
 	getCountProductsInWorkflow(): Observable<number> {
-		return this.productSrv.queryCount('status.status.inWorkflow == true && archived == false && deleted == false').pipe(first());
+		return this.productSrv.queryCount(
+			'status.id != null && status.status.inWorkflow == true ' +
+			'AND status.status.category != "inspiration" AND status.status.category != "refused" ' +
+			'&& archived == false && deleted == false'
+		).pipe(first());
 	}
 
 	getCountSupplierUnderAssessment(): Observable<number> {
-		return this.supplierSrv.queryCount('status.status.name == "_UnderAssessment" && deleted == false').pipe(first());
+		return this.supplierSrv.queryCount(
+			'status.status.name == "_UnderAssessment" && archived == false && deleted == false'
+		).pipe(first());
 	}
 
 	getCountTotalTasks(): Observable<number> {
-		const now = realmDateFormat(new Date());
-		return this.taskSrv.queryCount(`dueDate > ${now}`).pipe(first());
+		return this.taskSrv.queryCount(
+			`(dueDate > ${this.weeksAgo} OR dueDate == null)	&& assignee.id == "${this.userId}" && deleted == false`
+		).pipe(first());
 	}
 
 	getCountTasksDone(): Observable<number> {
-		const now = realmDateFormat(new Date());
-		return this.taskSrv.queryCount(`dueDate > ${now} AND done == true`).pipe(first());
+		return this.taskSrv.queryCount(
+			`(dueDate > ${this.weeksAgo} OR dueDate == null) ` +
+			`&& done == true && assignee.id == "${this.userId}"	&& deleted == false`
+		).pipe(first());
 	}
 
 	getFirstFewTasks(): Observable<Task[]> {
-		return this.taskSrv.queryMany({ take: 3, query: 'deleted == false AND done == false', descending: false }).pipe();
+		return this.taskSrv.queryMany({
+			take: 5,
+			query: `(dueDate > ${this.weeksAgo} OR dueDate == null)	` +
+				`&& done == false && assignee.id == "${this.userId}"	&& deleted == false`,
+			descending: false
+		}).pipe();
 	}
 
 	updateTask(task: Task) {
