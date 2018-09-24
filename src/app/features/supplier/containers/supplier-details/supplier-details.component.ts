a7608ad2e1631923e6510bc68497e66f60e7121c
TRINH Van Huy <kingofspeed6@gmail.com>
2018-09-24 16:56:45 +0200

    :bug: fix bug supplier doesn't exist & prodcut doesn't exist, fix getting product 2 times

--- src/app/features/supplier/containers/supplier-details/supplier-details.component.ts
+++ src/app/features/supplier/containers/supplier-details/supplier-details.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, NgModuleRef } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { Router, ActivatedRoute } from '@angular/router';
 import { Observable } from 'rxjs';
 import { map, switchMap, takeUntil, tap } from 'rxjs/operators';
 import { SupplierFeatureService } from '~features/supplier/services/supplier-feature.service';
@@ -7,6 +7,7 @@ import { Contact, Product, Supplier, Attachment } from '~models';
 import { DialogService } from '~shared/dialog';
 import { AutoUnsub } from '~utils';
 import { NewContactDlgComponent } from '~features/supplier/containers/new-contact-dlg/new-contact-dlg.component';
+import { NotificationService, NotificationType } from '~shared/notifications';
 
 @Component({
 	selector: 'supplier-details-app',
@@ -14,27 +15,46 @@ import { NewContactDlgComponent } from '~features/supplier/containers/new-contac
 	styleUrls: ['./supplier-details.component.scss']
 })
 export class SupplierDetailsComponent extends AutoUnsub implements OnInit {
-
-	supplier$: Observable<Supplier>;
+	supplier: Supplier;
 
 	constructor(
 		private route: ActivatedRoute,
+		private router: Router,
 		private featureSrv: SupplierFeatureService,
+		private notifSrv: NotificationService,
 		private dlgSrv: DialogService,
-		private moduleRef: NgModuleRef<any>) {
-		super();
-	}
+		private moduleRef: NgModuleRef<any>
+		) {
+			super();
+		}
 
-	ngOnInit() {
-		const id$ = this.route.params.pipe(
-			map(params => params.id)
-		);
+		ngOnInit() {
+			const id$ = this.route.params.pipe(map(params => params.id));
 
-		this.supplier$ = id$.pipe(
-			switchMap(id => this.featureSrv.selectOne(id)),
-		);
-
-	}
-
-
-}
+			id$.subscribe(id => {
+				this.featureSrv.selectOne(id).subscribe(
+					_supplier => {
+            if (Object.keys(_supplier).length === 0 || !_supplier) {
+              this.notifSrv.add({
+                type: NotificationType.ERROR,
+                title: 'The supplier doesn\'t exist',
+                timeout: 3500
+              });
+              this.router.navigate(['supplier']);
+            } else {
+              this.supplier = _supplier;
+            }
+					},
+					err => {
+            this.notifSrv.add({
+              type: NotificationType.ERROR,
+              title: 'Error',
+              message: 'There is an error, please try again later',
+              timeout: 3500
+            });
+            this.router.navigate(['supplier']);
+					}
+					);
+				});
+			}
+		}
