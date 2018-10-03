9c3a36191e62a437bd4ea3106bb2b32865624a75
TRINH Van Huy <kingofspeed6@gmail.com>
2018-10-03 10:45:35 +0200

    :zap: modify resize image

--- src/app/shared/utils/file.util.ts
+++ src/app/shared/utils/file.util.ts
@@ -1,32 +1,31 @@
 
 export function resizeSizeToLimit(file: File, limit: number = 1000000, resizeFunc?: any ) {
-  if (file.size > limit) {
-    const newImageDiffRatio = 1 - ((file.size - limit) / file.size);
-    const reader  = new FileReader();
-    reader.onload = (e) => {
-      const img = new Image();
-      const canvasCopy = document.createElement('canvas');
-      const copyContext = canvasCopy.getContext('2d');
+	if (file.size > limit) {
+		const newImageDiffRatio = 1 - ((file.size - limit) / file.size);
+		const reader  = new FileReader();
+		reader.onload = (e) => {
+			const img = new Image();
+			const canvasCopy = document.createElement('canvas');
+			const copyContext = canvasCopy.getContext('2d');
 
-      img.onload = function()
-      {
-          const elem = document.createElement('canvas');
-          const ctx = elem.getContext('2d');
-          canvasCopy.width = img.width * newImageDiffRatio;
-          canvasCopy.height = img.height * newImageDiffRatio;
-          copyContext.drawImage(img, 0, 0);
+			img.onload = () => {
+				const elem = document.createElement('canvas');
+				const ctx = elem.getContext('2d');
+				canvasCopy.width = img.width * newImageDiffRatio;
+				canvasCopy.height = img.height * newImageDiffRatio;
+				copyContext.drawImage(img, 0, 0);
 
-          canvas.width = img.width * ratio;
-          canvas.height = img.height * ratio;
-          ctx.drawImage(canvasCopy, 0, 0, canvasCopy.width, canvasCopy.height, 0, 0, canvas.width, canvas.height);
-      };
+				elem.width = img.width * newImageDiffRatio;
+				elem.height = img.height * newImageDiffRatio;
+				ctx.drawImage(canvasCopy, 0, 0, canvasCopy.width, canvasCopy.height, 0, 0, elem.width, elem.height);
+			};
 
-      img.src = reader.result;
-    }
-    reader.readAsDataURL(file);
-  }
-  if (resizeFunc) {
-    return resizeFunc(file);
-  }
-  return file;
+			img.src = reader.result;
+		};
+		reader.readAsDataURL(file);
+	}
+	if (resizeFunc) {
+		return resizeFunc(file);
+	}
+	return file;
 }
