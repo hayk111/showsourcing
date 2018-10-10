b63506bda61982e2c173d217923716393a6f5e90
Michael Vanegas <michael@showsourcing.com>
2018-10-10 14:25:35 +0200

    fixed dropdown orientation

--- src/app/shared/task-common/components/picker-entity-selector/picker-entity-selector.component.ts
+++ src/app/shared/task-common/components/picker-entity-selector/picker-entity-selector.component.ts
@@ -1,4 +1,4 @@
-import { Component, ChangeDetectionStrategy, Input, ViewChild, AfterViewInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, HostBinding, Input, OnInit, ViewChild } from '@angular/core';
 import { User } from '~models/user.model';
 import { PickerService } from '~shared/picker';
 import { SelectorEntityComponent } from '~shared/selectors/components/selector-entity/selector-entity.component';
@@ -7,33 +7,49 @@ import { SelectorEntityComponent } from '~shared/selectors/components/selector-e
 	selector: 'picker-entity-selector-app',
 	templateUrl: './picker-entity-selector.component.html',
 	styleUrls: ['./picker-entity-selector.component.scss'],
-	changeDetection: ChangeDetectionStrategy.OnPush,
-	host: {
-		'[style.top]': 'event.srcElement.offsetTop + \'px\'',
-		'[style.left]': 'event.srcElement.offsetLeft + \'px\'',
-	}
+	changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class PickerEntitySelectorComponent implements AfterViewInit {
+export class PickerEntitySelectorComponent implements OnInit {
 
-	@Input() event: MouseEvent;
+	@HostBinding('style.left')
+	left: string;
+	@HostBinding('style.top')
+	top: string;
+
+	@Input() set event(event: MouseEvent) {
+		// this -114 value is constant since it is only for the styling part
+		this.posX = (event.pageX - event.offsetX - 114); // with the page and offset we find the element clicked position
+		this.left = this.posX + 'px';
+		// same for this -5
+		this.posY = (event.pageY - event.offsetY - 5);
+		this.top = this.posY + 'px';
+	}
 	@Input() callback: Function;
 	@ViewChild(SelectorEntityComponent) selector: SelectorEntityComponent;
 
+	/** user selected from the dropdown */
 	user: User;
+	/** position click horizontally */
+	posX: number;
+	/** position click vertically */
+	posY: number;
 
-	constructor(private pickerSrv: PickerService) { console.log(event); }
+	constructor(private pickerSrv: PickerService) { }
 
-	ngAfterViewInit() {
-		setTimeout(_ => this.selector.selector.open(), 50);
-	}
-
-	close() {
-		this.pickerSrv.close();
+	ngOnInit() {
+		// this is done since we have a bug where the first click won't alter the dropdown position (instead of top is bottom)
+		// and  we have to add a setTimeout, this way we don't. 650 is the "middle" of the page
+		this.selector.selector.ngSelect.dropdownPosition = this.posY > 650 ? 'top' : 'bottom';
+		this.selector.open();
 	}
 
 	updateAssignee(user: User) {
 		this.user = user;
 		this.callback(this.user);
 		this.close();
+	}
+
+	close() {
+		this.pickerSrv.close();
 	}
 }
