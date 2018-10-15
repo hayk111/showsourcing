41bb66592ecc09c9efaba3f16c8fe2b54afeb05d
Michael Vanegas <michael@showsourcing.com>
2018-10-15 10:34:02 +0200

    fix comment PR

--- src/app/features/on-boarding/on-boarding.module.ts
+++ src/app/features/on-boarding/on-boarding.module.ts
@@ -3,12 +3,11 @@ import { NgModule } from '@angular/core';
 
 import { FindBusinessComponent } from './components';
 import { WelcomeComponent } from './components/welcome/welcome.component';
-import { OnBoardingPageComponent } from './containers';
 
 @NgModule({
 	imports: [
 		CommonModule
 	],
-	declarations: [OnBoardingPageComponent, FindBusinessComponent, WelcomeComponent]
+	declarations: [FindBusinessComponent, WelcomeComponent]
 })
 export class OnBoardingModule { }
