0a3ba862fad90d94b7ad2fa3d6c8ed81ed4b5ec2
Ced c <cedvandenbosch@gmail.com>
2018-10-16 11:09:49 +0200

    commit

--- projects/supplier-onboarding/src/app/on-boarding/on-boarding.module.ts
+++ projects/supplier-onboarding/src/app/on-boarding/on-boarding.module.ts
@@ -2,24 +2,19 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { SharedModule } from '~shared/shared.module';
 
-import { FindBusinessComponent } from './components';
-import { AddressComponent } from './components/address/address.component';
-import { BusinessDescriptionComponent } from './components/business-description/business-description.component';
-import { BusinessTypeComponent } from './components/business-type/business-type.component';
-import { CategoryComponent } from './components/category/category.component';
-import { ContactDetailsComponent } from './components/contact-details/contact-details.component';
-import { WelcomeComponent } from './components/welcome/welcome.component';
+import {
+	FindBusinessComponent,
+	WelcomeComponent,
+	AddressComponent,
+	BusinessTypeComponent,
+	CategoryComponent,
+	BusinessDescriptionComponent,
+	ContactDetailsComponent
+} from './components';
 
 @NgModule({
-	imports: [
-		CommonModule,
-		SharedModule
-	],
-	declarations: [
-		FindBusinessComponent, WelcomeComponent,
-		AddressComponent, BusinessTypeComponent,
-		CategoryComponent, BusinessDescriptionComponent,
-		ContactDetailsComponent
-	]
+	imports: [CommonModule, SharedModule],
+	declarations: [],
+	exports: [FindBusinessComponent, WelcomeComponent]
 })
 export class OnBoardingModule { }
