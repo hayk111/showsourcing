f20c8085a2579816ee8cd56fe2bf7df54801e77a
Michael Vanegas <michael@showsourcing.com>
2018-10-16 11:25:46 +0200

    contact details screen

--- projects/supplier-onboarding/src/app/on-boarding/components/contact-details/contact-details.component.ts
+++ projects/supplier-onboarding/src/app/on-boarding/components/contact-details/contact-details.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { Component, OnInit, ChangeDetectionStrategy, ViewChild } from '@angular/core';
+import { InputDirective } from '~shared/inputs';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
 	selector: 'contact-details-app',
@@ -8,9 +11,35 @@ import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
 })
 export class ContactDetailsComponent implements OnInit {
 
-	constructor() { }
+	form: FormGroup;
+	@ViewChild(InputDirective) input: InputDirective;
+
+	constructor(
+		private router: Router,
+		private fb: FormBuilder) { }
 
 	ngOnInit() {
+		this.form = this.fb.group({
+			contactEmail: ['', [Validators.required, Validators.email]],
+			contactPhone: ['', Validators.required],
+			wechat: [''],
+			whatsapp: [''],
+			website: ['']
+		});
 	}
+
+	previousPage() {
+		this.router.navigate(['supplier', 'business-description']);
+	}
+
+	nextPage() {
+		this.router.navigate(['supplier', 'account-creation']);
+	}
+
+	onSubmit() {
+		// stuff
+		this.nextPage();
+	}
+
 
 }
