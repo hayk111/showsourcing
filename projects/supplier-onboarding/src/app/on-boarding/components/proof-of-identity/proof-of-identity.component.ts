11db42f3f57c0de75c7d16bbc034979a5d8c58e9
TRINH Van Huy <kingofspeed6@gmail.com>
2018-10-17 14:56:28 +0200

    :zap: add file

--- projects/supplier-onboarding/src/app/on-boarding/components/proof-of-identity/proof-of-identity.component.ts
+++ projects/supplier-onboarding/src/app/on-boarding/components/proof-of-identity/proof-of-identity.component.ts
@@ -1,48 +1,63 @@
-import { Component, OnInit, ChangeDetectionStrategy, ViewChild, ElementRef } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  ChangeDetectionStrategy,
+  ViewChild,
+  ElementRef
+} from '@angular/core';
 import { Router } from '@angular/router';
 import { Attachment } from '~models';
 import { TrackingComponent } from '~shared/tracking-component/tracking-component';
 
 @Component({
-	selector: 'proof-of-identity-app',
-	templateUrl: './proof-of-identity.component.html',
-	styleUrls: ['./proof-of-identity.component.scss', './../common-boarding.component.scss'],
-	changeDetection: ChangeDetectionStrategy.OnPush
+  selector: 'proof-of-identity-app',
+  templateUrl: './proof-of-identity.component.html',
+  styleUrls: [
+    './proof-of-identity.component.scss',
+    './../common-boarding.component.scss'
+  ],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class ProofOfIdentityComponent extends TrackingComponent implements OnInit {
-	public listFile: Attachment[] = [];
+export class ProofOfIdentityComponent extends TrackingComponent
+  implements OnInit {
+  public listFile: Attachment[] = [];
 
-	/** hidden file input */
-  @ViewChild('inpFile') inpFile: ElementRef;
+  /** hidden file input */
+  @ViewChild('inpFile')
+  inpFile: ElementRef;
 
-	constructor(private router: Router) {
+  constructor(private router: Router) {
     super();
   }
 
-	ngOnInit() {
-	}
+  ngOnInit() {}
 
-	previousPage() {
-		this.router.navigate(['account-creation']);
-	}
-
-	nextPage() {
-		this.router.navigate(['qrcode']);
-	}
-
-	onSubmit() {
-		// stuff
-		this.nextPage();
-  }
-  /** opens the file browser window so the user can select a file he wants to upload */
-	openFileBrowser() {
-		this.inpFile.nativeElement.click();
+  previousPage() {
+    this.router.navigate(['account-creation']);
   }
 
-	/** when adding a new image, by selecting in the file browser or by dropping it on the component */
-	async add(files: Array<File>) {
-		if (files.length === 0) {
-			return;
+  nextPage() {
+    this.router.navigate(['qrcode']);
+  }
+
+  onSubmit() {
+    // stuff
+    this.nextPage();
+  }
+
+  openFileBrowser() {
+    this.inpFile.nativeElement.click();
+  }
+
+  async add(files: Array<File>) {
+    console.log(files);
+    if (files.length === 0) {
+      return;
     }
-	}
+    for (const file of files) {
+      const attachment = new Attachment(file.name, file.size);
+      this.listFile.push(attachment);
+    }
+    console.log(this.listFile);
+  }
 }
