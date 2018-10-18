0c214396d227a568d8914aac4c30f4dfba0f9a45
TRINH Van Huy <kingofspeed6@gmail.com>
2018-10-18 07:17:18 +0200

    :art: done review and send

--- projects/supplier-onboarding/src/app/on-boarding/components/verification/verification.component.ts
+++ projects/supplier-onboarding/src/app/on-boarding/components/verification/verification.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
 import { Router } from '@angular/router';
+import { Attachment } from '~models';
+import { TrackingComponent } from '~shared/tracking-component/tracking-component';
 
 @Component({
 	selector: 'verification-app',
@@ -7,10 +9,25 @@ import { Router } from '@angular/router';
 	styleUrls: ['./verification.component.scss', './../common-boarding.component.scss'],
 	changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class VerificationComponent implements OnInit {
+export class VerificationComponent extends TrackingComponent implements OnInit {
 
-	constructor(private router: Router) { }
+	constructor(private router: Router) {
+		super();
+	}
 
+	public data = {
+		'name': 'Bussiness name',
+		'address': '3 rue de Venise, 69100 Villeurbane',
+		'type': 'Manufacturer',
+		'category': ['Watch', 'Jeweery'],	
+		'description': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+		'attachments': [
+			new Attachment('file 1', 15464),
+			new Attachment('file 2', 534873),
+			new Attachment('file 3', 654564),
+			new Attachment('file 4', 131321),
+			new Attachment('file 5', 85454)]
+	}
 	ngOnInit() {
 	}
 
