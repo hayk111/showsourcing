40a57f131c2d86d332a975b010e782ae048b19f2
TRINH Van Huy <kingofspeed6@gmail.com>
2018-10-21 16:03:19 +0200

    :zap: show confirm dialog & delete file from server

--- projects/supplier-onboarding/src/app/on-boarding/components/proof-of-identity/proof-of-identity.component.ts
+++ projects/supplier-onboarding/src/app/on-boarding/components/proof-of-identity/proof-of-identity.component.ts
@@ -9,6 +9,8 @@ import { Router } from '@angular/router';
 import { TrackingComponent } from '~shared/tracking-component/tracking-component';
 import { OnBoardingService } from '../../services';
 import { Attachment, SupplierClaim } from '~models';
+import { DialogService } from '~shared/dialog';
+import { ConfirmDialogComponent } from '~shared/dialog/containers/confirm-dialog/confirm-dialog.component';
 
 @Component({
   selector: 'proof-of-identity-app',
@@ -29,7 +31,11 @@ export class ProofOfIdentityComponent extends TrackingComponent
   @ViewChild('inpFile')
   inpFile: ElementRef;
 
-  constructor(private router: Router, private onBoardSrv: OnBoardingService) {
+  constructor(
+    private router: Router,
+    private onBoardSrv: OnBoardingService,
+		private dlgSrv: DialogService
+    ) {
     super();
 
     // TODO REMOVE FIXED DATA
@@ -65,7 +71,13 @@ export class ProofOfIdentityComponent extends TrackingComponent
   }
 
   public onDelete(file: Attachment) {
-    this.listFile = this.listFile.filter(x => x !== file);
+    // this.listFile = this.listFile.filter(x => x !== file); 
+		console.log(this.dlgSrv),
+		console.log(ConfirmDialogComponent),
+    this.dlgSrv.open(ConfirmDialogComponent, {
+			text: 'Remove 1 file ?',
+			callback: () => this.onBoardSrv.updateClaim({attachment: this.listFile}).subscribe()
+		});
   }
 
   getAttachmentFromFile(file: File): Attachment {
