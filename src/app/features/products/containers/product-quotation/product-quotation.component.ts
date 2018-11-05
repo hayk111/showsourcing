6a01611fc22051a8e8780f172580a38b2628be37
TRINH Van Huy <kingofspeed6@gmail.com>
2018-11-05 11:28:51 +0100

    :fire: remove unused code

--- src/app/features/products/containers/product-quotation/product-quotation.component.ts
+++ src/app/features/products/containers/product-quotation/product-quotation.component.ts
@@ -27,47 +27,7 @@ import { RfqDialogComponent } from '~shared/custom-dialog';
 export class ProductQuotationComponent extends AutoUnsub implements OnInit {
 	// whether the form is open
 	product$: Observable<Product>;
-	product: Product;
-
-
-	@ViewChild(EditableTextComponent) editable: EditableTextComponent;
-	@ViewChild('txt') textarea: ElementRef;
-
-	// those are the custom fields for the first form section
-	// ultimately "sections" should be added to the form descriptor so we only have one array of custom fields
-	customFields: CustomField[] = [
-		{ name: 'supplier', type: 'selector', metadata: { target: 'supplier', type: 'entity', labelName: 'name', canCreate: true } },
-		{
-			name: 'category', type: 'selector',
-			metadata: { target: 'category', type: 'entity', labelName: 'name', canCreate: true }
-		},
-		{ name: 'name', type: 'text', required: true, label: 'name' },
-		{ name: 'price', type: 'price' },
-		// { name: 'createdBy', type: 'selector', metadata: { target: 'user', type: 'entity', labelName: 'name' } },
-		{
-			name: 'assignee', label: 'Assignee', type: 'selector',
-			metadata: { target: 'user', type: 'entity', labelName: 'name' }
-		},
-		{ name: 'minimumOrderQuantity', type: 'number', label: 'MOQ' },
-		{ name: 'moqDescription', type: 'text', label: 'MOQ description' },
-		// {
-		// 	name: 'event', label: 'Found at', type: 'selector',
-		// 	metadata: { target: 'event', type: 'entity', labelName: 'name', canCreate: true }
-		// },
-		{ name: 'tags', type: 'selector', metadata: { target: 'tag', type: 'entity', labelName: 'name', canCreate: true }, multiple: true },
-
-	];
-	// those are the custom field for the second form section
-	customFields2: CustomField[] = [
-		{ name: 'innerCarton', type: 'packaging', label: 'inner carton' },
-		{ name: 'masterCarton', type: 'packaging', label: 'master carton' },
-		{ name: 'sample', type: 'yesNo' },
-		{ name: 'samplePrice', type: 'price', label: 'Sample Price' },
-		// { name: 'samplePrice', type: 'number', label: 'Sample Price' },
-		{ name: 'priceMatrix', type: 'priceMatrix', label: 'price matrix' },
-	];
-
-	typeEntity = ERM.PRODUCT;
+  product: Product;
 
 	constructor(
 		private route: ActivatedRoute,
@@ -90,24 +50,9 @@ export class ProductQuotationComponent extends AutoUnsub implements OnInit {
 		);
 	}
 
-	updateProduct(product: Product, fields?: string) {
-		product.id = this.product.id;
-		this.srv.update(product).subscribe();
-	}
-
-	updateProductProp(isCancel: boolean, value: any, prop: string) {
-		if (!isCancel) {
-			this.updateProduct({ [prop]: value }, prop);
-		}
-	}
-
 	openRfq() {
 		// we add manually the supplier self email, since it is not on the contacts
 		this.dlgSrv.openFromModule(RfqDialogComponent, this.module, { product: this.product });
-	}
-
-	get priceAmount() {
-		return (this.product && this.product.price) ? (this.product.price.value / 10000) : '-';
 	}
 
 }
