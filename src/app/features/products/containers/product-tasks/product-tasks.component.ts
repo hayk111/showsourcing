bebb6047f88d13f54b38ccb63fd95c3ee60d3003
TRINH Van Huy <van-huy.trinh@ant-tech.eu>
2018-11-06 17:12:49 +0100

    :zap: search task in supplier & product task

--- src/app/features/products/containers/product-tasks/product-tasks.component.ts
+++ src/app/features/products/containers/product-tasks/product-tasks.component.ts
@@ -1,4 +1,9 @@
-import { ChangeDetectionStrategy, Component, NgModuleRef, OnInit } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  NgModuleRef,
+  OnInit
+} from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import { TaskService, UserService } from '~global-services';
 import { DialogService } from '~shared/dialog';
@@ -8,34 +13,58 @@ import { AbstractTaskCommonComponent } from '~shared/task-common/containers/abst
 import { Task } from '~models';
 
 @Component({
-	selector: 'product-tasks-app',
-	templateUrl: './product-tasks.component.html',
-	styleUrls: ['./product-tasks.component.scss'],
-	changeDetection: ChangeDetectionStrategy.OnPush
+  selector: 'product-tasks-app',
+  templateUrl: './product-tasks.component.html',
+  styleUrls: ['./product-tasks.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class ProductTasksComponent extends AbstractTaskCommonComponent implements OnInit {
+export class ProductTasksComponent extends AbstractTaskCommonComponent
+  implements OnInit {
+  constructor(
+    protected route: ActivatedRoute,
+    protected userSrv: UserService,
+    protected router: Router,
+    protected featureSrv: TaskService,
+    protected searchSrv: SearchService,
+    protected selectionSrv: SelectionService,
+    protected dlgSrv: DialogService,
+    protected moduleRef: NgModuleRef<any>
+  ) {
+    super(
+      router,
+      userSrv,
+      featureSrv,
+      searchSrv,
+      selectionSrv,
+      dlgSrv,
+      moduleRef
+    );
+  }
 
-	constructor(
-		protected route: ActivatedRoute,
-		protected userSrv: UserService,
-		protected router: Router,
-		protected featureSrv: TaskService,
-		protected searchSrv: SearchService,
-		protected selectionSrv: SelectionService,
-		protected dlgSrv: DialogService,
-		protected moduleRef: NgModuleRef<any>) {
-		super(router, userSrv, featureSrv, searchSrv, selectionSrv, dlgSrv, moduleRef);
-	}
+  ngOnInit() {
+    super.ngOnInit();
+    this.filterList.addFilter({
+      type: FilterType.PRODUCT,
+      value: this.route.parent.snapshot.params.id
+    });
+  }
 
-	ngOnInit() {
-		super.ngOnInit();
-		this.filterList.addFilter({ type: FilterType.PRODUCT, value: this.route.parent.snapshot.params.id });
-	}
+  createTask(name: string) {
+    const newTask = new Task({
+      name,
+      product: { id: this.route.parent.snapshot.params.id }
+    });
+    this.featureSrv.create(newTask).subscribe();
+    this.refetch();
+  }
 
-	createTask(name: string) {
-		const newTask = new Task({ name, product: { id: this.route.parent.snapshot.params.id } });
-		this.featureSrv.create(newTask).subscribe();
-		this.refetch();
-	}
+  search(str: string) {
+    // TODO, POSSIBLE SEARCHING FULL NAME ASSIGNEE
+    this.currentSearch = str
+      ? `name CONTAINS[c] "${str}"` +
+        ` OR supplier.name CONTAINS[c] "${str}"` +
+        ` OR product.name CONTAINS[c] "${str}"`
+      : '';
+    this.onPredicateChange();
+  }
 }
-
