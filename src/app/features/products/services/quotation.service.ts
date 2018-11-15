ea8de8acb7f6c507742a8cc760fb612472385572
TRINH Van Huy <van-huy.trinh@ant-tech.eu>
2018-11-15 17:25:32 +0100

    :zap: add quote service

--- src/app/features/products/services/quotation.service.ts
+++ src/app/features/products/services/quotation.service.ts
@@ -1,22 +1,30 @@
 import { Injectable } from '@angular/core';
-import { QuoteService, SupplierService, UserService } from '~global-services';
+import {
+  QuoteService,
+  SupplierService,
+  UserService,
+  QuoteQueries
+} from '~global-services';
 import { SupplierQueries } from '~global-services/supplier/supplier.queries';
 import { ApolloStateService } from '~shared/apollo';
 import { Product, Quote } from '~models';
 import { Observable } from 'apollo-link';
+import { first } from 'rxjs/operators';
 
 @Injectable()
 export class QuoteFeatureService extends QuoteService {
-
-	constructor(
-		protected apolloState: ApolloStateService,
-		private quoteService: QuoteService,
-		protected userSrv: UserService
-	) {
-		super(apolloState, userSrv);
+  constructor(
+    protected apolloState: ApolloStateService,
+    private quoteService: QuoteService,
+    protected userSrv: UserService
+  ) {
+    super(apolloState, userSrv);
   }
 
-  getQuotationFromProducts(products: any): Observable<Quote[]> {
-    return null;
+  getQuotationFromProducts(productIds: any): Observable<Quote[]> {
+    return this.queryMany(
+      { query: `${productIds} CONTAINS product.id` },
+      QuoteQueries.many
+    ).pipe(first());
   }
 }
