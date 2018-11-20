744e3e343bc6d0cd6b51a021d01c4a59c4e2aff8
Michael Vanegas <michael@showsourcing.com>
2018-11-20 13:33:59 +0100

    samples raw design, model, service

--- src/app/shared/card/components/card-header/card-header.component.ts
+++ src/app/shared/card/components/card-header/card-header.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit, ChangeDetectionStrategy, Input, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit, ChangeDetectionStrategy, Input, Output, EventEmitter } from '@angular/core';
+import { Sample } from '~models';
 
 @Component({
 	selector: 'card-header-app',
@@ -7,6 +8,17 @@ import { Component, OnInit, ChangeDetectionStrategy, Input, ViewEncapsulation } 
 	changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class CardHeaderComponent {
+
+	@Input() samples: Sample[];
+	@Input() selection: Map<string, any>;
+	@Input() hasProduct = false;
+	@Input() hasSupplier = false;
+	@Output() bottomReached = new EventEmitter<null>();
+	@Output() createSample = new EventEmitter<string>();
+	@Output() previewClicked = new EventEmitter<Sample>();
+	@Output() taskSelect = new EventEmitter<Sample>();
+	@Output() taskUnselect = new EventEmitter<Sample>();
+	@Output() updateSample = new EventEmitter<Sample>();
 
 	constructor() { }
 
