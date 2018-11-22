775a034a8d477b28e41007914afa21a75503681d
TRINH Van Huy <van-huy.trinh@ant-tech.eu>
2018-11-22 16:05:10 +0100

    :zap: show price matrix

--- src/app/shared/custom-dialog/component/compare-quotation/item-comapre-column/item-comapre-column.component.ts
+++ src/app/shared/custom-dialog/component/compare-quotation/item-comapre-column/item-comapre-column.component.ts
@@ -1,4 +1,11 @@
-import { ChangeDetectionStrategy, Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
+import {
+	ChangeDetectionStrategy,
+	Component,
+	Input,
+	OnInit,
+	Output,
+	EventEmitter
+} from '@angular/core';
 import { Product, Packaging, Quote } from '~models';
 import { TrackingComponent } from '~shared/tracking-component/tracking-component';
 @Component({
@@ -7,23 +14,32 @@ import { TrackingComponent } from '~shared/tracking-component/tracking-component
 	styleUrls: ['./item-comapre-column.component.scss'],
 	changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class ItemCompareColumnComponent extends TrackingComponent implements OnInit {
-
-	@Input() type: ('title' | 'content') = 'content';
+export class ItemCompareColumnComponent extends TrackingComponent
+	implements OnInit {
+	@Input() type: 'title' | 'content' = 'content';
 	@Input() product: Product;
 	@Input() quote: Quote;
-	@Input() maxLengthPrixMatrix = 0;
+	@Input() priceMatrixLabels = [];
 
 	constructor() {
 		super();
 	}
 
-	ngOnInit() { }
+	ngOnInit() {}
 
 	getPackagingString(packaging: Packaging): string {
 		if (!packaging) {
 			return '';
 		}
-		return `${packaging.width || 0} x ${packaging.height || 0} x ${packaging.depth || 0}${packaging.unit}`;
+		return `${packaging.width || 0} x ${packaging.height ||
+			0} x ${packaging.depth || 0}${packaging.unit}`;
 	}
- }
+
+	getPriceMatrixRowByLabel(_label: string) {
+		const label = _label.toLowerCase();
+		return (
+			this.quote.priceMatrix.rows.find(x => x.label.toLowerCase() === label)
+				.price || '-'
+		);
+	}
+}
