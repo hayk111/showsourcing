3f7aaf2d68009d83434889e97f010257b5ca9060
Michael Vanegas <michael@showsourcing.com>
2018-11-30 12:15:02 +0100

    WEB-904 dynamic form styling and divider

--- src/app/core/entity-services/sample/sample.queries.ts
+++ src/app/core/entity-services/sample/sample.queries.ts
@@ -2,4 +2,44 @@ import { GlobalQueries } from '~entity-services/_global/global-queries.class';
 
 export class SampleQueries extends GlobalQueries {
 
+	static readonly images = `images { id, orientation, imageType, urls { url } }`;
+	static readonly createdBy = `createdBy { id, firstName, lastName, avatar { id, urls { url } } }`;
+	static readonly assignee = `assignee { id, firstName, lastName, avatar { id, urls { url } }}`;
+	static readonly supplier = `supplier {
+		id, name, address, country, favorite, officeEmail,
+		logoImage { id, urls { url } }
+		categories { id, name }
+		images { id, urls { url }, imageType }
+	}`;
+	static readonly product = `product {
+		id, name, ${SampleQueries.images}, favorite, description,
+	}`;
+	static readonly price = (name = 'price') => `${name} { id, currency, value } `;
+
+	static readonly one = `
+		name,
+		description
+		creationDate,
+		deleted,
+		${SampleQueries.product}
+		${SampleQueries.supplier}
+		${SampleQueries.images}
+		${SampleQueries.price()}
+		${SampleQueries.createdBy}
+		${SampleQueries.assignee}
+	`;
+
+	static readonly many = `
+		name,
+		description
+		creationDate,
+		deleted,
+		${SampleQueries.product}
+		${SampleQueries.supplier}
+		${SampleQueries.images}
+		${SampleQueries.price()}
+		${SampleQueries.createdBy}
+		${SampleQueries.assignee}
+	`;
+
 }
