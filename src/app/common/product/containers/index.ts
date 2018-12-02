9763411af021d87129da8f6cf1eeeef03e80a649
TRINH Van Huy <van-huy.trinh@ant-tech.eu>
2018-12-02 19:20:29 +0100

    :zap: update product preview

--- src/app/common/product/containers/index.ts
+++ src/app/common/product/containers/index.ts
@@ -1,2 +1,3 @@
 export * from './find-products-dialog/find-products-dialog.component';
 export * from './product-preview/product-preview.component';
+export * from './product-preview/price-with-quantity/price-with-quantity.component';
