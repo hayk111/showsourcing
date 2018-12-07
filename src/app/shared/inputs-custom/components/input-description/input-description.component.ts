4c387e65bb0fd11eb4c8f9e4430a06b525baa493
Michael Vanegas <michael@showsourcing.com>
2018-12-07 19:39:41 +0100

    show all button

--- src/app/shared/inputs-custom/components/input-description/input-description.component.ts
+++ src/app/shared/inputs-custom/components/input-description/input-description.component.ts
@@ -1,22 +1,36 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter, ElementRef, ViewChild, AfterViewInit, Renderer2 } from '@angular/core';
 
 @Component({
 	selector: 'input-description-app',
 	templateUrl: './input-description.component.html',
 	styleUrls: ['./input-description.component.scss']
 })
-export class InputDescriptionComponent implements OnInit {
+export class InputDescriptionComponent implements AfterViewInit {
 	@Input() description = '';
 	@Input() hasLabel = false;
 	@Output() update = new EventEmitter<string>();
-	constructor() { }
 
-	ngOnInit() {
+	@ViewChild('container') container: ElementRef<HTMLElement>;
+
+	showMore = false;
+
+	constructor(private render: Renderer2) { }
+
+	ngAfterViewInit() {
+		if (this.container.nativeElement.clientHeight > 85 && !this.showMore) {
+			this.render.setStyle(this.container.nativeElement, 'height', '85px');
+			this.showMore = true;
+		}
 	}
 
 	updateDescription(isCancel: boolean = true, newDescription: string) {
 		if (!isCancel) {
 			this.update.emit(newDescription);
 		}
+	}
+
+	showAll() {
+		this.render.setStyle(this.container.nativeElement, 'height', '100%');
+		this.showMore = false;
 	}
 }
