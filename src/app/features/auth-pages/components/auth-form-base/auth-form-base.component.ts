5221ee1119531c1c44476ec7ee4bf2652cedcb19
TRINH Van Huy <van-huy.trinh@ant-tech.eu>
2018-12-12 16:47:53 +0100

    :bug: fix project creat by

--- src/app/features/auth-pages/components/auth-form-base/auth-form-base.component.ts
+++ src/app/features/auth-pages/components/auth-form-base/auth-form-base.component.ts
@@ -9,65 +9,18 @@ import { Router, ActivatedRoute } from '@angular/router';
 import { UserService } from '~entity-services';
 
 @Component({
-	selector: 'registration-app',
-	templateUrl: './registration.component.html',
-	styleUrls: ['./registration.component.scss', '../form-style.scss']
+	selector: 'auth-form-app',
+	templateUrl: './auth-form.component.html',
+	styleUrls: ['./auth-form.component.scss', '../form-style.scss']
 })
-export class RegistrationComponent extends AutoUnsub implements OnInit {
-	form: FormGroup;
-	pending$ = new Subject<boolean>();
-	error: string;
-	queryParams: any;
-	filedFocused = 'firstName';
-
+export class AuthFormComponent extends AutoUnsub implements OnInit {
+	@Input() pending = false;
 	constructor(
-		private authSrv: AuthenticationService,
-		private fb: FormBuilder,
-		private router: Router,
-		private route: ActivatedRoute,
-		private userSrv: UserService
 	) {
 		super();
 	}
 
 	ngOnInit() {
-		this.queryParams = this.route.snapshot.queryParams || '/';
-
-		this.form = this.fb.group({
-			firstName: ['', Validators.required],
-			lastName: ['', Validators.required],
-			email: [
-				'',
-				Validators.compose([Validators.required, Validators.email])
-			],
-			password: [
-				'',
-				Validators.compose([Validators.required, Validators.minLength(8)])
-			]
-		});
-	}
-
-	createAccount() {
-		if (this.form.valid) {
-			this.pending$.next(true);
-			this.authSrv.register(this.form.value).pipe(
-				takeUntil(this._destroy$),
-				take(1),
-			).subscribe(
-				r => this.router.navigateByUrl(this.queryParams.returnUrl),
-				e => this.onError(e)
-			);
-		}
-	}
-
-	onError(error) {
-		this.pending$.next(false);
-		if (error.error && error.error.errors && error.error.errors.email) {
-			this.form.get('email').markAsPristine();
-			this.error = 'An account already exist with this email address.';
-		} else {
-			this.error = error.error.message;
-		}
 	}
 }
 
