6f12451c4fff0e4b3caf4b0184afdecb3bc790a8
TRINH Van Huy <van-huy.trinh@ant-tech.eu>
2018-12-17 07:02:14 +0100

    :zap: create status box

--- src/app/shared/badge/badge.module.ts
+++ src/app/shared/badge/badge.module.ts
@@ -3,12 +3,13 @@ import { NgModule } from '@angular/core';
 import { BadgeComponent } from '~shared/badge/components/badge/badge.component';
 import { IconsModule } from '~shared/icons';
 import { StatusBadgeComponent } from '~shared/badge/components/status-badge/status-badge.component';
+import { StatusBoxComponent } from '~shared/badge/components/status-box/status-box.component';
 import { UtilsModule } from '~shared/utils';
 import { InfoBadgeComponent } from './components/info-badge/info-badge.component';
 
 @NgModule({
 	imports: [CommonModule, IconsModule, UtilsModule],
-	declarations: [BadgeComponent, StatusBadgeComponent, InfoBadgeComponent],
-	exports: [BadgeComponent, StatusBadgeComponent, InfoBadgeComponent],
+	declarations: [BadgeComponent, StatusBadgeComponent, StatusBoxComponent, InfoBadgeComponent],
+	exports: [BadgeComponent, StatusBadgeComponent, StatusBoxComponent, InfoBadgeComponent],
 })
 export class BadgeModule { }
