cf79d5d9fa2f53de3a94d6246c5dbca4136a7fe1
TRINH Van Huy <van-huy.trinh@ant-tech.eu>
2018-12-17 07:02:14 +0100

    :zap: create status box

---
+++ src/app/shared/badge/components/status-box/status-box.component.ts
@@ -0,0 +1,49 @@
+import { ChangeDetectionStrategy, Component, Input, OnInit } from '@angular/core';
+import { EntityMetadata, SampleStatus, SupplierStatus, ProductStatus } from '~models';
+
+@Component({
+	selector: 'status-box-app',
+	templateUrl: './status-box.component.html',
+	styleUrls: ['./status-box.component.scss'],
+	changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class StatusBoxComponent implements OnInit {
+
+	@Input() size = 's';
+
+	@Input() status: ProductStatus | SupplierStatus | SampleStatus;
+	@Input() round = true;
+
+	// we need to pass this so when the
+	// status is null, because the product or supplier are new
+	@Input() typeEntity: EntityMetadata;
+	// if we display the caret down or not
+	@Input() hasArrow = false;
+	@Input() type: ('list' | 'badge') = 'badge';
+
+	constructor() {
+	}
+
+	ngOnInit() {
+
+	}
+
+	get getType() {
+		// by default is secondary since is the color for NEW elements
+		if (this.status) {
+			switch (this.status.category) {
+				case 'inProgress':
+					return 'in-progress';
+				case 'validated':
+					return 'success';
+				case 'refused':
+					return 'warn';
+				case 'inspiration':
+					return 'secondary-light';
+				default:
+					return 'secondary';
+			}
+		}
+	}
+
+}
