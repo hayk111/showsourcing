f8e1d0ea93207bf2888fbd45c3e39b823edcd7c0
Michael Vanegas <michael@showsourcing.com>
2018-12-17 17:28:12 +0100

    description fix section

--- src/app/shared/inputs-custom/components/input-description/input-description.component.ts
+++ src/app/shared/inputs-custom/components/input-description/input-description.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit, Input, Output, EventEmitter, ElementRef, ViewChild, 
 export class InputDescriptionComponent implements AfterViewInit {
 	@Input() description = '';
 	@Input() hasLabel = false;
+	@Input() isBig = false;
 	@Output() update = new EventEmitter<string>();
 
 	@ViewChild('container') container: ElementRef<HTMLElement>;
@@ -23,8 +24,8 @@ export class InputDescriptionComponent implements AfterViewInit {
 	updateDescription(isCancel: boolean = true, newDescription: string) {
 		if (!isCancel) {
 			this.update.emit(newDescription);
-			this.adaptSize();
 		}
+		this.adaptSize();
 	}
 
 	showAll() {
@@ -36,6 +37,9 @@ export class InputDescriptionComponent implements AfterViewInit {
 		if (this.container.nativeElement.clientHeight > 85 && !this.showMore) {
 			this.render.setStyle(this.container.nativeElement, 'height', '85px');
 			this.showMore = true;
+		} else {
+			this.render.setStyle(this.container.nativeElement, 'height', '100%');
+			this.showMore = false;
 		}
 	}
 }
