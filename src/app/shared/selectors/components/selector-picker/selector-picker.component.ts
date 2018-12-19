9946caf522a4dd9c3b4dca75bdd038924b0feddc
Michael Vanegas <michael@showsourcing.com>
2018-12-19 09:54:25 +0100

    scroll fix and styling

--- src/app/shared/selectors/components/selector-picker/selector-picker.component.ts
+++ src/app/shared/selectors/components/selector-picker/selector-picker.component.ts
@@ -90,6 +90,7 @@ export class SelectorPickerComponent extends AbstractInput implements OnInit, Af
 			name: ['']
 		});
 		this.choicesLocal = this.getChoicesLocal(this.type, this.searchTxt);
+		this.scd.deregister(this.cdkVirtualScrollViewport);
 	}
 	ngAfterViewInit() {
 		this.keyManager = new ActiveDescendantKeyManager(this.virtualItems).withWrap().withTypeAhead();
