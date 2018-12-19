63689d5093d0c3b44fd2c0b72d915e232cbcfdbc
cedric8 <cedric@showsourcing.com>
2018-12-19 12:11:32 +0100

    fix build 2

--- src/environments/environment.loc.ts
+++ src/environments/environment.loc.ts
@@ -2,6 +2,7 @@ import { LogLevel } from '~utils/logger/log-level';
 
 export const environment = {
 	production: false,
+	staging: false,
 	graphqlUrl: 'ws://localhost:9080/graphql',
 	apiUrl: 'http://localhost',
 	LOG_LEVEL: LogLevel.DEBUG,
