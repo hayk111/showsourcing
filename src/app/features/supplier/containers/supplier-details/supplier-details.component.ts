b36ba5b9bd02f50ef505648e44f10ab580d1d79d
TRINH Van Huy <van-huy.trinh@ant-tech.eu>
2018-12-21 11:56:21 +0100

    :zap: fix routing

--- src/app/features/supplier/containers/supplier-details/supplier-details.component.ts
+++ src/app/features/supplier/containers/supplier-details/supplier-details.component.ts
@@ -1,11 +1,23 @@
-import { Component, NgModuleRef, OnInit } from '@angular/core';
+import {
+	Component,
+	OnInit,
+	ChangeDetectionStrategy,
+	NgModuleRef
+} from '@angular/core';
+import { Observable } from 'rxjs';
 import { ActivatedRoute, Router } from '@angular/router';
-import { map, switchMap, takeUntil } from 'rxjs/operators';
-import { SupplierFeatureService } from '~features/supplier/services/supplier-feature.service';
-import { Supplier } from '~models';
-import { DialogService } from '~shared/dialog/services';
-import { NotificationService, NotificationType } from '~shared/notifications';
+import { Supplier } from '~models/supplier.model';
+import { Contact } from '~models/contact.model';
+import { Product, Attachment } from '~models';
 import { AutoUnsub } from '~utils';
+import { SupplierFeatureService } from '~features/supplier/services/supplier-feature.service';
+import { DialogService } from '~shared/dialog/services';
+import { takeUntil, map, switchMap, tap } from 'rxjs/operators';
+import { NewContactDlgComponent } from '~features/supplier/containers/new-contact-dlg/new-contact-dlg.component';
+import { ActivityService } from '~common/activity/services/activity.service';
+import { ActivityFeed } from '~common/activity/interfaces/client-feed.interfaces';
+import { NotificationService, NotificationType } from '~shared/notifications';
+
 import { TabModel } from '~shared/navbar';
 
 @Component({
@@ -16,13 +28,21 @@ import { TabModel } from '~shared/navbar';
 export class SupplierDetailsComponent extends AutoUnsub implements OnInit {
 	supplier: Supplier;
 
-	tabs: TabModel[] = [{
-		title: 'Activity',
-		link: 'tasks'
-	}, {
-		title: 'Products',
-		link: 'products'
-	}];
+	supplier$: Observable<Supplier>;
+	products$: Observable<Product[]>;
+	contacts$: Observable<Contact[]>;
+	feedResult: ActivityFeed;
+
+	tabs: TabModel[] = [
+		{
+			title: 'Activity',
+			link: 'tasks'
+		},
+		{
+			title: 'Products',
+			link: '../products'
+		}
+	];
 
 	constructor(
 		private route: ActivatedRoute,
@@ -30,6 +50,7 @@ export class SupplierDetailsComponent extends AutoUnsub implements OnInit {
 		private featureSrv: SupplierFeatureService,
 		private notifSrv: NotificationService,
 		private dlgSrv: DialogService,
+		private activitySrv: ActivityService,
 		private moduleRef: NgModuleRef<any>
 	) {
 		super();
@@ -38,12 +59,28 @@ export class SupplierDetailsComponent extends AutoUnsub implements OnInit {
 	ngOnInit() {
 		const id$ = this.route.params.pipe(map(params => params.id));
 
-		id$.pipe(
-			switchMap((id: string) => this.featureSrv.selectOne(id)),
-			takeUntil(this._destroy$)
-		).subscribe(
-			supplier => this.onSupplier(supplier),
-			err => this.onError(err)
+		id$
+			.pipe(
+				switchMap((id: string) => this.featureSrv.selectOne(id)),
+				takeUntil(this._destroy$)
+			)
+			.subscribe(
+				supplier => this.onSupplier(supplier),
+				err => this.onError(err)
+			);
+
+		this.supplier$ = id$.pipe(
+			switchMap(id => this.featureSrv.selectOne(id)),
+			tap(supplier => (this.supplier = supplier))
+		);
+
+		// getting his products
+		this.products$ = id$.pipe(switchMap(id => this.featureSrv.getProducts(id)));
+
+		this.contacts$ = id$.pipe(switchMap(id => this.featureSrv.getContacts(id)));
+
+		this.feedResult = this.activitySrv.getSupplierFeed(
+			this.route.parent.snapshot.params.id
 		);
 	}
 
