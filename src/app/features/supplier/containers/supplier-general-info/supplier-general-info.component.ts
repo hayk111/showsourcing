fe87cd4768028f375b75a745a2fc59637d4fb144
TRINH Van Huy <van-huy.trinh@ant-tech.eu>
2018-12-21 11:25:37 +0100

    :zap: problem routing

--- src/app/features/supplier/containers/supplier-general-info/supplier-general-info.component.ts
+++ src/app/features/supplier/containers/supplier-general-info/supplier-general-info.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, ChangeDetectionStrategy, NgModuleRef } from '@angular/core';
 import { Observable } from 'rxjs';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Supplier } from '~models/supplier.model';
 import { Contact } from '~models/contact.model';
 import { Product, Attachment } from '~models';
@@ -11,19 +11,17 @@ import { takeUntil, map, switchMap, tap } from 'rxjs/operators';
 import { NewContactDlgComponent } from '~features/supplier/containers/new-contact-dlg/new-contact-dlg.component';
 import { ActivityService } from '~common/activity/services/activity.service';
 import { ActivityFeed } from '~common/activity/interfaces/client-feed.interfaces';
+import { NotificationService, NotificationType } from '~shared/notifications';
 
 import { TabModel } from '~shared/navbar';
 
 @Component({
 	selector: 'supplier-general-info-app',
 	templateUrl: './supplier-general-info.component.html',
-	styleUrls: ['./supplier-general-info.component.scss'],
-	changeDetection: ChangeDetectionStrategy.OnPush,
-	host: {
-		class: 'flex'
-	}
+	styleUrls: ['./supplier-general-info.component.scss']
 })
 export class SupplierGeneralInfoComponent extends AutoUnsub implements OnInit {
+
 	supplier: Supplier;
 	supplier$: Observable<Supplier>;
 	products$: Observable<Product[]>;
@@ -35,14 +33,16 @@ export class SupplierGeneralInfoComponent extends AutoUnsub implements OnInit {
 		link: 'tasks'
 	}, {
 		title: 'Products',
-		link: 'products'
+		link: '../products'
 	}];
 
 	constructor(
 		private route: ActivatedRoute,
+		private router: Router,
 		private featureSrv: SupplierFeatureService,
 		private dlgSrv: DialogService,
 		private activitySrv: ActivityService,
+		private notifSrv: NotificationService,
 	) {
 		super();
 	}
@@ -55,22 +55,29 @@ export class SupplierGeneralInfoComponent extends AutoUnsub implements OnInit {
 			map(params => params.id),
 		);
 
+		id$.pipe(
+			switchMap((id: string) => this.featureSrv.selectOne(id)),
+			takeUntil(this._destroy$)
+		).subscribe(
+			supplier => this.onSupplier(supplier),
+			err => this.onError(err)
+		);
 		// getting supplier
-		this.supplier$ = id$.pipe(
-			switchMap(id => this.featureSrv.selectOne(id)),
-			tap(supplier => this.supplier = supplier)
-		);
+		// this.supplier$ = id$.pipe(
+		// 	switchMap(id => this.featureSrv.selectOne(id)),
+		// 	tap(supplier => this.supplier = supplier)
+		// );
 
-		// getting his products
-		this.products$ = id$.pipe(
-			switchMap(id => this.featureSrv.getProducts(id))
-		);
+		// // getting his products
+		// this.products$ = id$.pipe(
+		// 	switchMap(id => this.featureSrv.getProducts(id))
+		// );
 
-		this.contacts$ = id$.pipe(
-			switchMap(id => this.featureSrv.getContacts(id))
-		);
+		// this.contacts$ = id$.pipe(
+		// 	switchMap(id => this.featureSrv.getContacts(id))
+		// );
 
-		this.feedResult = this.activitySrv.getSupplierFeed(this.route.parent.snapshot.params.id);
+		// this.feedResult = this.activitySrv.getSupplierFeed(this.route.parent.snapshot.params.id);
 	}
 
 	/** updates supplier */
@@ -95,5 +102,28 @@ export class SupplierGeneralInfoComponent extends AutoUnsub implements OnInit {
 	onFileRemoved(attachment: Attachment) {
 		const attachments = this.supplier.attachments.filter(atc => atc.id !== attachment.id);
 		this.featureSrv.update({ id: this.supplier.id, attachments }).subscribe();
+	}
+
+	onSupplier(supplier) {
+		if (!supplier) {
+			this.notifSrv.add({
+				type: NotificationType.ERROR,
+				title: 'The supplier doesn\'t exist',
+				timeout: 3500
+			});
+			this.router.navigate(['supplier']);
+		} else {
+			this.supplier = supplier;
+		}
+	}
+
+	onError(err) {
+		this.notifSrv.add({
+			type: NotificationType.ERROR,
+			title: 'Error',
+			message: 'There is an error, please try again later',
+			timeout: 3500
+		});
+		this.router.navigate(['supplier']);
 	}
 }
