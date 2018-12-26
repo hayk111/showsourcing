c10723c887bcc2b6bb277f361076adb1e55bbdfa
TRINH Van Huy <van-huy.trinh@ant-tech.eu>
2018-12-26 11:18:23 +0100

    :fire: remove product card

--- src/app/features/products/product.module.ts
+++ src/app/features/products/product.module.ts
@@ -11,7 +11,6 @@ import {
 	ProductSubInfoComponent,
 	ProjectCardComponent,
 	SelectionActionsComponent,
-	ProductCardComponent,
 	ProductListComponent,
 	ProductInformationComponent
 } from '~features/products/components';
