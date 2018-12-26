c0befabadb62e176c45524b39b8d00fc7dcee349
TRINH Van Huy <van-huy.trinh@ant-tech.eu>
2018-12-26 11:19:34 +0100

    :fire: remove unused code

--- src/app/common/comment/components/comment-list/comment-list.component.ts
+++ src/app/common/comment/components/comment-list/comment-list.component.ts
@@ -22,7 +22,6 @@ export class CommentListComponent extends TrackingComponent implements OnInit {
 	}
 
 	ngOnInit() {
-		// this._sorByProperty(this.comments, 'creationDate', this.order === 'asc');
 		this.comments = this.order === 'asc' ? this.comments : this.comments.reverse();
 		if (this.comments && this.comments.length > 0) {
 			this.amountShown = this.comments.length;
@@ -35,37 +34,5 @@ export class CommentListComponent extends TrackingComponent implements OnInit {
 	 */
 	showMore() {
 		this.amountShown = this.amountShown >= 2 ? this.amountShown - 2 : 0;
-	}
-
-	private _getProperty(propertyName: string, object: any ) {
-		const parts = propertyName.split( '.' );
-		const length = parts.length;
-		let  property = object || '';
-		for ( let i = 0; i < length; i++ ) {
-			property = property[parts[i]];
-		}
-		return property;
-	}
-
-	private _sorByProperty(arr: any[], propName: string, isOrderASC: boolean): any[]  {
-		const that = this;
-		if (typeof that._getProperty(propName, arr[0]) === 'string') {
-			arr.sort(function (a, b) {
-				const valueA = that._getProperty(propName, a);
-				const valueB = that._getProperty(propName, b);
-				return valueA.localeCompare(valueB, 'en', {ignorePunctuation: true, sensitivity: 'base'});
-			});
-		} else {
-			arr.sort((a, b) => {
-				const valueA = that._getProperty(propName, a);
-				const valueB = that._getProperty(propName, b);
-				return valueA - valueB;
-			});
-		}
-
-		if (!isOrderASC) {
-			arr.reverse();
-		}
-		return arr;
 	}
 }
