16e18fccac89d1f4b3d70791072ae1803bcf20d5
TRINH Van Huy <van-huy.trinh@ant-tech.eu>
2018-12-26 11:18:23 +0100

    :fire: remove product card

--- src/app/features/products/components/index.ts
+++ src/app/features/products/components/index.ts
@@ -2,6 +2,5 @@ export * from '~features/products/components/product-icons/product-icons.compone
 export * from '~features/products/components/product-sub-info/product-sub-info.component';
 export * from '~features/products/components/selection-actions/selection-actions.component';
 export * from '~features/products/components/project-card/project-card.component';
-export * from '~features/products/components/product-card/product-card.component';
 export * from '~features/products/components/product-list/product-list.component';
 export * from '~features/products/components/product-information/product-information.component';
