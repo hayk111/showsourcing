422d2715f4ab0361cb3c2c23a55d0a3cc737a6d2
Michael Vanegas <michael@showsourcing.com>
2019-01-28 16:51:23 +0100

    first test count

--- src/app/core/entity-services/supplier/supplier.queries.ts
+++ src/app/core/entity-services/supplier/supplier.queries.ts
@@ -12,6 +12,8 @@ export abstract class SupplierQueries extends GlobalQueries {
 	static readonly attachments = `attachments { id, fileName, url, size }`;
 	// tslint:disable-next-line:max-line-length
 	static readonly productsCount = `productsCount:  _count(type: "Product", field: "supplier.id", query:"archived == false AND deleted == false")`;
+	// tslint:disable-next-line:max-line-length
+	// static readonly productsCount = `products: _LinkingObjects(objectType: "Product" property:"supplier" query:"archived == false AND deleted == false") { ... on ProductCollection { count }}`;
 	static readonly comments = `comments { id, text, ${SupplierQueries.createdBy}, creationDate }`;
 
 	static readonly one = `
--- src/app/core/entity-services/tag/tag.queries.ts
+++ src/app/core/entity-services/tag/tag.queries.ts
@@ -6,6 +6,11 @@ export abstract class TagQueries extends GlobalQueries {
 	// tslint:disable-next-line:max-line-length
 	static readonly productsCount = `productsCount:  _count(type: "Product", field: "tags.id", query:"archived == false AND deleted == false")`;
 	static readonly suppliersCount = `suppliersCount:  _count(type: "Supplier", field: "tags.id", query:"deleted == false")`;
+	// tslint:disable-next-line:max-line-length
+	// static readonly productsCount = `products: _LinkingObjects(objectType: "Product" property:"tag" query:"archived == false AND deleted == false") { ... on ProductCollection { count }}`;
+	// tslint:disable-next-line:max-line-length
+	// static readonly suppliersCount = `suppliers: _LinkingObjects(objectType: "Supplier" property:"tag" query:"deleted == false") { ... on SupplierCollection { count }}`;
+
 
 	static readonly many = `
 		name,
