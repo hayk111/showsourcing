1f187aaec00d3b4eaaf5c804031ef9f12cba60a4
cedric8 <cedric@showsourcing.com>
2019-02-11 11:02:00 +0100

    placeholder

--- src/app/shared/workflow-managament-table/workflow-managament-table.module.ts
+++ src/app/shared/workflow-managament-table/workflow-managament-table.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { WorkflowManagamentTableComponent } from './workflow-managament-table.component';
+import { DragDropModule } from '@angular/cdk/drag-drop';
 
 @NgModule({
 	declarations: [WorkflowManagamentTableComponent],
 	imports: [
-		CommonModule
+		CommonModule,
+		DragDropModule
 	],
 	exports: [WorkflowManagamentTableComponent]
 })
