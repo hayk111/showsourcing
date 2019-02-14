fdeee7e92a4ce6d8352710c9949d6ccb282f9e93
cedric8 <cedric@showsourcing.com>
2019-02-14 13:59:20 +0100

    fixing routing

--- src/app/features/invitation/routes.ts
+++ src/app/features/invitation/routes.ts
@@ -2,5 +2,8 @@ import { Routes } from '@angular/router';
 import { HandleInvitationComponent } from '~features/invitation/components';
 
 export const routes: Routes = [
-	{ path: ':id/accept-invite', component: HandleInvitationComponent }
+	{
+		path: ':id/accept-invite',
+		component: HandleInvitationComponent,
+	}
 ];
