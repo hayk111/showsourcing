5c64f1fb0e923de6ae22f7d143e6fdc0c4d24e6c
Michael Vanegas <michael@showsourcing.com>
2019-03-06 15:01:43 +0100

    fixed custom field, cancel extendded fields desc, event selector display fix

--- src/app/features/products/routes.ts
+++ src/app/features/products/routes.ts
@@ -22,7 +22,7 @@ export const routes: Array<Route> = [
 		component: ProductDetailsComponent,
 		data: { skipScrollTop: true },
 		children: [
-			{ path: '', redirectTo: 'shipping', pathMatch: 'full', data: { skipScrollTop: true } },
+			{ path: '', redirectTo: 'activity', pathMatch: 'full', data: { skipScrollTop: true } },
 			{ path: 'shipping', component: ProductShippingComponent, data: { skipScrollTop: true } },
 			{ path: 'activity', component: ProductActivityComponent, data: { skipScrollTop: true } },
 			{ path: 'samples', component: ProductSamplesComponent, data: { skipScrollTop: true } },
