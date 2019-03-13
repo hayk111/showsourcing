72a524860d374cd504ec11f80b70e362057676e4
Michael Vanegas <michael@showsourcing.com>
2019-03-13 16:54:28 +0100

    Inputs, Outputs and basic html

--- src/app/common/product/components/product-selection-bar/product-selection-bar.component.ts
+++ src/app/common/product/components/product-selection-bar/product-selection-bar.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { Component, OnInit, EventEmitter, ChangeDetectionStrategy, Input, Output } from '@angular/core';
+import { Project } from '~core/models';
 
 @Component({
 	selector: 'product-selection-bar-app',
@@ -7,6 +8,19 @@ import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
 	changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ProductSelectionBarComponent implements OnInit {
+
+	@Input() isShown = false;
+	@Input() selection: Map<string, boolean>;
+	@Output() close = new EventEmitter<null>();
+	@Output() compareProducts = new EventEmitter<null>();
+	@Output() export = new EventEmitter<null>();
+	@Output() deleted = new EventEmitter<null>();
+	@Output() addProject = new EventEmitter<Project[]>();
+	@Output() liked = new EventEmitter<boolean>();
+	@Output() disliked = new EventEmitter<boolean>();
+	@Output() favorited = new EventEmitter<null>();
+	@Output() unfavorited = new EventEmitter<null>();
+	@Output() statusUpdated = new EventEmitter<any>();
 
 	constructor() { }
 
