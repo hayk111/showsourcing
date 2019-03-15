3d35f6d0f559e6eb7f51c60baa9d026646846c4c
Michael Vanegas <michael@showsourcing.com>
2019-03-15 12:12:11 +0100

    fixed script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -15,25 +15,22 @@ ENDPOINT=$2
 BUILD="npm run build"
 REGION="eu-central-1" # default region for app.show.sourcing.com
 
-CHOICES[1]="app-test.showsourcing.com"
-CHOICES[2]="app-dev.showsourcing.com"
-CHOICES[3]="app2.showsourcing.com"
-CHOICES[4]="app-sta.showsourcing.com"
-CHOICES[5]="app.showsourcing.com"
+CHOICES[1]="app-dev.showsourcing.com"
+CHOICES[2]="app-sta.showsourcing.com"
+CHOICES[3]="app.showsourcing.com"
 
 
 if [ -z "$ENDPOINT" ]; then
 	echo "Please pick an endpoint to deploy to"
-	for I in 1 2 3 4 5
+	for I in 1 2 3
 	do
 		echo "$I) ${CHOICES[$I]}"
 	done
 	read n
 	ENDPOINT=${CHOICES[n]}
 	case $n in
-		2) BUILD="npm run build:dev" REGION="us-east-2";;
-		3) BUILD="npm run build:dev" REGION="us-east-2";;
-		4) BUILD="npm run build:sta" REGION="eu-west-1";;
+		1) BUILD="npm run build:dev" REGION="us-east-2";;
+		2) BUILD="npm run build:sta" REGION="eu-west-1";;
 	esac
 	[ -z "$ENDPOINT" ] && echo "Invalid endpoint" && exit 0
 fi
