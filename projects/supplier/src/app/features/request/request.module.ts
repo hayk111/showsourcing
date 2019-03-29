205d80b00f61852611cb6bbc94dbd7d68403ed96
Michael Vanegas <michael@showsourcing.com>
2019-03-29 15:35:42 +0100

    added request elemetn details page

--- projects/supplier/src/app/features/request/request.module.ts
+++ projects/supplier/src/app/features/request/request.module.ts
@@ -5,13 +5,14 @@ import { RequestCommonModule } from '~common/request';
 import { SharedModule } from '~shared/shared.module';
 
 import { RequestListViewComponent } from './components';
-import { RequestDetailsComponent, RequestsPageComponent } from './containers';
+import { RequestDetailsComponent, RequestsPageComponent, RequestElementDetailsComponent } from './containers';
 import { routes } from './routes';
 
 
 @NgModule({
 	declarations: [
 		RequestDetailsComponent,
+		RequestElementDetailsComponent,
 		RequestListViewComponent,
 		RequestsPageComponent,
 	],
