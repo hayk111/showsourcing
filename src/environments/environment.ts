f8c9740445e15e44e3b1c97fe1d0765fa8f1ecde
Michael Vanegas <michael@showsourcing.com>
2019-03-29 16:03:35 +0100

    default client replace filename

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,6 @@
 import { LogLevel } from '~utils/logger/log-level';
 import { WEB_VERSION } from './global.const';
+import { Client } from '~core/apollo/services/apollo-client-names.const';
 
 // The file contents for the current environment will overwrite these during build.
 // The build system defaults to the dev environment which uses `environment.ts`, but if you do
@@ -16,7 +17,8 @@ export const environment = {
 	getStreamKey: 'aner534ygtg9',
 	mixPanelKey: '9143fc0c3d674a93d201e8d9e12fb4f9',
 	hubspotKey: '5511311',
-	LOG_LEVEL: LogLevel.DEBUG
+	LOG_LEVEL: LogLevel.DEBUG,
+	defaultClient: Client.TEAM
 };
 
 
