7b7cb7c5177c392977030a9e2f2ed205e9ec60ac
Michael Vanegas <michael@showsourcing.com>
2019-04-02 12:51:37 +0200

    added component request sorting sup

---
+++ projects/supplier/src/app/features/request/components/request-sorting-menu/request-sorting-menu.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+	selector: 'request-sorting-menu-sup',
+	templateUrl: './request-sorting-menu.component.html',
+	styleUrls: ['./request-sorting-menu.component.scss']
+})
+export class RequestSortingMenuComponent implements OnInit {
+
+	constructor() { }
+
+	ngOnInit() {
+	}
+
+}
