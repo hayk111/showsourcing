cf71a7bf48fcae785a09bc9d57f8bb1b997f9e8f
Michael Vanegas <michael@showsourcing.com>
2019-04-09 11:40:07 +0200

    fixed buyer request element list view, reply fields display

--- src/app/features/requests/components/request-element-list-view/request-element-list-view.component.ts
+++ src/app/features/requests/components/request-element-list-view/request-element-list-view.component.ts
@@ -1,6 +1,6 @@
 import { ChangeDetectionStrategy, Component, EventEmitter, Input, Output, TemplateRef, ViewChild } from '@angular/core';
 import { ListViewComponent } from '~core/list-page';
-import { ExtendedField, RequestElement } from '~core/models';
+import { ExtendedField, RequestElement, Price } from '~core/models';
 
 @Component({
 	selector: 'request-element-list-view-app',
@@ -16,10 +16,22 @@ export class RequestElementListViewComponent extends ListViewComponent<RequestEl
 	private _rows: Array<RequestElement>;
 	@Input() set rows(rows: Array<RequestElement>) {
 		this._rows = rows;
-		let firstRow, reply;
-		if (rows && (firstRow = rows[0]) && (reply = firstRow.reply) && reply.fields && reply.fields.length)
-			this.fields = rows[0].reply.fields;
-		else
+		this.prices = [];
+		if (rows) {
+			// we map all the fields so we can iterate them on the column loop
+			this.fields = rows.map(row => {
+				// we need to map all the fields, since we use the index to access the price in case if there is any, we need the order
+				// in case the field is not type price will be just undefined inside the matrix
+				this.prices.push(row.reply.fields.map(field => {
+					if (field.definition.type === 'price') {
+						return field.value ? JSON.parse(field.value) : {};
+					}
+				}));
+				return row.reply.fields;
+			});
+			console.log(this.fields);
+			console.log(this.prices);
+		} else
 			this.fields = [];
 	}
 	get rows() {
@@ -28,7 +40,11 @@ export class RequestElementListViewComponent extends ListViewComponent<RequestEl
 	@Output() openRequestDlg = new EventEmitter<null>();
 
 	@ViewChild('contextualMenu') contextualMenuTemplate: TemplateRef<any>;
-	fields: ExtendedField[];
+
+	// both are matrix since we need to iterate over requestElement and then over requestElement.reply.fields
+	// M[i][j] -> i: request element rows // j: fields
+	fields: ExtendedField[][];
+	prices: Price[][];
 
 	constructor() { super(); }
 
