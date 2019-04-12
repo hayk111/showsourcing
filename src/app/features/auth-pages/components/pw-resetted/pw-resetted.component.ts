f4b77a8b574c7ab76e9fbfd813b38b26780f6aef
cedric <cedric@showsourcing.com>
2019-04-12 18:20:59 +0200

    reset password

--- src/app/features/auth-pages/components/pw-resetted/pw-resetted.component.ts
+++ src/app/features/auth-pages/components/pw-resetted/pw-resetted.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
 	selector: 'pw-resetted-app',
 	templateUrl: './pw-resetted.component.html',
 	styleUrls: ['./pw-resetted.component.scss']
 })
-export class PwResettedComponent implements OnInit {
-	@Output() back = new EventEmitter<any>();
-	constructor() { }
+export class PwResettedComponent {
 
-	ngOnInit() {
+	constructor(private router: Router) { }
+
+	goLogin() {
+		this.router.navigate(['auth', 'login']);
 	}
 
 }
