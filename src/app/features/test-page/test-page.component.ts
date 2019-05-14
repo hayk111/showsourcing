a8e5ca81da75d762962c78cdb61538ba7cdfdab2
Michael Vanegas <michael@showsourcing.com>
2019-05-14 15:16:13 +0200

    removed test page

--- src/app/features/test-page/test-page.component.ts
+++ src/app/features/test-page/test-page.component.ts
@@ -1,8 +1,4 @@
 import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
-import { ExtendedField, Product, ExtendedFieldDefinition } from '~core/models';
-import { ProductService } from '~core/entity-services';
-import { ExtendedFieldDefinitionService } from '~core/entity-services/extended-field-definition/extended-field-definition.service';
 
 @Component({
 	selector: 'app-test-page',
@@ -12,17 +8,9 @@ import { ExtendedFieldDefinitionService } from '~core/entity-services/extended-f
 })
 export class TestPageComponent implements OnInit {
 
-	definitions: Observable<Product>;
-	fieldDefinitions$: Observable<ExtendedFieldDefinition[]>;
-
-	constructor(private productSrv: ProductService, private exSrv: ExtendedFieldDefinitionService) { }
+	constructor() { }
 
 	ngOnInit() {
-		this.fieldDefinitions$ = this.exSrv.queryMany(
-			{ query: 'target == "Product.extendedField" OR target == "product.harbour" OR target == "product.incoterm"' }
-		);
-		this.definitions = this.productSrv.queryOne('47668cbe-e34a-4172-9afa-a549e698dcee');
-		this.definitions.subscribe(p => console.log(p));
 	}
 
 }
