2e353eaa50c45004b5935b6849c0345fd7d5ac0d
Michael Vanegas <michael@showsourcing.com>
2019-05-14 18:30:55 +0200

    added search parameters to request

--- src/app/features/requests/containers/request-page/request-page.component.ts
+++ src/app/features/requests/containers/request-page/request-page.component.ts
@@ -31,7 +31,7 @@ export class RequestPageComponent extends AutoUnsub implements OnInit {
 		this.listSrv.setup({
 			key: ListPageKey.REQUEST,
 			entitySrv: this.requestSrv,
-			searchedFields: [],
+			searchedFields: ['title', 'message', 'recipient.name', 'recipient.email', 'recipient.company', 'templateName', 'requestElements.name'],
 			entityMetadata: ERM.SUPPLIER_REQUEST,
 			initialFilters: [],
 			originComponentDestroy$: this._destroy$,
