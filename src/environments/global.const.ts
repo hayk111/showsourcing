1207740742eed3a2aab37379667e5c7a6c34aab2
Michael Vanegas <michael@showsourcing.com>
2019-06-04 10:54:57 +0200

    build 2.4.0-11

--- src/environments/global.const.ts
+++ src/environments/global.const.ts
@@ -1,2 +1,2 @@
-// this file was updated on Mon May 27 2019 14:31:10 GMT+0200 (Romance Daylight Time)
-export const WEB_VERSION = '2.4.0-10';
+// this file was updated on Tue Jun 04 2019 10:52:21 GMT+0200 (Romance Daylight Time)
+export const WEB_VERSION = '2.4.0-11';
