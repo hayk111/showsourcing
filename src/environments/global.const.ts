6866236f1da33439efd0782fcc2d941de6019ccf
Michael Vanegas <michael@showsourcing.com>
2019-06-19 10:27:09 +0200

    fixed env sta

--- src/environments/global.const.ts
+++ src/environments/global.const.ts
@@ -1,2 +1,2 @@
-// this file was updated on Mon Jun 17 2019 13:11:29 GMT+0200 (Romance Daylight Time)
-export const WEB_VERSION = '2.4.0-24';
+// this file was updated on Wed Jun 19 2019 10:23:03 GMT+0200 (Romance Daylight Time)
+export const WEB_VERSION = '2.4.0-26';
