0a7bf5a8ca0785c9b7e8b2fbb5c051bdbc85036b
Michael Vanegas <michael@showsourcing.com>
2019-06-24 11:12:07 +0200

    version 2.5.0-2

--- src/environments/global.const.ts
+++ src/environments/global.const.ts
@@ -1,2 +1,2 @@
-// this file was updated on Fri Jun 21 2019 16:58:49 GMT+0200 (Central European Summer Time)
-export const WEB_VERSION = '2.5.0-1';
+// this file was updated on Mon Jun 24 2019 11:10:17 GMT+0200 (Central European Summer Time)
+export const WEB_VERSION = '2.5.0-2';
