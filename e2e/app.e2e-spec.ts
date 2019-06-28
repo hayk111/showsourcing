3647502de4a27ea72edbf9271e31fda0e6688e1a
Co NGUYEN <phuc-co.nguyenmai@ant-tech.eu>
2019-06-28 15:54:07 +0700

    add test-suite-flow (except step 3)

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -4,4 +4,5 @@ import './pages/auth/forgot-password/forgot-password.e2e-spec';
 import './pages/auth/select-team/select-team.e2e-spec';
 import './pages/auth/create-a-team/create-a-team.e2e-spec';
 import './pages/auth/change-password/change-password.e2e-spec';
+import './pages/auth/flow/flow.e2e-spec';
 
