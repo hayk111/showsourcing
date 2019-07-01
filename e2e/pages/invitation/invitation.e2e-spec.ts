cb11a01785a69c4fc5a1dacbda815ec0b76f450e
Co NGUYEN <phuc-co.nguyenmai@ant-tech.eu>
2019-07-01 17:21:26 +0700

    test suite invitation

--- e2e/pages/invitation/invitation.e2e-spec.ts
+++ e2e/pages/invitation/invitation.e2e-spec.ts
@@ -8,7 +8,14 @@ describe('test suite invitation', () => {
 	let pageLogin: LoginPage;
 	let invitationPage: InvitationPage;
 
-	beforeEach(async () => {
+	// beforeEach(async () => {
+	// });
+
+	afterEach(async () => {
+		await browser.restart();
+	});
+
+	it('should send invitation when creating invitation on the team management page', async () => {
 		pageChangePw = new ChangePasswordPage();
 		pageLogin = new LoginPage();
 		invitationPage = new InvitationPage();
@@ -36,21 +43,109 @@ describe('test suite invitation', () => {
 			await pageChangePw.completeOnBoarding();
 		}
 
-	});
-
-	afterEach(async () => {
-		await browser.restart();
-	});
-
-	it('should send invitation when creating invitation on the team management page', async () => {
 		const inviteBtn = await invitationPage.inviteBtn();
 		await inviteBtn.click();
 
-		await invitationPage.inviteUser('phuc-co.nguyenmai@ant-tech.eu');
+		await invitationPage.inviteUser('ss.test1.anttech@gmail.com'); // this email doesn't have
 
 		browser.sleep(1000);
 
 		const isSuccess = await invitationPage.isNotiSuccess();
 		return expect(isSuccess).toBe(true);
 	});
+
+	it('should ask to login / register when going on the invitation link as a new user', async () => {
+		const mailListener = await invitationPage.startMailListener('ss.test1.anttech@gmail.com', 'AntTech2017');
+		if (mailListener) {
+			const mail: any = await browser.controlFlow().wait(_ => {
+				return invitationPage.getLastEmail(mailListener);
+			}, 10000);
+
+			if (mail) {
+				const headers = mail.headers;
+				if (headers.senderemail === 'info@showsourcing.com') {
+					expect(headers.senderemail).toContain('info@showsourcing.com');
+
+					const hrefs = mail.html.toString().match(/<a.*?>/g);
+
+					if (hrefs && hrefs.length) {
+						const link = hrefs[0].match(/href="([^"]*)/);
+						if (link[1] && link[1].length) {
+							await invitationPage.navigateToLink(link[1]);
+
+							const isUrlInvitation = await browser.driver.wait(async _ => {
+								const url: string = await browser.driver.getCurrentUrl();
+								return /accept-invite/.test(url);
+							}, 10000);
+
+							if (isUrlInvitation) {
+								const btns = await invitationPage.btnOfInvitationApp;
+								await invitationPage.stopMailListener;
+								return expect(btns.length).toBeGreaterThan(1);
+							} else {
+								fail('can not go to page accept-invite');
+							}
+
+						}
+					} else {
+						fail('can not get hrefs');
+					}
+				} else {
+					await invitationPage.stopMailListener;
+					fail('failed to get email of info@showsourcing.com');
+				}
+			} else {
+				await invitationPage.stopMailListener;
+				fail('can not get email');
+			}
+		} else {
+			await invitationPage.stopMailListener;
+			fail('can not start mailListener');
+		}
+	});
+
+	it('should get on the invitation acceptation page when going on the invitation link as an existing user', async () => {
+		const mailListener = await invitationPage.startMailListener('ss.test1.anttech@gmail.com', 'AntTech2017');
+		if (mailListener) {
+			const mail: any = await browser.controlFlow().wait(_ => {
+				return invitationPage.getLastEmail(mailListener);
+			}, 10000);
+
+			if (mail) {
+				const headers = mail.headers;
+				if (headers.senderemail === 'info@showsourcing.com') {
+					expect(headers.senderemail).toContain('info@showsourcing.com');
+
+					const hrefs = mail.html.toString().match(/<a.*?>/g);
+
+					if (hrefs && hrefs.length) {
+						const link = hrefs[0].match(/href="([^"]*)/);
+						if (link[1] && link[1].length) {
+							await invitationPage.navigateToLink(link[1]);
+
+							const isUrlInvitation = await browser.driver.wait(async _ => {
+								const url: string = await browser.driver.getCurrentUrl();
+								return /accept-invite/.test(url);
+							}, 10000);
+
+							await invitationPage.stopMailListener;
+							return expect(isUrlInvitation).toBeGreaterThan(1);
+						}
+					} else {
+						fail('can not get hrefs');
+					}
+				} else {
+					await invitationPage.stopMailListener;
+					fail('failed to get email of info@showsourcing.com');
+				}
+			} else {
+				await invitationPage.stopMailListener;
+				fail('can not get email');
+			}
+		} else {
+			await invitationPage.stopMailListener;
+			fail('can not start mailListener');
+		}
+	});
+
 });
--- e2e/pages/invitation/invitation.po.ts
+++ e2e/pages/invitation/invitation.po.ts
@@ -1,9 +1,14 @@
-import { browser, by, element, By } from 'protractor';
-
+import { browser, by, element, protractor } from 'protractor';
+declare function require(name: string);
+const MailListener = require('mail-listener2');
 export class InvitationPage {
 
 	navigateToManageTeams() {
 		return browser.get('/settings/team/members');
+	}
+
+	navigateToLink(link: string) {
+		return browser.get(link);
 	}
 
 	async inviteUser(email: string) {
@@ -18,7 +23,7 @@ export class InvitationPage {
 			if (await btn.isDisplayed()) {
 				await btn.click();
 			} else {
-				console.log('can not get btn Invite of ialog-footer-app');
+				console.log('can not get btn Invite of dialog-footer-app');
 			}
 		} else {
 			console.log('can not get email inp of invite-user-dlgapp');
@@ -35,6 +40,11 @@ export class InvitationPage {
 
 	get notificationApp() {
 		return browser.driver.findElement(by.tagName('notification-app'));
+	}
+
+	get btnOfInvitationApp() {
+		const handleInvitationApp = browser.driver.findElement(by.tagName('handle-invitation-app'));
+		return handleInvitationApp.findElements(by.css('button.secondary'));
 	}
 
 	async isNotiSuccess() {
@@ -55,4 +65,58 @@ export class InvitationPage {
 		const btns = await subPanel.findElements(by.css('button'));
 		return btns[0];
 	}
+
+	startMailListener(email: string, password: string) {
+		return new Promise((resolve, reject) => {
+			const time = new Date().getTime();
+			const mailListener = new MailListener({
+				username: email,
+				password: password,
+				host: 'imap.gmail.com',
+				port: 993, // imap port
+				secure: true,
+				tls: true,
+				connTimeout: 10000, // Default by node-imap
+				authTimeout: 5000, // Default by node-imap,
+				debug: console.log,
+				tlsOptions: {
+					rejectUnauthorized: false
+				},
+				mailbox: 'INBOX',
+				searchFilter: ['UNSEEN', ['SINCE', time], ['FROM', 'info@showsourcing.com']],
+				fetchUnreadOnStart: true,
+			});
+			mailListener.start();
+
+			mailListener.on('server:connected', () => {
+				console.log('Mail listener initialized');
+				resolve(mailListener);
+			});
+
+			mailListener.on('error', (err) => {
+				console.log(err);
+			});
+
+			mailListener.on('server:disconnected', () => {
+				console.log('imapDisconnected');
+				reject();
+			});
+
+		});
+	}
+
+	stopMailListener(mailListener) {
+		mailListener.stop();
+	}
+
+	getLastEmail(mailListener) {
+		const deferred = protractor.promise.defer();
+		console.log('Waiting for an email...');
+
+		mailListener.on('mail', mail => {
+			deferred.fulfill(mail);
+		});
+		return deferred.promise;
+	}
+
 }
