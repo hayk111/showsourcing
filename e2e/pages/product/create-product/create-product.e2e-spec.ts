fb317c0738eb2de607373b681f81ad89266d38b1
Co NGUYEN <phuc-co.nguyenmai@ant-tech.eu>
2019-07-04 12:51:59 +0700

    - update test suite "create product" (step 3) - add id "topPanelBtn" for "top-panel" component

--- e2e/pages/product/create-product/create-product.e2e-spec.ts
+++ e2e/pages/product/create-product/create-product.e2e-spec.ts
@@ -1,12 +1,13 @@
 import { CreateProductPage } from './create-product.po';
 import { LoginPage } from '../../auth/login/login.po';
 import { ChangePasswordPage } from '../../auth/change-password/change-password.po';
-import { protractor, browser } from 'protractor';
+import { protractor, browser, by } from 'protractor';
 
 describe('create product test suite', () => {
 	let pageCreateProduct: CreateProductPage;
 	let pageLogin: LoginPage;
 	let pageChangePw: ChangePasswordPage;
+	let numTabAvailable: any;
 
 	beforeAll(async () => {
 		console.log('starting beforeAll....');
@@ -21,10 +22,6 @@ describe('create product test suite', () => {
 
 		await browser.driver.wait(async _ => {
 			const url: string = await browser.driver.getCurrentUrl();
-			if (/pick-a-team/.test(url)) {
-
-				console.log(url)
-			}
 			return /pick-a-team/.test(url);
 		}, 10000);
 
@@ -52,49 +49,66 @@ describe('create product test suite', () => {
 			if (!await pageCreateProduct.creationProductDlgApp.isDisplayed()) {
 				fail('can not open "creation-product-dlg-app"');
 			}
+
+			if (!numTabAvailable) {
+				numTabAvailable = await pageCreateProduct.countTabs();
+			}
 		} else {
 			fail('can not find button "Add product" with id "topPanelBtn"');
 		}
 		console.log('beforeEach is done');
 	});
 
-	// it('first field should be focussed by default', async () => {
-	// 	const inp = await pageCreateProduct.getInputByName('name');
-	// 	if (await inp.isDisplayed()) {
-	// 		if (await inp.getId() === await browser.driver.switchTo().activeElement().getId()) {
-	// 			return expect(true).toBe(true);
-	// 		} else {
-	// 			fail('field "name" not focussed by default');
-	// 		}
-	// 	} else {
-	// 		fail('can not get input with name "name"');
-	// 	}
-	// });
+	afterAll(async () => {
+		await browser.restart();
+	});
 
-	it('all fields should be focusable', async () => {
-		// expect(false).toBe(true, 'This is not something I\'ve expected');
-		// expect(true).toBe(true)
-		let expectInp = true, expectSelector = true, expectBoolean = true, expectButtons = true, expectCheckbox = true;
+	it('first field should be focused by default', async () => {
+		const inp = await pageCreateProduct.getInputByName('name');
+		if (await inp.isDisplayed()) {
+			if (await inp.getId() === await browser.driver.switchTo().activeElement().getId()) {
+				return expect(true).toBe(true);
+			} else {
+				fail('field "name" not focussed by default');
+			}
+		} else {
+			fail('can not get input with name "name"');
+		}
+	});
 
-		// check input field focusable
-		const inps = await pageCreateProduct.inputsOfDynamicformApp;
-		for (let i = 0; i < inps.length; i++) {
-			if (await inps[i].getAttribute('type') !== 'radio') {
-				const placeholder = await inps[i].getAttribute('placeholder');
-				const type = await inps[i].getAttribute('type');
-				const isHidden = await inps[i].getAttribute('hidden');
-				if (!await inps[i].getAttribute('hidden')) {
-					await inps[i].sendKeys(1);
-					browser.sleep(5000);
-					if (!(await inps[i].getId() === await browser.driver.switchTo().activeElement().getId())) {
-						expectInp = false;
-					}
+	it('when selector field is focused it should be opened when typing', async () => {
+		if (!numTabAvailable) {
+			fail('can not count all fields focusable');
+		}
+		let currentActiveElem = await browser.driver.switchTo().activeElement();
+
+		// when field "name" not be focused, we will trigger tab to focus field "name"
+		if (!currentActiveElem || !await currentActiveElem.getId()) {
+			await browser.actions().sendKeys(protractor.Key.TAB).perform();
+			currentActiveElem = await browser.driver.switchTo().activeElement();
+		}
+		// find selector fields
+		const selectors = await pageCreateProduct.selectorPlaceHolderApp;
+		let count = 0;
+		const failures = [];
+		for (let i = 0; i < numTabAvailable; i++) {
+			await browser.actions().sendKeys(protractor.Key.TAB).perform();
+
+			const curActiveElem = await browser.driver.switchTo().activeElement();
+			if (await curActiveElem.getTagName() === 'selector-placeholder-app') {
+				await curActiveElem.sendKeys('abc');
+
+				browser.sleep(1000);
+				const isOpenedSelPickerApp = await pageCreateProduct.selPickerApp.isDisplayed();
+
+				if (isOpenedSelPickerApp) { // opened "selector-picker-app"
+					count++;
+					await pageCreateProduct.closeSelPickerApp();
+				} else {
+					failures.push(await curActiveElem.getText());
 				}
 			}
 		}
-
-
-		browser.sleep(200000);
-		return expect(expectInp).toBe(true, 'input field focusable');
+		return expect(count).toEqual(selectors.length, `Failed: ${failures.join(',')}`);
 	});
 });
--- e2e/pages/product/create-product/create-product.po.ts
+++ e2e/pages/product/create-product/create-product.po.ts
@@ -1,4 +1,4 @@
-import { browser, by, element } from 'protractor';
+import { browser, by, element, protractor } from 'protractor';
 
 export class CreateProductPage {
 
@@ -28,9 +28,68 @@ export class CreateProductPage {
 		return await browser.driver.findElement(by.css(`[ng-reflect-name="${name}"]`));
 	}
 
+	async countTabs() {
+		const array = [];
+		let currentActiveElem = await browser.driver.switchTo().activeElement(), firstId;
+
+		// when field "name" not be focused, we will trigger tab to focus field "name"
+		if (!currentActiveElem || !await currentActiveElem.getId()) {
+			await browser.actions().sendKeys(protractor.Key.TAB).perform();
+			currentActiveElem = await browser.driver.switchTo().activeElement();
+		}
+		firstId = await currentActiveElem.getId();
+		array.push(firstId); // add id of field "name" to array
+
+		// trigger next focus
+		await browser.actions().sendKeys(protractor.Key.TAB).perform();
+		currentActiveElem = await browser.driver.switchTo().activeElement();
+
+		while (firstId !== await currentActiveElem.getId()) {
+			array.push(await currentActiveElem.getId());
+			await browser.actions().sendKeys(protractor.Key.TAB).perform();
+			currentActiveElem = await browser.driver.switchTo().activeElement();
+		}
+		return array.length || 0; // length will be 37 (except button "Save product")
+	}
+
+	async isOpenedSelPicker() {
+		const app = await browser.driver.findElement(by.tagName('selector-picker-app'));
+		return app && await app.isDisplayed();
+	}
+
 	get inputsOfDynamicformApp() {
 		const dynamicformApp = browser.driver.findElement(by.tagName('dynamic-form-app'));
 		return dynamicformApp.findElements(by.tagName('input'));
+	}
+
+	get inpRadiosOfDlgApp() {
+		const dlgApp = browser.driver.findElement(by.tagName('dialog-app'));
+		return dlgApp.findElements(by.css('input[type="radio"]'));
+	}
+
+	async fieldsOfDlgApp() {
+		const dlgApp = browser.driver.findElement(by.tagName('dialog-app'));
+		const textInps = await dlgApp.findElements(by.css('input[type="text"]'));
+		const numInps = await dlgApp.findElements(by.css('input[type="number"]'));
+		const radioInps = await dlgApp.findElements(by.css('input[type="radio"]'));
+		const btns = await dlgApp.findElements(by.css('button.secondary'));
+		console.log({
+			textInps: textInps.length,
+			numInps: numInps.length,
+			radioInps: radioInps.length,
+			btns: btns.length
+		});
+		return [...textInps, ...numInps, ...radioInps, ...btns];
+	}
+
+	async closeSelPickerApp() {
+		const doneBtn = await this.selPickerApp.findElement(by.css('button.secondary'));
+		await doneBtn.click();
+	}
+
+	get selectorPlaceHolderApp() {
+		const dlgApp = browser.driver.findElement(by.tagName('dialog-app'));
+		return dlgApp.findElements(by.tagName('selector-placeholder-app'));
 	}
 
 	get dynamicformApp() {
@@ -45,36 +104,7 @@ export class CreateProductPage {
 		return browser.driver.findElement(by.tagName('creation-product-dlg-app'));
 	}
 
-	get errorElem() {
-		return element(by.tagName('error-app'));
+	get selPickerApp() {
+		return browser.driver.findElement(by.tagName('selector-picker-app'));
 	}
-
-	get spinnerElem() {
-		return element(by.tagName('spinner-app'));
-	}
-
 }
-// ] text [ 0 ] [ 'name' ]
-// [1] text [ 1 ] [ '' ]
-// [1] text [ 2 ] [ 'MOQ' ]
-// [1] text [ 3 ] [ '' ]
-// [1] text [ 4 ] [ '' ]
-// [1] text [ 5 ] [ '' ]
-// [1] text [ 6 ] [ 'Length' ]
-// [1] text [ 7 ] [ 'Width' ]
-// [1] text [ 8 ] [ 'Height' ]
-// [1] text [ 9 ] [ '' ]
-// [1] text [ 10 ] [ 'Quantity' ]
-// [1] text [ 11 ] [ 'Weight' ]
-// [1] text [ 12 ] [ '' ]
-// [1] text [ 13 ] [ 'Length' ]
-// [1] text [ 14 ] [ 'Width' ]
-// [1] text [ 15 ] [ 'Height' ]
-// [1] text [ 16 ] [ '' ]
-// [1] text [ 17 ] [ 'Quantity' ]
-// [1] text [ 18 ] [ 'Weight' ]
-// [1] text [ 19 ] [ '' ]
-// [1] text [ 20 ] [ 'Master Carton CBM' ]
-// [1] text [ 21 ] [ 'Quantity per 20\'' ]
-// [1] text [ 22 ] [ 'Quantity per 40\'' ]
-// [1] text [ 23 ] [ 'Quantity per 40\' HC' ]
