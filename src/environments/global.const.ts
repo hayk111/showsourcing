6920d7b061b56f85e938d6af38ab50bfd42d18f4
Michael Vanegas <michael@showsourcing.com>
2019-07-11 16:56:28 +0200

    fixed trget on extended field

--- src/environments/global.const.ts
+++ src/environments/global.const.ts
@@ -1,2 +1,2 @@
-// this file was updated on Mon Jul 08 2019 17:10:55 GMT+0200 (Central European Summer Time)
-export const WEB_VERSION = '2.5.0-8';
+// this file was updated on Thu Jul 11 2019 16:54:30 GMT+0200 (Central European Summer Time)
+export const WEB_VERSION = '2.5.1';
