ff9744be0c02348dcdf97de19af0900065bf6e02
hayk3 <hayk@simplytechnologies.com>
2019-07-30 16:36:44 +0400

    Added pagination component and initil unit tests

---
+++ src/app/shared/pagination/components/pagination.component.ts
@@ -0,0 +1,39 @@
+import {
+	ChangeDetectionStrategy,
+	Component,
+	ContentChildren,
+	EventEmitter,
+	HostListener,
+	Input,
+	OnChanges,
+	Output,
+	QueryList,
+	TemplateRef,
+} from '@angular/core';
+import { DEFAULT_TAKE_PAGINATION } from '~core/entity-services/_global/select-params';
+
+@Component({
+	selector: 'pagination-app',
+	templateUrl: './pagination.component.html',
+	styleUrls: ['./pagination.component.scss'],
+	changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class PaginationComponent {
+	/** Different rows displayed */
+	@Input() rows;
+	@Input() sections;
+	// whether the table is currently loading
+	@Input() pending = false;
+	// whether rows are selectable
+	/** how many items were skipped so we can display the pages */
+	@Input() skipped: number;
+
+	@Output() previous = new EventEmitter<undefined>();
+	@Output() next = new EventEmitter<undefined>();
+	@Output() goToPage = new EventEmitter<number>();
+
+	itemsPerPage = DEFAULT_TAKE_PAGINATION;
+	// current index of the pagination
+	@Input() indexPagination = 0;
+	@Input() totalSections = 1;
+}
