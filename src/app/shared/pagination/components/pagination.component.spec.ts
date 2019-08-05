a4f17307921849caa3692c7c1b2aa9f57ba731ab
hayk3 <hayk@simplytechnologies.com>
2019-08-05 10:44:48 +0400

    Add some more unit tests to the pagination component

--- src/app/shared/pagination/components/pagination.component.spec.ts
+++ src/app/shared/pagination/components/pagination.component.spec.ts
@@ -1,37 +1,55 @@
-import {TestBed, ComponentFixture} from '@angular/core/testing';
+import { TestBed, ComponentFixture } from '@angular/core/testing';
+import { DebugElement } from '@angular/core';
 import { IconComponent } from '~shared/icons';
-
 import { PaginationComponent } from './pagination.component';
+import { of } from 'rxjs';
 
 describe('Component: PaginationComponent', () => {
 	let component: PaginationComponent;
 	let fixture: ComponentFixture<PaginationComponent>;
+	let de: DebugElement;
+	let spy: jasmine.Spy;
 
-	beforeEach(() => {
+	beforeEach(async () => {
 		TestBed.configureTestingModule({
-			declarations: [PaginationComponent, IconComponent],
-			imports: []
+			declarations: [PaginationComponent, IconComponent]
 		}).compileComponents();
 	});
 
-	it('should instantiate', () => {
+	beforeEach(() => {
 		fixture = TestBed.createComponent(PaginationComponent);
 		component = fixture.componentInstance;
 
+		fixture.detectChanges();
+	});
+
+	it('should instantiate', () => {
 		expect(component).toBeDefined();
 	});
 
 	it('should start with pagination index 0', () => {
-		fixture = TestBed.createComponent(PaginationComponent);
-		component = fixture.componentInstance;
-
 		expect(component.currentPage).toEqual(0);
 	});
 
-	it('should start with default section count 1', () => {
-		fixture = TestBed.createComponent(PaginationComponent);
-		component = fixture.componentInstance;
+	it('goToNextPage should increment currentPage', () => {
+		component.totalPages = 2; // assuming there are 2 total pages
+		const currPage = component.currentPage;
+		component.goToNextPage();
+		expect(component.currentPage).toEqual(currPage + 1);
+	});
 
-		expect(component.totalPages).toEqual(1);
+	it('goToPreviousPage should decrement currentPage', () => {
+		component.totalPages = 2; // assuming there are 2 total pages
+		const currPage = 1; // assuming we are on the page 2
+		component.goToPreviousPage();
+		expect(component.currentPage).toEqual(currPage - 1);
+	});
+
+	it('goToIndexPage should navigate to the page and call buildPaginatorRange', () => {
+		spy = spyOn<any>(component, 'buildPaginatorRange');
+
+		component.goToIndexPage(5);
+		expect(component.currentPage).toEqual(5);
+		expect(spy).toHaveBeenCalled();
 	});
 });
