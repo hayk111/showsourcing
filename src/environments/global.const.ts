abb7ffdd63a831ef1ee705e77c65e7fa4474306a
Michael Vanegas <michael@showsourcing.com>
2019-08-05 18:35:33 +0200

    updated the versioning

--- src/environments/global.const.ts
+++ src/environments/global.const.ts
@@ -1,2 +1,3 @@
-// this file was updated on Mon Aug 05 2019 14:03:21 GMT+0200 (Central European Summer Time)
-export const WEB_VERSION = '2.6.0-5';
+import * as packageJson from '../../package.json';
+
+export const WEB_VERSION = packageJson.version;
