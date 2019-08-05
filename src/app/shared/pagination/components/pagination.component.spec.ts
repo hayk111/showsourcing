40928217e683ead8b9da2647ea0f4c469aa2e3f0
cedvdb <cedvandenbosch@gmail.com>
2019-08-05 10:51:41 +0200

    removed deprecated event

--- src/app/shared/pagination/components/pagination.component.spec.ts
+++ src/app/shared/pagination/components/pagination.component.spec.ts
@@ -46,10 +46,25 @@ describe('Component: PaginationComponent', () => {
 	});
 
 	it('goToIndexPage should navigate to the page and call buildPaginatorRange', () => {
-		spy = spyOn<any>(component, 'buildPaginatorRange');
+		// spy = spyOn<any>(component, 'buildPaginatorRange');
 
 		component.goToIndexPage(5);
+		fixture.detectChanges();
 		expect(component.currentPage).toEqual(5);
-		expect(spy).toHaveBeenCalled();
+		// expect(spy).toHaveBeenCalled();
+	});
+
+	it('should build range with even number width', () => {
+		component.width = 4;
+		component.currentPage = 0;
+		fixture.detectChanges();
+		expect(component.range).toEqual([0, 1, 2, 3, 4]);
+	});
+
+	it('should build range with odd number width', () => {
+		component.width = 3;
+		component.currentPage = 0;
+		fixture.detectChanges();
+		expect(component.range).toEqual([0, 1, 2]);
 	});
 });
