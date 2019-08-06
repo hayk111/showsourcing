e7420b32c9d68ee76aac91e516e0ac35ae486c88
cedvdb <cedvandenbosch@gmail.com>
2019-08-06 10:56:47 +0200

    pagination tests

--- src/app/shared/pagination/components/pagination.component.spec.ts
+++ src/app/shared/pagination/components/pagination.component.spec.ts
@@ -17,7 +17,7 @@ const ITEMS_PER_PAGE = DEFAULT_TAKE_PAGINATION;
 	`<pagination-app
 		[width]="width"
 		[count]="count"
-		[currentPage]=[currentPage]><pagination-app>`
+		[currentPage]="currentPage"><pagination-app>`
 })
 export class TestHostComponent {
 	@ViewChild(PaginationComponent, { static: true }) component: any;
@@ -28,10 +28,13 @@ export class TestHostComponent {
 }
 
 
-fdescribe('Component: PaginationComponent', () => {
+describe('Component: PaginationComponent', () => {
 	let testComp: TestHostComponent;
 	let paginationComp: PaginationComponent;
 	let fixture: ComponentFixture<TestHostComponent>;
+	let previousBtn: HTMLButtonElement;
+	let nextBtn: HTMLButtonElement;
+	let currentPageElem: HTMLElement;
 
 	beforeEach(async () => {
 		TestBed.configureTestingModule({
@@ -44,18 +47,25 @@ fdescribe('Component: PaginationComponent', () => {
 		testComp = fixture.componentInstance;
 		paginationComp = testComp.component;
 		testComp.width = 5;
-
 		fixture.detectChanges();
+		previousBtn = fixture.nativeElement.querySelector('icon-app[name="angle-left"]');
+		nextBtn = fixture.nativeElement.querySelector('icon-app[name="angle-right"]');
+		currentPageElem = fixture.nativeElement.querySelector('.selected');
 	});
 
-	it('should set the fields of the pagination component and call ngOnChanges', () => {
-		testComp.width = 7;
-		spyOn(paginationComp, 'ngOnChanges').and.callThrough();
-
+	it('displayed current page is + 1 current page', () => {
 		fixture.detectChanges();
-		expect(paginationComp.width).toEqual(7);
-		expect(paginationComp.ngOnChanges).toHaveBeenCalled();
+		expect(currentPageElem.textContent).toContain('1');
 	});
+
+	// it('should set the fields of the pagination component and call ngOnChanges', () => {
+	// 	testComp.width = 7;
+	// 	spyOn(paginationComp, 'ngOnChanges').and.callThrough();
+
+	// 	fixture.detectChanges();
+	// 	expect(paginationComp.width).toEqual(7);
+	// 	expect(paginationComp.ngOnChanges).toHaveBeenCalled();
+	// });
 
 	it('should have the correct number of total pages', () => {
 		// 0 items
@@ -82,58 +92,76 @@ fdescribe('Component: PaginationComponent', () => {
 		fixture.detectChanges();
 		expect(paginationComp.range).toEqual([0, 1, 2, 3, 4]);
 
-		testComp.currentPage = 1;
+		testComp.currentPage = 2;
 		fixture.detectChanges();
 		expect(paginationComp.range).toEqual([0, 1, 2, 3, 4]);
 
-		testComp.currentPage = 3;
+		testComp.currentPage = 4;
 		fixture.detectChanges();
-		expect(paginationComp.range).toEqual([1, 2, 3, 4, 5]);
+		expect(paginationComp.range).toEqual([2, 3, 4, 5, 6]);
 
 		testComp.currentPage = 6;
 		fixture.detectChanges();
 		expect(paginationComp.range).toEqual([2, 3, 4, 5, 6]);
 
-		// when only two pages
-		// testComp.count = ITEMS_PER_PAGE * 2;
-		// testComp.currentPage = 0;
-		// fixture.detectChanges();
-		// expect(paginationComp.range).toEqual([0, 1]);
 	});
 
-	// it('previous button should be disabled when on page 0', () => {
-	// 	const previousBtn = fixture.nativeElement.querySelector('icon-app[name="angle-left"]');
-	// 	expect(previousBtn.class).toContain('disabled');
-	// });
+	it('should build the range [0] when no items', () => {
+		// when no items
+		testComp.count = 0;
+		testComp.currentPage = 0;
+		fixture.detectChanges();
+		expect(paginationComp.range).toEqual([0]);
+	});
 
-	// it('next button should be disabled when on last page', () => {
-	// 	const nextBtn = fixture.nativeElement.querySelector('icon-app[name="angle-right"]');
-	// 	expect(nextBtn.class).toContain('disabled');
-	// });
+	it('should build the correct range when less page than width', () => {
+		// when only two pages
+		testComp.count = ITEMS_PER_PAGE * 2;
+		testComp.currentPage = 0;
+		fixture.detectChanges();
+		expect(paginationComp.range).toEqual([0, 1]);
+	});
 
-	// it('clicking on next icon should go to next page', () => {
-	// 	spyOn(component.goToPage, 'emit');
-	// 	const currPage = component.currentPage;
-	// 	component.goToNextPage();
-	// 	expect(component.currentPage).toEqual(currPage + 1);
-	// 	expect(component.goToPage.emit).toHaveBeenCalledWith(component.currentPage);
-	// });
+	it('previous button should be disabled when on page 0', () => {
+		expect(paginationComp.currentPage).toEqual(0);
+		expect(previousBtn.className).toContain('disabled');
+	});
 
-	// it('clicking on previous icon should go to previous page', () => {
-	// 	spyOn(component.goToPage, 'emit');
-	// 	const currPage = component.currentPage;
-	// 	component.goToPreviousPage();
-	// 	expect(component.currentPage).toEqual(currPage - 1);
-	// 	expect(component.goToPage.emit).toHaveBeenCalledWith(component.currentPage);
-	// });
+	it('next button should be disabled when on last page', () => {
+		testComp.count = ITEMS_PER_PAGE * 20;
+		testComp.currentPage = 19;
+		fixture.detectChanges();
+		expect(nextBtn.className).toContain('disabled');
+	});
 
-	// it('clicking on a index page button should go to index page', () => {
-	// 	spyOn(component.goToPage, 'emit');
-	// 	component.goToIndexPage(4);
-	// 	fixture.detectChanges();
-	// 	expect(component.currentPage).toEqual(4);
-	// 	expect(component.goToPage.emit).toHaveBeenCalledWith(component.currentPage);
-	// });
+	it('clicking on next icon should go to next page', () => {
+		spyOn(paginationComp.goToPage, 'emit');
+		const currPage = paginationComp.currentPage;
+		nextBtn.click();
+		expect(paginationComp.currentPage).toEqual(currPage + 1);
+		expect(paginationComp.goToPage.emit).toHaveBeenCalledWith(paginationComp.currentPage);
+	});
+
+	it('clicking on previous icon should go to previous page', () => {
+		spyOn(paginationComp.goToPage, 'emit');
+		testComp.currentPage = 1;
+		fixture.detectChanges();
+		const currPage = paginationComp.currentPage;
+		previousBtn.click();
+		expect(paginationComp.currentPage).toEqual(currPage - 1);
+		expect(paginationComp.goToPage.emit).toHaveBeenCalledWith(paginationComp.currentPage);
+	});
+
+	it('clicking on a index page button should go to index page', () => {
+		spyOn(paginationComp.goToPage, 'emit');
+		const indexBtns = fixture.nativeElement.querySelectorAll('.pagination-item');
+		const fourthPage = indexBtns[4];
+		testComp.currentPage = 1;
+		fixture.detectChanges();
+		fourthPage.click();
+		expect(paginationComp.currentPage).toEqual(4);
+		expect(paginationComp.goToPage.emit).toHaveBeenCalledWith(paginationComp.currentPage);
+	});
 
 
 });
