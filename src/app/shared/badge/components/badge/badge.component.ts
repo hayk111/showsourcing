98370704d300fd54a0de12d25dabcefd7cf34ed3
cedvdb <cedvandenbosch@gmail.com>
2019-08-08 15:37:02 +0200

    badge update

--- src/app/shared/badge/components/badge/badge.component.ts
+++ src/app/shared/badge/components/badge/badge.component.ts
@@ -4,38 +4,34 @@ import { Component, OnInit, Input } from '@angular/core';
 	selector: 'badge-app',
 	templateUrl: './badge.component.html',
 	styleUrls: ['./badge.component.scss'],
+	/** @deprecated */
 	host: {
-		'[class.in-progress]': 'type === "in-progress" || type === "inProgress"',
-		'[class.secondary]': 'type === "secondary"',
 		'[class.primary]': 'type === "primary"',
-		'[class.secondary-light]': 'type === "secondary-light"',
+		'[class.secondary]': 'type === "secondary"',
 		'[class.warn]': 'type === "warn"',
 		'[class.success]': 'type === "success"',
 		'[class.accent]': 'type === "accent"',
+		'[class.vibrant]': 'type === "vibrant"',
+		'[class.small]': 'size === "s" || size === "small"',
+		'[class.medium]': 'size === "m" || size === "medium"',
 		'[class.flexVAlign]': 'true',
 		'[class.flexCenter]': 'true',
+		/** @deprecated we won't have rounds anymore*/
 		'[class.round]': 'round',
+		/** @deprecated legacy sizes please remove the scss as well*/
 		'[class.tiny]': 'size === "tiny"',
 		'[class.mini]': 'size === "mini"',
 		'[class.xsmall]': 'size === "xs"',
 		'[class.thick]': 'size === "thick"',
-		'[class.small]': 'size === "s" || size === "small"',
-		'[class.medium]': 'size === "m" || size === "medium"',
 		'[class.large]': 'size === "l" || size === "large"'
 	}
 })
-export class BadgeComponent implements OnInit {
+export class BadgeComponent {
+	@Input() type: 'primary' | 'secondary' | 'accent' | 'vibrant' | 'success' | 'warn' | 'none' = 'secondary';
 	@Input() size = 's';
 	/** type gives us the possibility to have predefined color set
 	 * for background and color. The none type is when we want to inherit
 	 */
-	@Input() round = true;
-	// tslint:disable-next-line: max-line-length
-	@Input() type: 'in-progress' | 'inProgress' | 'secondary' | 'secondary-light' | 'accent' | 'success' | 'warn' | 'primary' | 'none' = 'secondary';
-
-	constructor() { }
-
-	ngOnInit() {
-	}
-
+	/** @deprecated */
+	@Input() round = false;
 }
