6d56cb81eb4d24f25623423f9973379cab59f84d
Michael Vanegas <michael@showsourcing.com>
2019-08-13 11:43:12 +0200

    added unit tests blank

---
+++ src/app/core/descriptors/sample-descriptor.component.spec.ts
@@ -0,0 +1,7 @@
+import { SampleDescriptor } from './sample-descriptor.component';
+
+describe('SampleDescriptor', () => {
+	it('should create an instance', () => {
+		expect(new SampleDescriptor()).toBeTruthy();
+	});
+});
