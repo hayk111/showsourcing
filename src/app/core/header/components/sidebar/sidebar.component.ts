ef1d55d1480488ccd9ecce468c4e1080d6710341
Co NGUYEN <phuc-co.nguyenmai@ant-tech.eu>
2019-08-13 15:47:10 +0700

    update

--- src/app/core/header/components/sidebar/sidebar.component.ts
+++ src/app/core/header/components/sidebar/sidebar.component.ts
@@ -5,6 +5,8 @@ import { Observable } from 'rxjs';
 import { AuthenticationService } from '~core/auth/services/authentication.service';
 import { DEFAULT_REPLIED_STATUS, Team, User } from '~core/models';
 import { SupplierRequestService, TeamService, UserService } from '~entity-services';
+import { Router } from '@angular/router';
+import { translate } from '~utils';
 
 @Component({
 	selector: 'sidebar-app',
@@ -16,7 +18,8 @@ export class SidebarComponent implements OnInit {
 	team$: Observable<Team>;
 	requestCount$: Observable<number>;
 	isProd = environment.production;
-
+	navItems: any[];
+	hoverIndex: boolean;
 	constructor(
 		private authSrv: AuthenticationService,
 		private userSrv: UserService,
@@ -29,10 +32,48 @@ export class SidebarComponent implements OnInit {
 		this.requestCount$ = this.requestSrv.selectCount(
 			`status == "${DEFAULT_REPLIED_STATUS}" AND senderTeamId == "${this.teamSrv.selectedTeamSync.id}"`
 		);
+		this.navItems = [{
+			icon: 'assets/icons/product-sidebar.svg',
+			link: ['/product'],
+			children: [{
+				icon: 'assets/icons/product-sidebar.svg',
+				label: translate('products')
+			},
+			{
+				icon: 'assets/icons/product-sidebar.svg',
+				label: translate('samples')
+			},
+			{
+				icon: 'assets/icons/product-sidebar.svg',
+				label: translate('boards')
+			}]
+		},
+		{
+			icon: 'assets/icons/product-sidebar.svg',
+			link: ['/project']
+		},
+		{
+			icon: 'assets/icons/product-sidebar.svg',
+			link: ['/supplier']
+		},
+		{
+			icon: 'assets/icons/product-sidebar.svg',
+			link: ['/workspace']
+		},
+		{
+			icon: 'assets/icons/product-sidebar.svg',
+			link: ['/request'],
+			notifBadge: true
+		}
+	];
 	}
 
 	logout() {
 		this.authSrv.logout();
+	}
+
+	hoverItem(index) {
+		this.hoverIndex = index;
 	}
 
 }
