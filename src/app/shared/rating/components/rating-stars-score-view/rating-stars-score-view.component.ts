c904fa69618042b1483e06cf9151c744831af834
Michael Vanegas <michael@showsourcing.com>
2019-08-26 17:19:18 +0200

    für Ced

--- src/app/shared/rating/components/rating-stars-score-view/rating-stars-score-view.component.ts
+++ src/app/shared/rating/components/rating-stars-score-view/rating-stars-score-view.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { Component, OnInit, ChangeDetectionStrategy, Input, ChangeDetectorRef } from '@angular/core';
+import { ProductVote } from '~core/models';
 
 @Component({
 	selector: 'rating-stars-score-view-app',
@@ -8,9 +9,29 @@ import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
 })
 export class RatingStarsScoreViewComponent implements OnInit {
 
-	constructor() { }
+	@Input() votes: ProductVote[];
+
+	votesMap: Map<number, number>;
+	totalVotes = 0;
+
+	constructor(private cdr: ChangeDetectorRef) {
+		this.votesMap = new Map();
+		this.votesMap.set(1, 4);
+		this.votesMap.set(2, 1);
+		this.votesMap.set(3, 8);
+		this.votesMap.set(4, 5);
+		this.votesMap.set(5, 13);
+		this.votesMap.forEach((v, k) => this.totalVotes += v);
+	}
 
 	ngOnInit() {
+		setTimeout(_ => {
+			this.votesMap.set(1, 12);
+			this.totalVotes = 0;
+			this.votesMap.forEach((v, k) => this.totalVotes += v);
+			console.log('we called it ', this.votesMap);
+			// this.cdr.markForCheck();
+		}, 2000);
 	}
 
 }
