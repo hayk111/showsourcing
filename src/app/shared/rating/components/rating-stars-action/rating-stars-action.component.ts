ca90a783fb5ccb24b4bce0fa6c01ffcfcbc8e89e
Michael Vanegas <michael@showsourcing.com>
2019-08-27 16:14:07 +0200

    added inputs

--- src/app/shared/rating/components/rating-stars-action/rating-stars-action.component.ts
+++ src/app/shared/rating/components/rating-stars-action/rating-stars-action.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { Component, OnInit, ChangeDetectionStrategy, Input, Output, EventEmitter } from '@angular/core';
+import { ProductVote } from '~core/models';
+import { UserService } from '~core/entity-services';
 
 @Component({
 	selector: 'rating-stars-action-app',
@@ -8,7 +10,22 @@ import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
 })
 export class RatingStarsActionComponent implements OnInit {
 
-	constructor() { }
+	private _votes: ProductVote[];
+	@Input() set votes(votes: ProductVote[]) {
+		this._votes = votes;
+		const voteIndex = (votes || []).findIndex(v => v.user && v.user.id === this.userSrv.userSync.id);
+		if (~voteIndex)
+			this.myVote = votes[voteIndex];
+	}
+	get votes() {
+		return this._votes;
+	}
+
+	@Output() numVoted = new EventEmitter<number>();
+
+	myVote: ProductVote;
+
+	constructor(private userSrv: UserService) { }
 
 	ngOnInit() {
 	}
