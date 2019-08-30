11d4bc43752f77b1b0131ed266ab99e2a3cae7f4
Co NGUYEN <phuc-co.nguyenmai@ant-tech.eu>
2019-08-30 17:43:25 +0700

    Add translation for models

--- src/app/common/modals/component/reply-sent-dlg/reply-sent-dlg.component.ts
+++ src/app/common/modals/component/reply-sent-dlg/reply-sent-dlg.component.ts
@@ -1,6 +1,6 @@
 import { ChangeDetectionStrategy, Component, Input, OnInit } from '@angular/core';
 import { DialogService } from '~shared/dialog';
-import { translate } from '~utils';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
 	selector: 'reply-sent-dlg-app',
@@ -12,8 +12,11 @@ export class ReplySentDlgComponent implements OnInit {
 
 	// can be px, vh, 1%...
 	@Input() height = '80vh';
-	@Input() actionName = translate('reply');
-	constructor(private dlgSrv: DialogService) { }
+	@Input() actionName = this.translate.instant('label.reply');
+	constructor(
+		private dlgSrv: DialogService,
+		private translate: TranslateService
+		) { }
 
 	ngOnInit() {
 	}
