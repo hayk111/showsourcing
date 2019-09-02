5fadf69bcce818681b965040a2a1a95c3826c6a0
Michael Vanegas <michael@showsourcing.com>
2019-09-02 14:08:52 +0200

    fixed import logo

--- src/app/shared/icons/icons.module.ts
+++ src/app/shared/icons/icons.module.ts
@@ -1,15 +1,15 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { IconComponent } from '~shared/icons/components/icon/icon.component';
-import { LogoComponent } from './components/logo/logo.component';
 import { ImageModule } from '~shared/image/image.module';
+
+import { IconComponent, LogoComponent } from './components';
 
 @NgModule({
 	imports: [
 		CommonModule,
 		ImageModule
 	],
-	declarations: [IconComponent],
-	exports: [IconComponent]
+	declarations: [IconComponent, LogoComponent],
+	exports: [IconComponent, LogoComponent]
 })
 export class IconsModule { }
