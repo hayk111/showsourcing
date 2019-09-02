b1bd8c9da4b7817f3e0a29a77d520ababd8b9dc5
Eric <eric@simplytechnologies.net>
2019-09-02 14:27:01 +0400

    pane animation

--- src/app/shared/notif/components/notif-panel/notif-panel.component.ts
+++ src/app/shared/notif/components/notif-panel/notif-panel.component.ts
@@ -2,14 +2,30 @@ import { Component, OnInit, ChangeDetectionStrategy, Output, EventEmitter, Input
 import { GetStreamGroup } from '~common/activity/interfaces/get-stream-feed.interfaces';
 import { NotificationActivityService } from '~shared/notif/services/notification-activity.service';
 
+import { trigger, state, transition, style, animate } from '@angular/animations';
+
 @Component({
 	selector: 'notif-panel-app',
 	templateUrl: './notif-panel.component.html',
 	styleUrls: ['./notif-panel.component.scss'],
-	changeDetection: ChangeDetectionStrategy.OnPush
+	animations: [
+		trigger('paneAnimationTrigger', [
+			state('void', style({
+				transform: 'translateX(-100%)',
+			})),
+			transition('* => void', [
+				animate('300ms ease-in', style({ transform: 'translateX(-100%)' }))
+			]),
+			transition('void => *', [
+				animate('300ms ease-in', style({ transform: 'translateX(0)' }))
+			]),
+		]),
+	]
 })
 export class NotifPanelComponent implements OnInit {
 	@Input() results: GetStreamGroup[] = null;
+	@Output() startAnimation = new EventEmitter<void>();
+	@Output() endAnimation = new EventEmitter<void>();
 
 	constructor(public notifActivitySrv: NotificationActivityService) {}
 
