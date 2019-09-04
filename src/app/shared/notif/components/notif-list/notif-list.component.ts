675c2f7ad969b5fd7271392f3ea61a79db0c1e13
Eric <eric@simplytechnologies.net>
2019-09-04 12:10:53 +0400

    add infinte scroll feature to notifications panel + change animate time + fix submenu z-index issue

--- src/app/shared/notif/components/notif-list/notif-list.component.ts
+++ src/app/shared/notif/components/notif-list/notif-list.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit, Input, ChangeDetectionStrategy, EventEmitter, Output, ChangeDetectorRef, OnDestroy } from '@angular/core';
+import {
+	Component, OnInit, OnChanges, Input, ChangeDetectionStrategy,
+	ChangeDetectorRef, SimpleChanges, HostListener } from '@angular/core';
 import { GetStreamGroup } from '~common/activity/interfaces/get-stream-feed.interfaces';
 import { NotificationActivityService } from '~shared/notif/services/notification-activity.service';
-import { Subscription } from 'rxjs';
 import { AutoUnsub } from '~utils';
 
 @Component({
@@ -11,13 +12,28 @@ import { AutoUnsub } from '~utils';
 	changeDetection: ChangeDetectionStrategy.OnPush
 
 })
-export class NotifListComponent extends AutoUnsub implements OnInit  {
-
+export class NotifListComponent extends AutoUnsub implements OnInit, OnChanges {
 	@Input() activities: GetStreamGroup[] = [];
 	@Input() isDashboardBox = false;
+	@HostListener('window:resize', ['$event'])
+	onResize(event) {
+		this.notifActivitySrv.onWindowResize(event);
+	}
 
+	isLoading = false;
+	isAllLoaded = false;
 	constructor(private notifActivitySrv: NotificationActivityService, private changeDetRef: ChangeDetectorRef) {
 		super();
+	}
+
+	ngOnChanges(changes: SimpleChanges) {
+		if (changes.activities && !changes.activities.isFirstChange()) {
+			this.isLoading = false;
+			const { previousValue, currentValue } = changes.activities;
+			if (previousValue.length === currentValue.length) {
+				this.isAllLoaded = true;
+			}
+		}
 	}
 
 
@@ -39,6 +55,13 @@ export class NotifListComponent extends AutoUnsub implements OnInit  {
 	markAsRead(notificationId: string) {
 		this.activities.find(activity => activity.id === notificationId).is_read = true;
 		this.changeDetRef.markForCheck();
+	}
+
+	loadMore() {
+		if (!this.isAllLoaded && !this.isLoading) {
+			this.isLoading = true;
+			this.notifActivitySrv.loadMore();
+		}
 	}
 
 }
