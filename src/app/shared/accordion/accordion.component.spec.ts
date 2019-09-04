abd2f15fb9630ae62e4285a9e6bf0c104534f7ac
cedvdb <cedvandenbosch@gmail.com>
2019-09-04 16:42:31 +0200

    accordion

--- src/app/shared/accordion/accordion.component.spec.ts
+++ src/app/shared/accordion/accordion.component.spec.ts
@@ -1,17 +1,22 @@
+import { TestBed } from '@angular/core/testing';
+import { AccordionModule } from './accordion.module';
 
 
 describe('accordion component', () => {
+	beforeEach(async () => {
+		// TODO Thinh will do this
+	});
 
 	it('should display accordion-title-app', () => {
-
+		// TODO Thinh will do this
 	});
 
 	it('should display icon angle-down when closed, angle up when open', () => {
-
+		// TODO Thinh will do this
 	});
 
 	it('when clicking top bar (container for title and icon) it should toggle accordion', () => {
-
+		// TODO Thinh will do this
 	});
 
 });
