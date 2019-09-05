6151ee70dc6d24795418d62afec33729f345179e
cedvdb <cedvandenbosch@gmail.com>
2019-09-05 23:51:25 +0200

    in lib page

--- src/app/shared/logo-nav/logo-nav.component.ts
+++ src/app/shared/logo-nav/logo-nav.component.ts
@@ -4,7 +4,11 @@ import { Component, OnInit, ChangeDetectionStrategy, Input } from '@angular/core
 	selector: 'logo-nav',
 	templateUrl: './logo-nav.component.html',
 	styleUrls: ['./logo-nav.component.scss'],
-	changeDetection: ChangeDetectionStrategy.OnPush
+	changeDetection: ChangeDetectionStrategy.OnPush,
+	host: {
+		class: 'flex clickable pd-ms' // TODO: michael this is a note, I added
+		// clickable class on the accordion merge request
+	}
 })
 export class LogoNavComponent {
 
