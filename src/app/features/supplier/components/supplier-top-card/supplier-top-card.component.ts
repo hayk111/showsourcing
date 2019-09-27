278f531dba166acefb6c267d6a8fb6ff747248c9
thinh.huynh.re <truong-thinh.huynh-nguyen@ant-tech.eu>
2019-09-27 11:46:02 +0700

    Headers in supplier feature

--- src/app/features/supplier/components/supplier-top-card/supplier-top-card.component.ts
+++ src/app/features/supplier/components/supplier-top-card/supplier-top-card.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, ChangeDetectionStrategy, Input, Output, EventEmitter
 import { Location } from '@angular/common';
 import { ERM, Supplier, User } from '~models';
 import { TrackingComponent } from '~utils/tracking-component';
+import { Router } from '@angular/router';
 
 @Component({
 	selector: 'supplier-top-card-app',
@@ -20,6 +21,7 @@ export class SupplierTopCardComponent extends TrackingComponent implements OnIni
 	typeEntity = ERM.SUPPLIER;
 
 	constructor(
+		private router: Router,
 		private location: Location
 	) {
 		super();
@@ -34,6 +36,10 @@ export class SupplierTopCardComponent extends TrackingComponent implements OnIni
 
 	goBack() {
 		this.location.back();
+	}
+
+	getHeaderNavUrl(nav: string): string {
+		return this.router.url.substring(0, this.router.url.lastIndexOf('/') + 1) + nav;
 	}
 
 }
