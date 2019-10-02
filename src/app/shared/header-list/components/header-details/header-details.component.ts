f55885792f533b4267586a9545ff884cbc6fb8b7
thinh.huynh.re <truong-thinh.huynh-nguyen@ant-tech.eu>
2019-10-02 16:53:47 +0700

    Headers in settings page

--- src/app/shared/header-list/components/header-details/header-details.component.ts
+++ src/app/shared/header-list/components/header-details/header-details.component.ts
@@ -14,7 +14,7 @@ export class HeaderDetailsComponent extends TrackingComponent {
 	@Input() subTitle: string;
 	@Input() hasBackArrow = true;
 	@Input() hasLogo = true;
-	@Input() hasPadding = false;
+	@Input() headerType: 'details' | 'settings' | 'table';
 	@HostBinding('class.z-1') @Input() elevated = true;
 
 	constructor(
@@ -30,4 +30,11 @@ export class HeaderDetailsComponent extends TrackingComponent {
 	toDisplayString(nav: string) {
 		return nav.toLowerCase().replace(/-/g, ' ');
 	}
+
+	getType(cls?: string) {
+		return cls
+			? cls + (this.headerType ? '-' + this.headerType : '')
+			: this.headerType;
+	}
+
 }
