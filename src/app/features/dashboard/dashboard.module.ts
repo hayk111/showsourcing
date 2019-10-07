ffabb3072055bf7254054c1d045e6768bbbedb5f
hayk3 <hayk@simplytechnologies.com>
2019-10-07 15:17:51 +0400

    Adds team performance window and chart functionality

--- src/app/features/dashboard/dashboard.module.ts
+++ src/app/features/dashboard/dashboard.module.ts
@@ -7,6 +7,8 @@ import { DashboardHeaderComponent } from '~features/dashboard/components/dashboa
 import { TeamCardComponent } from '~features/dashboard/components/team-card/team-card.component';
 import { DashboardComponent } from '~features/dashboard/containers/dashboard/dashboard.component';
 import { TodoBoxComponent } from '~features/dashboard/components/todo-box/todo-box.component';
+import { TeamPerformanceComponent} from '~features/dashboard/components/team-performance/team-performance.component';
+import { LineChartComponent } from '~features/dashboard/components/line-chart/line-chart.component';
 import { routes } from '~features/dashboard/routes';
 import { DialogModule } from '~shared/dialog';
 import { SharedModule } from '~shared/shared.module';
@@ -14,6 +16,7 @@ import { ProductCommonModule } from '~common/product';
 import { TaskCommonModule } from '~common/task';
 import { SampleCommonModule } from '~common/sample';
 import { SupplierCommonModule } from '~common/supplier';
+import { ChartsModule } from 'ng2-charts';
 
 import {
 	TodoBoxTaskPreviewComponent,
@@ -24,6 +27,7 @@ import {
 
 @NgModule({
 	imports: [
+		ChartsModule,
 		SharedModule,
 		RouterModule.forChild(routes),
 		ActivityCommonModule,
@@ -38,11 +42,13 @@ import {
 		SummaryComponent,
 		TeamCardComponent,
 		DashboardHeaderComponent,
+		LineChartComponent,
 		TodoBoxComponent,
 		TodoBoxTaskPreviewComponent,
 		TodoBoxProductsPreviewComponent,
 		TodoBoxSamplePreviewComponent,
-		TodoBoxSupplierPreviewComponent
+		TodoBoxSupplierPreviewComponent,
+		TeamPerformanceComponent,
 	],
 	providers: [
 		ProductDialogService
