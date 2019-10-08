8fd9e94c602fd7b2b3856214aafeed97bba84640
cedvdb <cedvandenbosch@gmail.com>
2019-10-08 17:00:52 +0200

    fix extended fields querying only 20

--- src/app/core/entity-services/extended-field-definition/extended-field-definition.queries.ts
+++ src/app/core/entity-services/extended-field-definition/extended-field-definition.queries.ts
@@ -3,6 +3,7 @@ import { GlobalQueries } from '~entity-services/_global/global-queries.class';
 export abstract class ExtendedFieldDefinitionQueries extends GlobalQueries {
 	static readonly one = 'label type order metadata';
 	static readonly many = 'label type order metadata';
+	static readonly all = 'label type order metadata';
 }
 
 
