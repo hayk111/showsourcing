6444a52fea5d6676af2eafb4c2f7362c95f3166c
Michael Vanegas <michael@showsourcing.com>
2019-10-22 11:57:34 +0200

    project continue refactor

--- src/app/common/tables/tables-common.module.ts
+++ src/app/common/tables/tables-common.module.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { SharedModule } from '~shared/shared.module';
 
 import { ProjectsTableComponent } from './projects-table/projects-table.component';
@@ -7,12 +8,11 @@ import { SampleTableComponent } from './sample-table/sample-table.component';
 import { TaskTableComponent } from './task-table/task-table.component';
 
 
-
-
 @NgModule({
 	imports: [
 		CommonModule,
 		SharedModule,
+		RouterModule
 	],
 	declarations: [
 		ProjectsTableComponent,
