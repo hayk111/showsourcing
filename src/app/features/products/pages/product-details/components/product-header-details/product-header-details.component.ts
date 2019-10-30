0fc3d92c83896959bc3e5cc628a992e738a961c5
cedvdb <cedvandenbosch@gmail.com>
2019-10-30 16:43:20 +0100

    badge header step 1

--- src/app/features/products/pages/product-details/components/product-header-details/product-header-details.component.ts
+++ src/app/features/products/pages/product-details/components/product-header-details/product-header-details.component.ts
@@ -11,6 +11,8 @@ import { ERM } from '~models/_erm.enum';
 })
 export class ProductHeaderDetailsComponent {
 	@Input() product: Product;
+	// because at the moment the request count isn't on the product
+	@Input() requestCount: number;
 	@Output() delete = new EventEmitter<Product>();
 	@Output() export = new EventEmitter<Product>();
 	@Output() update = new EventEmitter<Product>();
@@ -28,6 +30,18 @@ export class ProductHeaderDetailsComponent {
 
 	onUserChanged(user: User) {
 		this.update.emit({ id: this.product.id, assignee: new User({ id: user.id }) });
+	}
+
+	hasBadge(type: string) {
+		if (!this.product)
+			return;
+
+		switch (type) {
+			case 'comments': return this.product.comments.length > 0;
+			case 'tasks': return this.product.tasksLinkedAssignedToMe.count > 0;
+			case 'samples': return this.product.samplesLinkedAssignedToMe.count > 0;
+			case 'requests': return this.requestCount > 0;
+		}
 	}
 
 }
