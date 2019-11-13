2e79f029531ecb891c943f831546432f422c0c1d
Michael Vanegas <michael@showsourcing.com>
2019-11-13 18:33:09 +0100

    removed unused comment

--- src/app/features/products/pages/product-details/components/product-sub-header-details/product-sub-header-details.component.ts
+++ src/app/features/products/pages/product-details/components/product-sub-header-details/product-sub-header-details.component.ts
@@ -19,20 +19,3 @@ export class ProductSubHeaderDetailsComponent implements OnInit {
 	}
 
 }
-/**
- product details
- - hover effects have a minimum-width
- - when clicking on the title the rating, sample, task, comment -> you should be redirected
- - status button change on design
- - add the update methods to the sub-header
-
-- sample/task/project list -> when hovering over list item, txt-btn class and open preview or open project
- - project list -> add button to add projects
- - product sub header -> supplier section -> name supplier and stars 2-4px margin
- - success color != task color, change it logo app
- - tags badge change color style
- - on rating badge , parenthesis shoudl not appear if ther eis no votes at all
-
- - Card with tables -> Titles on each table
-		 - Sample reference align to left iwth name
- */
