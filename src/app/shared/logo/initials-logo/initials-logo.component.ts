4d487eca87fab053d071ac889af3156ec6bc7b9c
Michael Vanegas <michael@showsourcing.com>
2019-11-18 15:38:45 +0100

    fixed initals logo component logic

--- src/app/shared/logo/initials-logo/initials-logo.component.ts
+++ src/app/shared/logo/initials-logo/initials-logo.component.ts
@@ -1,7 +1,8 @@
-import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input, ViewChild, ElementRef, AfterViewInit, Renderer2 } from '@angular/core';
 import { Client } from '~core/apollo/services/apollo-client-names.const';
-import { AppImage, Supplier } from '~core/models';
+import { AppImage, EntityName } from '~core/models';
 import { UploaderService } from '~shared/file/services/uploader.service';
+import { colorMap, Size, sizeMap } from '../logo/logo.component';
 
 @Component({
 	selector: 'initials-logo-app',
@@ -9,16 +10,15 @@ import { UploaderService } from '~shared/file/services/uploader.service';
 	styleUrls: ['./initials-logo.component.scss'],
 	changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class InitialsLogoComponent {
+export class InitialsLogoComponent implements AfterViewInit {
 
-	private _supplier: Supplier;
-	@Input() set supplier(supplier: Supplier) {
-		this._supplier = supplier;
-		if (supplier && supplier.name)
-			this.setInitials(supplier.name);
+	private _name: string;
+	@Input() set name(name: string) {
+		this._name = name;
+		this.setInitials(name);
 	}
-	get supplier() {
-		return this._supplier;
+	get name() {
+		return this._name;
 	}
 	private _image: AppImage;
 	@Input() set image(image: AppImage) {
@@ -27,19 +27,42 @@ export class InitialsLogoComponent {
 	get image() {
 		return this._image;
 	}
+	@Input() entity: any;
+	@Input() type: EntityName;
+	@Input() size: Size = 'm';
+
+	@ViewChild('initialsElement', { static: false }) initialsElement: ElementRef;
 
 	initials: string;
 
-	constructor(private uploaderSrv: UploaderService) { }
+	constructor(
+		private hostElement: ElementRef,
+		private uploaderSrv: UploaderService,
+		private render: Renderer2
+	) { }
+
+	ngAfterViewInit() {
+		// we choose the background color using the colors set on the logo-app
+		const color = colorMap[this.type] || 'secondary';
+		const size = sizeMap[this.size] || sizeMap.m;
+		this.render.setStyle(this.hostElement.nativeElement, 'height', `${size.background}px`);
+		this.render.setStyle(this.hostElement.nativeElement, 'width', `${size.background}px`);
+		this.render.addClass(this.initialsElement.nativeElement, `bg-${color}`);
+		this.render.setStyle(this.initialsElement.nativeElement, 'font-size', `${size.icon}px`);
+	}
 
 	addLogo(files: File[]) {
-		this.uploaderSrv.uploadImages(files, this.supplier, 'logoImage', false, Client.TEAM).subscribe();
+		this.uploaderSrv.uploadImages(files, this.entity, 'logoImage', false, Client.TEAM).subscribe();
 	}
 
 	private setInitials(text?: string) {
-		// we just take the first 3 separate strings
-		const splitName = text.split(' ', 3);
-		this.initials = splitName.map(char => char.length ? char[0] : '').join('');
+		if (text) {
+			// we just take the first 3 separate strings
+			const splitName = text.split(' ', 3);
+			this.initials = splitName.map(char => char.length ? char[0] : '').join('');
+		} else {
+			this.initials = '-';
+		}
 	}
 
 }
