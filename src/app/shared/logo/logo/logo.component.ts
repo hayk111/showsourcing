378f22892a60343837b1d3bb11be872bd4c6c7a1
Michael Vanegas <michael@showsourcing.com>
2019-11-19 17:24:54 +0100

    updated initials condition and ofnt sizes

--- src/app/shared/logo/logo/logo.component.ts
+++ src/app/shared/logo/logo/logo.component.ts
@@ -43,14 +43,14 @@ export const iconMap = {
 
 export type Size = 'xs' | 's' | 'ms' | 'm' | 'l' | 'xl' | 'xxl';
 
-export const sizeMap: { [key in Size]: { background: number, icon: number } } = {
-	xs: { background: 14, icon: 10 },
-	s: { background: 20, icon: 12 },
-	ms: { background: 27, icon: 12 },
-	m: { background: 32, icon: 16 },
-	l: { background: 36, icon: 24 },
-	xl: { background: 54, icon: 24 },
-	xxl: { background: 88, icon: 40 }
+export const sizeMap: { [key in Size]: { background: number, icon: number, font: number } } = {
+	xs: { background: 14, icon: 10, font: 2 },
+	s: { background: 20, icon: 12, font: 4 },
+	ms: { background: 27, icon: 12, font: 6 },
+	m: { background: 32, icon: 16, font: 8 },
+	l: { background: 36, icon: 24, font: 10 },
+	xl: { background: 54, icon: 24, font: 16 },
+	xxl: { background: 88, icon: 40, font: 32 }
 };
 
 @Component({
