af308bc6a78700a68fab48551e4782d3f4e31ec2
cedvdb <cedvandenbosch@gmail.com>
2019-11-27 12:32:45 +0100

    fuck you

--- projects/supplier/src/environments/environment.prod.ts
+++ projects/supplier/src/environments/environment.prod.ts
@@ -5,8 +5,8 @@ import { LogLevel } from '~utils';
 export const environment = {
 	production: true,
 	version: WEB_VERSION,
-	graphqlUrl: 'wss://showsourcingprod.us1.cloud.realm.io/graphql',
-	graphqlAuthUrl: 'https://showsourcingprod.us1.cloud.realm.io/auth',
+	graphqlUrl: 'wss://showsourcingdev.us1.cloud.realm.io/graphql',
+	graphqlAuthUrl: 'https://showsourcingdev.us1.cloud.realm.io/auth',
 	apiUrl: 'https://services.showsourcing.com',
 	mixPanelKey: 'b59449e8af12d91ac73a9aa92b1a29a7',
 	hubspotKey: '2134370',
