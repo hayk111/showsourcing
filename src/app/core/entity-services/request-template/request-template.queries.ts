2bbb57c15aad33bbbd9131d5ee778e78c8920201
cedvdb <cedvandenbosch@gmail.com>
2019-11-27 18:01:40 +0100

    remove unused attribute of requestTemplate

--- src/app/core/entity-services/request-template/request-template.queries.ts
+++ src/app/core/entity-services/request-template/request-template.queries.ts
@@ -9,21 +9,18 @@ export abstract class RequestTemplateQueries extends GlobalQueries {
 	static readonly one = `
 		name
 		targetedEntity
-		${RequestTemplateQueries.requestedFields}
 		${RequestTemplateQueries.fields}
 	`;
 
 	static readonly many = `
 		name
 		targetedEntity
-		${RequestTemplateQueries.requestedFields}
 		${RequestTemplateQueries.fields}
 		`;
 
 	static readonly all = `
 		name
 		targetedEntity
-		${RequestTemplateQueries.requestedFields}
 		${RequestTemplateQueries.fields}
 		`;
 
