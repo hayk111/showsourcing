c60deab0d4fcf93d82122643ad42367792893dc6
Michael Vanegas <michael@showsourcing.com>
2019-11-29 11:30:55 +0100

    revert me

--- src/app/features/dev/pages/playground/playground-page.component.ts
+++ src/app/features/dev/pages/playground/playground-page.component.ts
@@ -1,5 +1,5 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-
+import { StatusUtils, StatusCategory, Color } from '~utils';
 @Component({
 	selector: 'playground-page-app',
 	templateUrl: './playground-page.component.html',
@@ -8,10 +8,15 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 })
 export class PlaygroundPageComponent {
 
-
+	lep = StatusUtils.statusColorMap;
+	mep = StatusUtils.statusColorMap;
 
 	test() {
 		// Test function
+		console.log(this.lep);
+		this.lep[StatusCategory.NEW] = Color.WARN;
+		console.log(this.lep);
+		console.log(this.mep);
 	}
 
 }
--- src/app/utils/status.utils.ts
+++ src/app/utils/status.utils.ts
@@ -1,4 +1,5 @@
 import { Color } from '~utils/colors.enum';
+import { Status } from '~core/models/status.model';
 
 export enum StatusCategory {
 	NEW = 'new',
@@ -8,10 +9,20 @@ export enum StatusCategory {
 	REFUSED = 'refused',
 }
 
-export const statusColorMap = {
-	[StatusCategory.NEW]: Color.SECONDARY,
-	[StatusCategory.PREPARATION]: Color.SECONDARY_LIGHT,
-	[StatusCategory.IN_PROGRESS]: Color.PRIMARY,
-	[StatusCategory.VALIDATED]: Color.SUCCESS,
-	[StatusCategory.REFUSED]: Color.WARN,
-};
+export class StatusUtils {
+
+	static statusColorMap = {
+		[StatusCategory.NEW]: Color.SECONDARY,
+		[StatusCategory.PREPARATION]: Color.SECONDARY_LIGHT,
+		[StatusCategory.IN_PROGRESS]: Color.PRIMARY,
+		[StatusCategory.VALIDATED]: Color.SUCCESS,
+		[StatusCategory.REFUSED]: Color.WARN,
+	};
+
+	static defaultStatusColor = StatusUtils.statusColorMap[StatusCategory.NEW];
+
+	getStatusColor(status: Status) {
+
+	}
+
+}
