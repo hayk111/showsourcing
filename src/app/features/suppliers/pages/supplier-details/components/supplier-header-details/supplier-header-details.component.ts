39b79ca3268a4de8c047102e1f9dc40107aa4ae2
Michael Vanegas <michael@showsourcing.com>
2019-12-10 10:14:19 +0100

    WEB-2609 added select counts supplier header

--- src/app/features/suppliers/pages/supplier-details/components/supplier-header-details/supplier-header-details.component.ts
+++ src/app/features/suppliers/pages/supplier-details/components/supplier-header-details/supplier-header-details.component.ts
@@ -1,7 +1,10 @@
 import { Location } from '@angular/common';
-import { ChangeDetectionStrategy, Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { ChangeDetectionStrategy, Component, EventEmitter, Input, OnChanges, Output } from '@angular/core';
+import { Observable } from 'rxjs';
+import { ContactService, ProductService, SampleService, TaskService, UserService } from '~core/entity-services';
 import { Supplier } from '~models';
 import { TrackingComponent } from '~utils/tracking-component';
+import { distinctUntilChanged } from 'rxjs/operators';
 
 @Component({
 	selector: 'supplier-header-details-app',
@@ -9,21 +12,37 @@ import { TrackingComponent } from '~utils/tracking-component';
 	styleUrls: ['./supplier-header-details.component.scss'],
 	changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class SupplierHeaderDetailsComponent extends TrackingComponent implements OnInit {
+export class SupplierHeaderDetailsComponent extends TrackingComponent implements OnChanges {
 	@Input() supplier: Supplier;
 	@Output() delete = new EventEmitter<Supplier>();
 	@Output() archive = new EventEmitter<Supplier>();
 	@Output() export = new EventEmitter<Supplier>();
 	@Output() contact = new EventEmitter<Supplier>();
 
+	tasksCount$: Observable<number>;
+	samplesCount$: Observable<number>;
+	productsCount$: Observable<number>;
+	contactsCount$: Observable<number>;
+
 	constructor(
-		private location: Location
+		private location: Location,
+		private sampleSrv: SampleService,
+		private productSrv: ProductService,
+		private taskSrv: TaskService,
+		private contactSrv: ContactService
 	) {
 		super();
 	}
 
-
-	ngOnInit() {
+	ngOnChanges() {
+		this.tasksCount$ = this.taskSrv.selectCount(`supplier.id == "${this.supplier.id}" AND deleted == false AND archived == false`)
+			.pipe(distinctUntilChanged());
+		this.samplesCount$ = this.sampleSrv.selectCount(`supplier.id == "${this.supplier.id}" AND deleted == false AND archived == false`)
+			.pipe(distinctUntilChanged());
+		this.productsCount$ = this.productSrv.selectCount(`supplier.id == "${this.supplier.id}" AND deleted == false AND archived == false`)
+			.pipe(distinctUntilChanged());
+		this.contactsCount$ = this.contactSrv.selectCount(`supplier.id == "${this.supplier.id}" AND deleted == false`)
+			.pipe(distinctUntilChanged());
 	}
 
 	goBack() {
