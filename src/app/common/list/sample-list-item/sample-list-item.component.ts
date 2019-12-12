3aa705d0128f443d016e3716599a42e642103a5d
Michael Vanegas <michael@showsourcing.com>
2019-12-12 10:39:45 +0100

    revert me

--- src/app/common/list/sample-list-item/sample-list-item.component.ts
+++ src/app/common/list/sample-list-item/sample-list-item.component.ts
@@ -1,6 +1,7 @@
-import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input, EventEmitter, Output } from '@angular/core';
 import { TrackingComponent } from '~utils/tracking-component';
 import { Sample } from '~core/models';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
 	selector: 'sample-list-item-app',
@@ -10,5 +11,21 @@ import { Sample } from '~core/models';
 })
 export class SampleListItemComponent extends TrackingComponent {
 	@Input() sample: Sample;
+	@Input() hasSelection = true;
+	@Input() selection: Map<string, boolean>;
 
+	@Output() select = new EventEmitter<any>();
+	@Output() unselect = new EventEmitter<any>();
+
+	constructor(public translate: TranslateService) { super(); }
+
+	isSelected(row) {
+		if (!this.hasSelection)
+			return false;
+
+		if (this.selection)
+			return this.selection.has(row.id);
+
+		throw Error(`Selection Input is undefnied`);
+	}
 }
