8a797aafb61d4f9249beef0f9a84f289cd2df3fb
hayk3 <hayk@simplytechnologies.com>
2019-12-16 16:18:06 +0400

    Project list table content and UI fixes

--- src/app/common/tables/projects-table/projects-table.component.ts
+++ src/app/common/tables/projects-table/projects-table.component.ts
@@ -7,6 +7,7 @@ import { defaultConfig } from '../default-columns/default-config';
 
 const tableConfig: TableConfig = {
 	...defaultConfig,
+	projectLead: { name: 'projectLead', translationKey: 'project-lead', width: 152, sortProperty: 'createdBy.firstName' },
 };
 
 @Component({
@@ -15,11 +16,13 @@ const tableConfig: TableConfig = {
 	styleUrls: ['./projects-table.component.scss']
 })
 export class ProjectsTableComponent extends EntityTableComponent<Project> {
-	static DEFAULT_COLUMNS = [ 'logo', 'name', 'reference', 'productCount', 'status', 'dueDate', 'createdBy', 'creationDate' ];
+	static DEFAULT_COLUMNS = [ 'logo', 'name', 'projectLead', 'dueDate', 'status', 'createdBy', 'creationDate' ];
 	static DEFAULT_TABLE_CONFIG = tableConfig;
 	@Input() columns = ProjectsTableComponent.DEFAULT_COLUMNS;
 	@Input() tableConfig = ProjectsTableComponent.DEFAULT_TABLE_CONFIG;
 	@Input() navigation = true;
+
+	color = Color;
 
 	constructor(public translate: TranslateService) {
 		super();
