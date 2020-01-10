de8d4b5bdba27e6617ef693c9363a99abc7971d8
hayk111 <hayk.atoyan111@gmail.com>
2020-01-10 15:33:12 +0400

    Reimplements request list and UI bug fixes

--- src/app/common/tables/requests-table/requests-table.component.ts
+++ src/app/common/tables/requests-table/requests-table.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, EventEmitter, Output, TemplateRef, ViewChild } from '@angular/core';
+import { ChangeDetectionStrategy, Component, EventEmitter, Input, Output, TemplateRef, ViewChild } from '@angular/core';
 import { SupplierRequest } from '~core/models';
 import { TranslateService } from '@ngx-translate/core';
 import { EntityTableComponent, TableConfig } from '../entity-table.component';
@@ -7,12 +7,11 @@ const tableConfig: TableConfig = {
 	title: { name: 'title', translationKey: 'title', width: 300, sortProperty: 'title' },
 	type: { name: 'type', translationKey: 'type', width: 175, sortProperty: 'templateName' },
 	supplier: { name: 'supplier', translationKey: 'supplier', width: 220, sortProperty: 'recipient.company' },
-	sentTo: { name: 'sent to', translationKey: 'sent-to', width: 175, sortProperty: 'recipient.name' },
+	sentTo: { name: 'sentTo', translationKey: 'sent-to', width: 175, sortProperty: 'recipient.name' },
 	requests: { name: 'number requests', translationKey: 'requests', width: 90, sortable: false },
 	status: { name: 'status', translationKey: 'status', width: 175, sortProperty: 'status' },
-	sentOn: { name: 'sent on', translationKey: 'sent-on', width: 175, sortProperty: 'sentDate' },
-	sender: { name: 'sender', translationKey: 'sender', width: 175, sortProperty: 'sender.name' },
-	statusCreateInfo: { name: 'status templateName createInfo', translationKey: '', width: 450, sortable: false },
+	sentOn: { name: 'sentOn', translationKey: 'sent-on', width: 175, sortProperty: 'sentDate' },
+	sender: { name: 'sender', translationKey: 'sent-by', width: 175, sortProperty: 'sender.name' },
 };
 
 @Component({
@@ -24,10 +23,13 @@ const tableConfig: TableConfig = {
 	changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class RequestsTableComponent extends EntityTableComponent<SupplierRequest> {
-	columns = ['title', 'type', 'supplier', 'sentTo', 'requests', 'status', 'sentOn', 'sender'];
+	static DEFAULT_COLUMNS = ['title', 'type', 'supplier', 'sentTo', 'requests', 'status', 'sentOn', 'sender'];
+	static DEFAULT_TABLE_CONFIG = tableConfig;
+	@Input() columns = RequestsTableComponent.DEFAULT_COLUMNS;
+	@Input() tableConfig = RequestsTableComponent.DEFAULT_TABLE_CONFIG;
+
 	@Output() cancelRequest = new EventEmitter<SupplierRequest>();
 	@ViewChild('contextualMenu', { static: false }) contextualMenuTemplate: TemplateRef<any>;
-	tableConfig = tableConfig;
 
 	constructor(public translate: TranslateService) { super(); }
 
