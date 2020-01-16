33fbdbf96af23e54bda0a23d515a92eff5cfb382
cedvdb <cedvandenbosch@gmail.com>
2020-01-16 15:21:50 +0100

    add dot

--- src/app/core/i18n/missing-translation.service.ts
+++ src/app/core/i18n/missing-translation.service.ts
@@ -2,7 +2,7 @@ import {MissingTranslationHandler, MissingTranslationHandlerParams} from '@ngx-t
 
 export class AppMissingTranslationHandler implements MissingTranslationHandler {
 	handle(params: MissingTranslationHandlerParams) {
-		if (params.key.startsWith('_user-defined.')) {
+		if (params.key.startsWith('_user-defined..')) {
 			return params.key.split('.').slice(2).join('.');
 		} else {
 			return params.key;
