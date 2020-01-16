b51bf1819c35410df2c6bc1b67c242fd54ae5143
cedvdb <cedvandenbosch@gmail.com>
2020-01-16 13:13:46 +0100

    translation

--- src/app/core/i18n/missing-translation.service.ts
+++ src/app/core/i18n/missing-translation.service.ts
@@ -2,8 +2,10 @@ import {MissingTranslationHandler, MissingTranslationHandlerParams} from '@ngx-t
 
 export class AppMissingTranslationHandler implements MissingTranslationHandler {
 	handle(params: MissingTranslationHandlerParams) {
-		// since we use nested properties we will use the '.' to get the original translation.
-		// This is a quick fix, ultimately translation should be on one level then
-		return params.key.split('.').pop();
+		if (params.key.startsWith('_user-defined.')) {
+			return params.key.split('.').slice(2).join('.');
+		} else {
+			return params.key;
+		}
 	}
 }
