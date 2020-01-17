a083b9c7e8ea108c0737d4f5886611e4b2339200
Michael Vanegas <michael@showsourcing.com>
2020-01-17 11:26:29 +0100

    added counts to team performance

--- src/app/features/dashboard/pages/dashboard/components/team-performance/team-performance.component.ts
+++ src/app/features/dashboard/pages/dashboard/components/team-performance/team-performance.component.ts
@@ -1,12 +1,11 @@
-import { Component, OnInit, ChangeDetectionStrategy, Output, EventEmitter, ChangeDetectorRef } from '@angular/core';
-import { TrackingComponent } from '~utils/tracking-component';
-import { RPCActionTypes, RPCRequestStatus, Task, Sample } from '~models';
-import { RpcService, TaskService, SampleService, UserService } from '~core/entity-services';
-import { ChartDataSets } from 'chart.js';
-import { Observable, of, TimeoutError } from 'rxjs';
-import { map, first, tap, catchError } from 'rxjs/operators';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { DialogCommonService } from '~common/dialogs/services/dialog-common.service';
+import { ProductService, SampleService, SupplierService, TaskService, UserService } from '~core/entity-services';
 import { SelectParams } from '~core/entity-services/_global/select-params';
+import { Sample, Task } from '~models';
+import { TrackingComponent } from '~utils/tracking-component';
 
 @Component({
 	selector: 'team-performance-app',
@@ -16,80 +15,40 @@ import { SelectParams } from '~core/entity-services/_global/select-params';
 })
 export class TeamPerformanceComponent extends TrackingComponent implements OnInit {
 
-	teamStats$: Observable<any>;
-	lineChartData: Array<ChartDataSets> = [];
-	status = 'pending';
-
 	tasks$: Observable<Task[]>;
 	samples$: Observable<Sample[]>;
 
-	totalProducts = 0;
-	totalSuppliers = 0;
-	productsThisWeek = 0;
-	suppliersThisWeek = 0;
-
-	requestTimedOut = false;
-
-	@Output() inviteTeam = new EventEmitter<null>();
+	products$: Observable<number>;
+	suppliers$: Observable<number>;
+	productsAssignedToMe$: Observable<number>;
+	suppliersAssignedToMe$: Observable<number>;
 
 	constructor(
-		private rpcSrv: RpcService,
-		private cdr: ChangeDetectorRef,
 		private route: Router,
 		private taskSrv: TaskService,
 		private sampleSrv: SampleService,
-		private userSrv: UserService
+		private supplierSrv: SupplierService,
+		private productSrv: ProductService,
+		private userSrv: UserService,
+		public dlgCommonSrv: DialogCommonService
 	) {
 		super();
 	}
 
 	ngOnInit() {
 		const userId = this.userSrv.userId;
+
 		let selectParams = new SelectParams({ query: `assignee.id == "${userId}" AND done == false` });
 		this.tasks$ = this.taskSrv.queryMany(selectParams);
+
 		selectParams = { ...selectParams, query: `assignee.id == "${userId}"` };
 		this.samples$ = this.sampleSrv.queryMany(selectParams);
 
-		this.teamStats$ = this.rpcSrv.createRPC({
-			action: RPCActionTypes.GET_TEAM_STATS,
-		}).pipe(
-			catchError(err => {
-				if (err instanceof TimeoutError) {
-					this.requestTimedOut = true;
-					this.cdr.detectChanges();
-				}
+		this.products$ = this.productSrv.queryCount(`deleted == false AND archived == false`);
+		this.productsAssignedToMe$ = this.productSrv.queryCount(`deleted == false AND archived == false AND assignee.id == "${userId}"`);
 
-				return of();
-			}),
-			first(),
-			map((data: any) => JSON.parse(JSON.parse(data.reply))),
-			tap((teamStats: any) => {
-				this.totalProducts = teamStats.products.total;
-				this.productsThisWeek = teamStats.products.week0;
-				this.totalSuppliers = teamStats.suppliers.total;
-				this.suppliersThisWeek = teamStats.suppliers.week0;
-			}),
-			map((data: any) => {
-				const items = [];
-				const teamStats = data;
-
-				for (const key of Object.keys(teamStats)) {
-					const item: any = {};
-					item.label = key;
-					item.data = [];
-
-					for (const dataKey of Object.keys(teamStats[key])) {
-						if (dataKey.startsWith('week')) {
-							item.data.push(teamStats[key][dataKey]);
-						}
-					}
-
-					item.data.reverse();
-					items.push(item);
-				}
-
-				return items;
-			}));
+		this.suppliers$ = this.supplierSrv.queryCount(`deleted == false AND archived == false`);
+		this.suppliersAssignedToMe$ = this.supplierSrv.queryCount(`deleted == false AND archived == false AND assignee.id == "${userId}"`);
 	}
 
 	redirect(path: string) {
