8d0afcf84943a63278c983acc48d03092630bf8b
Michael Vanegas <michael@showsourcing.com>
2020-01-20 12:09:18 +0100

    added index fix

--- src/app/shared/selectors/components/index.ts
+++ src/app/shared/selectors/components/index.ts
@@ -1,4 +1,5 @@
 export * from './cdk-overlay/cdk-overlay.component';
+export * from './selector-options';
 export * from './selector-picker/selector-picker.component';
 export * from './selector-placeholder/selector-placeholder.component';
 export * from './selector/selector.component';
