71818abe5c793cdcffa2aa93975171046e9e2dac
Michael Vanegas <michael@showsourcing.com>
2020-01-27 16:44:15 +0100

    changed folder name

--- src/app/shared/selectors/components/index.ts
+++ src/app/shared/selectors/components/index.ts
@@ -2,4 +2,4 @@ export * from './cdk-overlay/cdk-overlay.component';
 export * from './selector-options';
 export * from './selector-picker/selector-picker.component';
 export * from './selector-placeholder/selector-placeholder.component';
-export * from './selector/selector.component';
+export * from './selector-container/selector.component';
