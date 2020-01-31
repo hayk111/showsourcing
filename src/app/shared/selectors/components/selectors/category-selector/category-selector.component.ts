23a76f3413fe4ab756fe55afd655b09063314a9c
Michael Vanegas <michael@showsourcing.com>
2020-01-31 14:57:29 +0100

    added category selector

---
+++ src/app/shared/selectors/components/selectors/category-selector/category-selector.component.ts
@@ -0,0 +1,90 @@
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { CategoryService } from '~core/entity-services';
+import { Category, ERM } from '~core/models';
+import { SelectorConfig, SelectorsService } from '~shared/selectors/services/selectors.service';
+
+import { AbstractSelectorComponent } from '../../abstract-selector.components';
+
+@Component({
+	selector: 'category-selector-app',
+	templateUrl: './category-selector.component.html',
+	styleUrls: ['./category-selector.component.scss'],
+	changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class CategorySelectorComponent extends AbstractSelectorComponent implements OnInit {
+
+	searchQuery = (text: string) => {
+		return `name CONTAINS[c] "${text}"`;
+	}
+	itemsMatchesName = (items: any[], name: string) => {
+		return items.filter(item => item.name === name);
+	}
+	isStoredFn(item) {
+		return !!this.value.find(value => value.id === item.id);
+	}
+	areStoredMatchesNameFn(name) {
+		return !!this.value.find(val => val.name.toLowerCase() === name);
+	}
+	itemNotStoredFn(item) {
+		return !((this.value || []).some(val => val.id === item.id));
+	}
+	deleteStoredItemFn(item) {
+		return this.value.filter(val => val.id !== item.id);
+	}
+
+	config: SelectorConfig;
+
+	constructor(
+		protected categorySrv: CategoryService,
+		protected selectorSrv: SelectorsService,
+		protected cd: ChangeDetectorRef
+	) { super(selectorSrv, cd); }
+
+	setup() {
+		this.config = {
+			entitySrv: this.categorySrv,
+			entityMetadata: ERM.CATEGORY,
+			searchQuery: this.searchQuery,
+			itemsMatchesName: this.itemsMatchesName,
+			itemsNotStored: this.itemsNotStored,
+			areStoredMatchesName: this.areStoredMatchesName,
+			initialFilters: this.filterList.asFilters(),
+			initialSeachTxt: this.initialSeachTxt
+		};
+		this.selectorSrv.setup(this.config);
+	}
+
+	updateSingleFn() {
+		const item = {
+			id: this.value.id,
+			name: this.value.name ? this.value.name : '',
+			__typename: this.value.__typename
+		};
+		return item;
+	}
+
+	updateMultipleFn() {
+		const trimValues = this.value.map(v => ({ id: v.id, name: v.name, __typename: v.__typename }));
+		return trimValues;
+	}
+
+	createFn(): any {
+		const name = this.selectorSrv.searchText;
+		let item;
+		let createObs$: Observable<Category>;
+		if (name) {
+			item = new Category({ name });
+			createObs$ = this.categorySrv.create(item);
+		}
+
+		if (createObs$ === undefined)
+			return;
+
+		createObs$.subscribe();
+		return item;
+	}
+
+
+
+}
