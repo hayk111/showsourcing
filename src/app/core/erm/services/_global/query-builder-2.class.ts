a56ca4f6467d59efde36a16e3ea072e42017ef53
augustin <doat.augustin@gmail.com>
2020-02-18 12:48:19 +0100

    remove comments and indent queries

--- src/app/core/erm/services/_global/query-builder-2.class.ts
+++ src/app/core/erm/services/_global/query-builder-2.class.ts
@@ -44,138 +44,116 @@ export class QueryBuilder {
 		this.customQueries = customQueries;
 	}
 
-	// TODO selectOne is no longer used (need to be removed in the application)
+	// TODO selectOne is no longer used (need to be removed from the app)
 
 	// get
 	queryOne = (str: string) => {
 		let query = `
-query Get${this.entityName}($teamId: ID!, $id: ID!) {
-  get${this.entityName}(teamId: $teamId, id: $id) {
-    id
-		teamId
-		${str}
-		${this.audit}
-  }
-}`;
+			query Get${this.entityName}($teamId: ID!, $id: ID!) {
+				get${this.entityName}(teamId: $teamId, id: $id) {
+					id
+					teamId
+					${str}
+					${this.audit}
+				}
+			}`;
 		if (this.customQueries?.queryOne) {
 			query = this.customQueries.queryOne;
 		}
 		gql(query);
 	};
 
-	// TODO selectMany is no longer used (need to be removed in the app)
-)
+	// TODO selectMany is no longer used (need to be removed from the app)
 
-	// TODO verify with a real search query (this one is suposed)
+
+	// TODO replace the query with a real search query (this one doesn't exist on the queries.graphql but should look's like this)
 	// search
 	queryMany = (str: string) => {
 		let query = `
-		query Search${this.entityName}s(
-	$filter: Searchable${this.entityName}FilterInput
-	$teamId: ID
-  $id: ModelIDKeyConditionInput
-  $sort: Searchable${this.entityName}SortInput
-  $limit: Int
-  $nextToken: String
-) {
-  search${this.entityName}s(
-    filter: $filter
-    sort: $sort
-    limit: $limit
-    nextToken: $nextToken
-  ) {
-    items {
-			id
-			teamId
-			${str}
-			${this.audit}
-    }
-    nextToken
-    total
-  }
-}
-`;
+			query Search${this.entityName}s(
+				$filter: Searchable${this.entityName}FilterInput
+				$teamId: ID
+				$id: ModelIDKeyConditionInput
+				$sort: Searchable${this.entityName}SortInput
+				$limit: Int
+				$nextToken: String
+			) {
+				search${this.entityName}s(
+					filter: $filter
+					sort: $sort
+					limit: $limit
+					nextToken: $nextToken
+				) {
+					items {
+						id
+						teamId
+						${str}
+						${this.audit}
+					}
+					nextToken
+					total
+				}
+			}`;
 		if (this.customQueries?.queryMany) {
 			query = this.customQueries.queryMany;
 		}
 		gql(query);
 	};
 
-	// TODO selectAll is no longer used (need to be removed)
-	// selectAll = (str: string) => gql(`
-	// 	subscription ${this.entityName}s {
-	// 		${this.entityName}s {
-	// 			items {
-	// 				id
-	// 				${str}
-	// 			},
-	// 			count
-	// 		}
-	// 	}`)
-
+	// TODO selectAll is no longer used (need to be removed from the app)
 	// list
 	queryAll = (str: string) => {
 		let query = `
-	query List${this.entityName}(
-  $teamId: ID
-  $id: ModelIDKeyConditionInput
-  $filter: ModelContactFilterInput
-  $limit: Int
-  $nextToken: String
-  $sortDirection: ModelSortDirection
-) {
-  list${this.entityName}(
-    teamId: $teamId
-    id: $id
-    filter: $filter
-    limit: $limit
-    nextToken: $nextToken
-    sortDirection: $sortDirection
-  ) {
-    items {
-      id
-      teamId
-			${str}
-			${this.audit}
-    }
-    nextToken
-  }
-}`;
+			query List${this.entityName}(
+				$teamId: ID
+				$id: ModelIDKeyConditionInput
+				$filter: ModelContactFilterInput
+				$limit: Int
+				$nextToken: String
+				$sortDirection: ModelSortDirection
+			) {
+				list${this.entityName}(
+					teamId: $teamId
+					id: $id
+					filter: $filter
+					limit: $limit
+					nextToken: $nextToken
+					sortDirection: $sortDirection
+				) {
+					items {
+						id
+						teamId
+						${str}
+						${this.audit}
+					}
+					nextToken
+				}
+			}`;
 		if (this.customQueries?.queryAll) {
 			query = this.customQueries.queryAll;
 		}
 		gql(query);
 	};
 
-	// TODO use Search to do it
-	// queryCount = () => gql(`
-	// 	query ${this.entityName}sCount($query: String) {
-	// 		${this.entityName}s(query: $query) {
-	// 			count
-	// 		}
-	// 	}`)
+	// TODO use Search to replace queryCount
 
-	// TODO selectCount is no longer used (need to be removed)
-	// selectCount = () => gql(`
-	// 	subscription ${this.entityName}sCount($query: String) {
-	// 		${this.entityName}s(query: $query) {
-	// 			count
-	// 		}
-	// 	}`)
+
+	// TODO selectCount is no longer used (need to be removed from the app)
+
 
 	create = (str: string) => {
 		let mutation = `
-		mutation Create${this.entityName}(
-  $input: Create${this.entityName}Input!
-  $condition: Model${this.entityName}ConditionInput
-) {
-  create${this.entityName}(input: $input, condition: $condition) {
-    id
-		teamId
-		${str}
-		${this.audit}
-  }
-}}`;
+			mutation Create${this.entityName}(
+				$input: Create${this.entityName}Input!
+				$condition: Model${this.entityName}ConditionInput
+			) {
+				create${this.entityName}(input: $input, condition: $condition) {
+					id
+					teamId
+					${str}
+					${this.audit}
+				}
+			}`;
 		if (this.customQueries?.create) {
 			mutation = this.customQueries.create;
 		}
@@ -184,17 +162,17 @@ query Get${this.entityName}($teamId: ID!, $id: ID!) {
 
 	update = (str: string) => {
 		let mutation = `
-		mutation Update${this.entityName}(
-  $input: Update${this.entityName}Input!
-  $condition: Model${this.entityName}ConditionInput
-) {
-  update${this.entityName}(input: $input, condition: $condition) {
-    id
-    teamId
-		${str}
-		${this.audit}
-  }
-}`;
+			mutation Update${this.entityName}(
+				$input: Update${this.entityName}Input!
+				$condition: Model${this.entityName}ConditionInput
+			) {
+				update${this.entityName}(input: $input, condition: $condition) {
+					id
+					teamId
+					${str}
+					${this.audit}
+				}
+			}`;
 		if (this.customQueries?.update) {
 			mutation = this.customQueries.update;
 		}
@@ -202,28 +180,21 @@ query Get${this.entityName}($teamId: ID!, $id: ID!) {
 	};
 
 	// TODO updateMany is no longer used (need to be managed in global service)
-	// updateMany = (str: string) => gql(`
-	// mutation updateMany${this.entityName}s($input: [${this.entityName}Input!]){
-	// 	create${this.entityName}s(input: $input, updatePolicy: MODIFIED) {
-	// 		${str}
-	// 	}
-	// }
-	// `)
 
-	// TODO change to delete for keep coherence
+
 	deleteOne = (str = "") => {
 		let mutation = `
-		mutation Delete${this.entityName}(
-  $input: Delete${this.entityName}Input!
-  $condition: Model${this.entityName}ConditionInput
-) {
-  delete${this.entityName}(input: $input, condition: $condition) {
-    id
-    teamId
-    ${str}
-		${this.audit}
-  }
-}`;
+			mutation Delete${this.entityName}(
+				$input: Delete${this.entityName}Input!
+				$condition: Model${this.entityName}ConditionInput
+			) {
+				delete${this.entityName}(input: $input, condition: $condition) {
+					id
+					teamId
+					${str}
+					${this.audit}
+				}
+			}`;
 		if (this.customQueries?.deleteOne) {
 			mutation = this.customQueries.deleteOne;
 		}
@@ -232,21 +203,10 @@ query Get${this.entityName}($teamId: ID!, $id: ID!) {
 
 
 	// TODO deleteMany is no longer used (need to be managed in global service)
-	// deleteMany = () => gql(`
-	// 	mutation delete${this.entityName}s($query: String!) {
-	// 		delete${this.entityName}s(query: $query)
-	// 	}`)
 
-	// TODO openSubscription is no longer used (need to be removed)
-	// openSubscription = (query: string) => gql(`
-	// 	mutation create${this.entityName}Subscription {
-	// 		create${this.entityName}Subscription(name: "${this.entityName}-subscription", query: "${query}") {
-	// 			items {
-	// 				id@skip(if: true)
-	// 			}
-	// 		}
-	// 	}
-	// `)
+
+	// TODO openSubscription is no longer used (need to be removed from the app)
+
 
 	private capitalize(str: string): string {
 		return str.charAt(0).toUpperCase() + str.slice(1);
