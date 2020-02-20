4b62f1f302080bf7dbb3187a15200025c4b3e9f2
cedvdb <cedvandenbosch@gmail.com>
2020-02-20 14:37:47 +0100

    fix wrong custom query on getUser

--- src/app/core/erm/services/user/user.service.ts
+++ src/app/core/erm/services/user/user.service.ts
@@ -11,8 +11,8 @@ import { GlobalService } from '~core/erm/services/_global/global.service-2';
 
 const customQueries = {
 	queryOne: `
-		query GetUser(id: $ID) {
-			user(id: $id) {
+		query GetUser($ID: ID!) {
+			user(id: $ID) {
 				id
 				firstName
 				lastName
