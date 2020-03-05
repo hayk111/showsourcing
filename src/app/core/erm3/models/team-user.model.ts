5c5e956526c35031cd2cb58646851fcafe157a49
augustin <doat.augustin@gmail.com>
2020-03-05 14:41:13 +0100

    update nested models in progress

--- src/app/core/erm3/models/team-user.model.ts
+++ src/app/core/erm3/models/team-user.model.ts
@@ -1,13 +1,19 @@
 
-import { User } from './user.model';
+import { TeamRole } from '../../../API.service';
 import { Team } from './team.model';
+import { User } from './user.model';
+import { Entity } from './_entity.model';
 
-export class TeamUser {
-	id: string;
-	teamId: string;
-	team: Team;
-	userId: string;
-	user: User;
-	role: string;
+export class TeamUser extends Entity<TeamUser> {
+	__typename ?= 'TeamUser';
+	teamId?: string;
+	userId?: string;
+	team?: Team;
+	user?: User;
+	role?: TeamRole;
+	_version?: number;
+	_deleted?: boolean;
+	_lastChangedAt?: number;
 }
 
+// TODO mission audits
