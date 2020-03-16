e104311760d2e3fea72b01a8050e43447cf9ab48
augustin <doat.augustin@gmail.com>
2020-03-16 14:48:04 +0100

    draft fuse-helper. the getFilteredItems$ is not subscribable.

--- src/app/core/list-page2/list-fuse-helper.service.ts
+++ src/app/core/list-page2/list-fuse-helper.service.ts
@@ -2,19 +2,32 @@ import { Injectable } from '@angular/core';
 import { SelectionService } from './selection.service';
 import { ApiService, ObservableQuery } from '~core/erm3/services/api.service';
 import { FilterService } from '~core/filters/filter.service';
+import * as Fuse from 'fuse.js';
 import { switchMap, tap, map, shareReplay } from 'rxjs/operators';
-import { forkJoin, Observable } from 'rxjs';
+import { forkJoin, Observable, from } from 'rxjs';
 import { Typename } from '~core/erm3/typename.type';
 import * as models from '~core/erm3/models';
-
+import gql from 'graphql-tag';
+import { FilterType } from '~core/filters';
+import { FetchPolicy } from 'apollo-client';
+import { DocumentNode } from 'graphql';
 
 @Injectable({ providedIn: 'root' })
 export class ListFuseHelperService<G = any> {
-
 	private queryRef: ObservableQuery<G[]>;
 	private typename: Typename;
+	private fuse: any;
 	pending = true;
 	total$: Observable<number>;
+
+	fuseOptions = {
+		shouldSort: true,
+		threshold: 0.6,
+		location: 0,
+		distance: 100,
+		minMatchCharLength: 1,
+		keys: ['name']
+	};
 
 	constructor(
 		private selectionSrv: SelectionService,
@@ -22,48 +35,62 @@ export class ListFuseHelperService<G = any> {
 		private filterSrv: FilterService
 	) {}
 
-	setup(typename: Typename, byTypename: Typename | 'Owner', byId: string) {
-		// TODO query with listBy/cache-first for setup the helper and be able to refetch: we need typename and byTypename and byId
-		// TODO initialize Fuse to have 'allItems' ready to search
+	setup(typename: Typename, query: DocumentNode, variables?, searchedFields?: string[]) {
+		/** this query is custom to test how is working the helper. It should be listBy
+		 *  cache-and-network for the first query to get the up to date datas
+		 */
+		this.typename = typename;
+		this.queryRef = this.apiSrv.query<G[]>({ variables, query, fetchPolicy: 'cache-and-network' });
+		this.fuseOptions.keys = searchedFields || this.fuseOptions.keys; // ? could be take from filterSrv ?
+		// when we refetch and get new datas it will reasign fuse
+		this.queryRef.data$.subscribe(datas => {
+			this.fuse = new Fuse(datas, this.fuseOptions);
+		});
 	}
 
-	getFilteredItems$() {
-		// TODO use filterSrv to search with fuse on allItems
+	getFilteredItems$(): Observable<G[]> {
+		return this.filterSrv.valueChanges$.pipe(
+			// the value changed should concern the FilterType search
+			map(() => this.fuse.search(this.filterSrv.getFiltersForType(FilterType.SEARCH))),
+			shareReplay(1)
+		);
+		// result.sort(); // ? should take sort property from filterSrv, not implemented yet
+		// ? should trigger getFilteredItems$() when sort is updated
+		// ? update pagination
 	}
 
-	private refetch() {
-		// TODO refetch the query and reinitialise Fuse
-		// this.pending = true;
-		// return this.queryRef.refetch({ fetchPolicy: 'network-only' })
-		// .then(_ => this.pending = false);
+	private refetch(fetchPolicy: FetchPolicy = 'network-only') {
+		this.pending = true;
+		return this.queryRef.refetch({ fetchPolicy }).then(_ => (this.pending = false));
 	}
 
 	create(entity: any) {
-		// TODO create entity and refetch network-only
-		// this.apiSrv.create(this.typename, entity).pipe(
-		// 	switchMap(_ => this.refetch())
-		// ).subscribe();
+		this.apiSrv
+			.create(this.typename, entity)
+			.pipe(switchMap(_ => this.refetch()))
+			.subscribe();
 	}
 
 	update(entity: any) {
-		// TODO update entity and refetch in cache-first !
-		// this.apiSrv.update(this.typename, entity);
+		this.apiSrv
+			.update(this.typename, entity)
+			.pipe(switchMap(_ => this.refetch('cache-first')))
+			.subscribe();
 	}
 
 	delete(entity: any) {
-		// TODO delete entity and refetch network-only
-		// this.apiSrv.delete(this.typename, entity).pipe(
-		// 	switchMap(_ => this.refetch())
-		// ).subscribe();
+		this.apiSrv
+			.delete(this.typename, entity)
+			.pipe(switchMap(_ => this.refetch()))
+			.subscribe();
 	}
 
 	deleteSelected() {
-		// ? same behavior
-		// const selected = this.selectionSrv.getSelectedValues();
-		// const all = selected.map(entity => this.apiSrv.delete(this.typename, entity));
-		// forkJoin(all).pipe(
-		// 	switchMap(_ => this.refetch())
-		// ).subscribe();
+		const selected = this.selectionSrv.getSelectedValues();
+		const all = selected.map(entity => this.apiSrv.delete(this.typename, entity));
+		forkJoin(all)
+			.pipe(switchMap(_ => this.refetch()))
+			.subscribe();
 	}
 
 	loadMore() {
@@ -94,5 +121,4 @@ export class ListFuseHelperService<G = any> {
 	sort() {
 		throw Error('not implemented');
 	}
-
 }
--- src/app/features/dev/pages/playground/playground-page.component.ts
+++ src/app/features/dev/pages/playground/playground-page.component.ts
@@ -1,4 +1,8 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { ListFuseHelperService } from '~core/list-page2/list-fuse-helper.service';
+import gql from 'graphql-tag';
+import { FilterService } from '~core/filters';
+import { Category } from '~core/erm3/models';
 
 @Component({
 	selector: 'playground-page-app',
@@ -6,10 +10,59 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 	styleUrls: ['./playground-page.component.scss'],
 	changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class PlaygroundPageComponent {
+export class PlaygroundPageComponent implements OnInit {
+	constructor(private listFuseHelper: ListFuseHelperService<Category>, private filterSrv: FilterService) {}
+
+	ngOnInit() {
+		this.listFuseHelper.setup(
+			'Category',
+			gql`
+				query ListCategorys($filter: ModelCategoryFilterInput, $limit: Int, $nextToken: String) {
+					listCategorys(filter: $filter, limit: $limit, nextToken: $nextToken) {
+						__typename
+						items {
+							__typename
+							id
+							teamId
+							team {
+								__typename
+								id
+								name
+								ownerUserId
+								companyId
+								createdByUserId
+								createdOn
+								lastUpdatedByUserId
+								lastUpdatedOn
+								_version
+								_deleted
+								_lastChangedAt
+							}
+							name
+							deleted
+							_version
+							_deleted
+							_lastChangedAt
+						}
+						nextToken
+						startedAt
+					}
+				}
+			`,
+			{ teamId: '353c0206-fa91-489b-bfb7-e896aeb7e25a', limit: 1000}
+		);
+
+		this.listFuseHelper.getFilteredItems$
+	}
+
+	handleInput(e) {
+		const value = e.target.value;
+		console.log('search ' + value);
+		this.filterSrv.search(value);
+	}
+
 
 	test() {
 		// Test function
 	}
-
 }
