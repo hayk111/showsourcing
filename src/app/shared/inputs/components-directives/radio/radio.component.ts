090c3f87483bafa322d1af00f121493b2674e3fb
cedvdb <cedvandenbosch@gmail.com>
2020-04-15 16:11:24 +0200

    radio

--- src/app/shared/inputs/components-directives/radio/radio.component.ts
+++ src/app/shared/inputs/components-directives/radio/radio.component.ts
@@ -19,28 +19,16 @@ import { AbstractInput, makeAccessorProvider } from '~shared/inputs/components-d
 	providers: [makeAccessorProvider(RadioComponent)],
 })
 export class RadioComponent extends AbstractInput {
-	protected static NEXT_UID = 0;
 
 	@Input() isVeritical = false;
 	@Input() disabled = false;
 	/** list of possible values and labels */
-	@Input() items: { label: string, value: boolean }[];
+	@Input() choices: { label: string, value: boolean }[];
 
-	/** id of element, if not specified it will generate automtically */
-	@Input()
-	get id(): string { return this._id; }
-	set id(value: string) { this._id = value; }
-	protected _id: string = 'radio-' + RadioComponent.NEXT_UID++;
-
-	@Output() change = new EventEmitter();
-	@Output() update = new EventEmitter<boolean>();
-	@Output() select = new EventEmitter<null>();
-
-	@ViewChildren('inp') inps: QueryList<ElementRef>;
-
-	getId(index) {
-		return this._id + '-' + index;
+	pick(value: any) {
+		this.value = value;
+		this.onChangeFn(value);
+		this.onTouchedFn(value);
 	}
-
 
 }
