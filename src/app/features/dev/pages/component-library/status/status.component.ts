67ffc919915b8f37e55811210d9cfd77932592cf
augustin <doat.augustin@gmail.com>
2020-04-24 00:25:25 +0200

    edit css component-library

--- src/app/features/dev/pages/component-library/status/status.component.ts
+++ src/app/features/dev/pages/component-library/status/status.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { Typename } from '~core/erm3/typename.type';
 
 @Component({
 	selector: 'status-app',
@@ -7,6 +8,9 @@ import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
 	changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class StatusComponent implements OnInit {
+	statusTypenames = ['Product', 'Task', 'Sample', 'Supplier'];
+	typenameSelected: Typename;
+	entitySelected: any;
 	constructor() {}
 
 	ngOnInit(): void {}
