6fd636b18213937b5b757a4b679f636e8bcb6d86
Hayk Atoyan <haykatoyan@Hayks-MacBook-Pro.local>
2020-05-07 15:12:56 +0400

    Removed redundant files

--- src/app/shared/editable/components/index 2.ts
+++
@@ -1,6 +0,0 @@
-export * from './editable-actions/editable-actions.component';
-export * from './editable-container/editable-container.component';
-export * from './editable-field/editable-field.component';
-export * from './editable-label/editable-label.component';
-export * from './editable-value/editable-value.component';
-
