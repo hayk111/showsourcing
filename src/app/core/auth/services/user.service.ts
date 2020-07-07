f83beb8301b5f5cc536a4914c5aa3c5f96e418d2
hayk3 <hayk@simplytechnologies.net>
2020-07-07 11:35:35 +0400

    small fix

--- src/app/core/auth/services/user.service.ts
+++ src/app/core/auth/services/user.service.ts
@@ -27,9 +27,6 @@ export class UserService {
 	}
 
 	init() {
-		authStatus.signIn('augustin@showsourcing.com', 'Test1234').then(user => {
-			console.log('UserService -> init -> user', user);
-		});
 		this.authSrv.signIn$.pipe(
 			// preemptively putting the "user" so we don't need to wait to make calls with user id
 			tap((userId: string) => this.setupUser(userId)),
