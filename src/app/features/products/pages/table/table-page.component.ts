4d568bbaf56c14c99aac6bd39bb1a12f4e3c0172
augustin <doat.augustin@gmail.com>
2020-08-28 02:13:30 +0200

    add image for product table

--- src/app/features/products/pages/table/table-page.component.ts
+++ src/app/features/products/pages/table/table-page.component.ts
@@ -1,32 +1,22 @@
-import { Component, OnInit, ChangeDetectorRef, ViewChild, OnDestroy } from '@angular/core';
-import { Subscription, combineLatest } from 'rxjs';
-import { map, switchMap } from 'rxjs/operators';
+import { Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { map } from 'rxjs/operators';
+import { api, Product } from 'showsourcing-api-lib';
 import { DialogCommonService } from '~common/dialogs/services/dialog-common.service';
 import { ProductsTableComponent } from '~common/tables/products-table/products-table.component';
 import { FilterService, FilterType } from '~core/filters';
 import { ListHelper2Service, ListPageViewService, SelectionService } from '~core/list-page2';
 import { DialogService } from '~shared/dialog';
-import { PaginationService } from '~shared/pagination/services/pagination.service';
 import { RatingService } from '~shared/rating/services/rating.service';
-import { api, Product } from 'showsourcing-api-lib';
 import { AutoUnsub } from '~utils';
-// dailah lama goes into pizza store
-// servant asks : what pizza do you want sir ?
-// dailah lama: Make me one with everything.
 
 @Component({
 	selector: 'table-page-app',
 	templateUrl: './table-page.component.html',
 	styleUrls: ['./table-page.component.scss'],
-	providers: [
-		ListHelper2Service,
-		ListPageViewService,
-		FilterService,
-		SelectionService
-	],
+	providers: [ListHelper2Service, ListPageViewService, FilterService, SelectionService],
 	host: {
-		class: 'table-page'
-	}
+		class: 'table-page',
+	},
 })
 export class TablePageComponent extends AutoUnsub implements OnInit, OnDestroy {
 	// filter displayed as button in the filter panel
@@ -39,12 +29,24 @@ export class TablePageComponent extends AutoUnsub implements OnInit, OnDestroy {
 		FilterType.TAGS,
 		FilterType.STATUS,
 		FilterType.ARCHIVED,
-		FilterType.FAVORITE
+		FilterType.FAVORITE,
 	];
 	columns = ProductsTableComponent.DEFAULT_COLUMNS;
 	tableConfig = ProductsTableComponent.DEFAULT_TABLE_CONFIG;
-	productsWithImages: any [];
-	productsSub: Subscription;
+
+	products$ = this.listHelper.data$.pipe(
+		map((products: Product[]) => {
+			return products.map((product: any) => {
+				product.images = api.Image.findLocal({
+					filter: {
+						property: 'nodeId',
+						isString: 'Product:' + product.id,
+					},
+				});
+				return product;
+			});
+		})
+	);
 
 	@ViewChild(ProductsTableComponent, { static: false }) productsTable: ProductsTableComponent;
 
@@ -55,9 +57,7 @@ export class TablePageComponent extends AutoUnsub implements OnInit, OnDestroy {
 		public selectionSrv: SelectionService,
 		public dialogCommonSrv: DialogCommonService,
 		public ratingSrv: RatingService,
-		private cdr: ChangeDetectorRef,
-		protected dlgSrv: DialogService,
-		private paginationSrv: PaginationService,
+		protected dlgSrv: DialogService
 	) {
 		super();
 	}
@@ -66,25 +66,6 @@ export class TablePageComponent extends AutoUnsub implements OnInit, OnDestroy {
 		this.filterSrv.setup([], ['name']);
 		this.viewSrv.setup({ typename: 'Product', destUrl: 'products', view: 'table' });
 		this.listHelper.setup('Product', this._destroy$);
-
-		// this.paginationSrv.pagination$
-		// 	.pipe(
-		// 		switchMap((pagination) => api.Product.find$({ pagination }).data$),
-		// 		// map((products: Product[]) => {
-		// 		// 	return products.map((product: any) => {
-		// 		// 		product.images = api.Image.findLocal({
-		// 		// 			filter: {
-		// 		// 				property: 'nodeId',
-		// 		// 				isString: 'Product:' + product.id
-		// 		// 			}
-		// 		// 		});
-		// 		// 		return product;
-		// 		// 	});
-		// 		// })
-		// 	).subscribe(data => {
-		// 		this.productsWithImages = data;
-		// 		this.cdr.markForCheck();
-		// 	});
 	}
 
 	addProject() {
